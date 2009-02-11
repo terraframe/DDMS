a37262b41cec1d2d88d91156fa545949aee35266
jsmethie@terraframe.com <jsmethie@terraframe.com>
2009-02-11 02:45:34 +0000

    -Updated MosquitoCollectionTest -Removed un-needed UeberTestSuite
    git-svn-id: https://subversion.assembla.com/svn/mdss@91 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/src/test/mdss/entomology/MosquitoCollectionTest.java
+++ trunk/src/test/mdss/entomology/MosquitoCollectionTest.java
@@ -20,6 +20,12 @@ import com.terraframe.mojo.session.StartSession;
 
 public class MosquitoCollectionTest extends TestCase
 {
+  private static GeoEntity sentinelSite    = null;
+
+  private static GeoEntity nonSentinelSite = null;
+
+  private static GeoEntity fixedTrap       = null;
+
   public static Test suite()
   {
     TestSuite suite = new TestSuite();
@@ -41,25 +47,39 @@ public class MosquitoCollectionTest extends TestCase
 
     return wrapper;
   }
-  
+
   protected static void classSetUp()
   {
+    sentinelSite = new GeoEntity();
+    sentinelSite.setGeoId("0");
+    sentinelSite.setEntityName("Sentinel Site");
+    sentinelSite.addTerrain(Terrain.SENTINEL_SITE);
+    sentinelSite.apply();
+
+    nonSentinelSite = new GeoEntity();
+    nonSentinelSite.setGeoId("1");
+    nonSentinelSite.setEntityName("Non Sentinel Site");
+    nonSentinelSite.addTerrain(Terrain.NON_SENTINEL_SITE);
+    nonSentinelSite.apply();
+
+    fixedTrap = new GeoEntity();
+    fixedTrap.setGeoId("2");
+    fixedTrap.setEntityName("Fixed Trap");
+    fixedTrap.addTerrain(Terrain.FIXED_TRAP);
+    fixedTrap.apply();
   }
 
   protected static void classTearDown()
   {
+    sentinelSite.delete();
+    nonSentinelSite.delete();
+    fixedTrap.delete();
   }
 
   public void testSentinelSiteCollection()
   {
-    GeoEntity geoEntity = new GeoEntity();
-    geoEntity.setGeoId("5");
-    geoEntity.setEntityName("Test Site");
-    geoEntity.addTerrain(Terrain.SENTINEL_SITE);
-    geoEntity.apply();
-
     MosquitoCollection collection = new MosquitoCollection();
-    collection.setGeoEntity(geoEntity);
+    collection.setGeoEntity(sentinelSite);
     collection.setDateCollected(new Date());
     collection.addCollectionMethod(CollectionMethod.WINDOW_TRAP);
     collection.apply();
@@ -78,21 +98,14 @@ public class MosquitoCollectionTest extends TestCase
     finally
     {
       collection.delete();
-      geoEntity.delete();
     }
   }
 
   @StartSession
   public void testNonSentinelSiteCollection()
   {
-    GeoEntity geoEntity = new GeoEntity();
-    geoEntity.setGeoId("0");
-    geoEntity.setEntityName("Test Site");
-    geoEntity.addTerrain(Terrain.NON_SENTINEL_SITE);
-    geoEntity.apply();
-
     MosquitoCollection collection = new MosquitoCollection();
-    collection.setGeoEntity(geoEntity);
+    collection.setGeoEntity(nonSentinelSite);
     collection.setDateCollected(new Date());
     collection.addCollectionMethod(CollectionMethod.WINDOW_TRAP);
     collection.apply();
@@ -111,55 +124,40 @@ public class MosquitoCollectionTest extends TestCase
     finally
     {
       collection.delete();
-      geoEntity.delete();
-    }    
+    }
   }
-  
+
   public void testInvalidGeoTerrain()
   {
-    GeoEntity geoEntity = new GeoEntity();
-    geoEntity.setGeoId("0");
-    geoEntity.setEntityName("Test Site");
-    geoEntity.addTerrain(Terrain.FIXED_TRAP);
-    geoEntity.apply();
-
     MosquitoCollection collection = null;
 
     try
     {
       collection = new MosquitoCollection();
-      collection.setGeoEntity(geoEntity);
+      collection.setGeoEntity(fixedTrap);
       collection.setDateCollected(new Date());
       collection.addCollectionMethod(CollectionMethod.WINDOW_TRAP);
       collection.apply();
-      
+
       fail("Able to create a mosquito collection with a GeoEntity which is not a (non)sentinel site");
     }
-    catch(InvalidMosquitoCollectionGeoEntityException e)
+    catch (InvalidMosquitoCollectionGeoEntityException e)
     {
-      //This is expected
+      // This is expected
     }
     finally
     {
-      if(collection != null && collection.isAppliedToDB())
+      if (collection != null && collection.isAppliedToDB())
       {
         collection.delete();
       }
-      
-      geoEntity.delete();
     }
   }
-  
+
   public void testGetMorphologicalSpecieGroups()
   {
-    GeoEntity geoEntity = new GeoEntity();
-    geoEntity.setGeoId("0");
-    geoEntity.setEntityName("Test Site");
-    geoEntity.addTerrain(Terrain.NON_SENTINEL_SITE);
-    geoEntity.apply();
-
     MosquitoCollection collection = new MosquitoCollection();
-    collection.setGeoEntity(geoEntity);
+    collection.setGeoEntity(sentinelSite);
     collection.setDateCollected(new Date());
     collection.addCollectionMethod(CollectionMethod.WINDOW_TRAP);
     collection.apply();
@@ -177,18 +175,19 @@ public class MosquitoCollectionTest extends TestCase
     group2.addIdentificationMethod(IdentificationMethod.TEST_METHOD);
     group2.setCollection(collection);
     group2.apply();
-    
+
     try
     {
-      MosquitoCollection collection2 = MosquitoCollection.get(collection.getId());      
-      OIterator<? extends MorphologicalSpecieGroup> iterator = collection2.getAllMorphologicalSpecieGroup();
+      MosquitoCollection collection2 = MosquitoCollection.get(collection.getId());
+      OIterator<? extends MorphologicalSpecieGroup> iterator = collection2
+          .getAllMorphologicalSpecieGroup();
       List<MorphologicalSpecieGroup> list = new LinkedList<MorphologicalSpecieGroup>();
-      
-      while(iterator.hasNext())
+
+      while (iterator.hasNext())
       {
         list.add(iterator.next());
       }
-      
+
       assertEquals(2, list.size());
       assertEquals(new Integer(20), list.get(0).getQuanity());
       assertEquals(new Integer(10), list.get(1).getQuanity());
@@ -198,23 +197,16 @@ public class MosquitoCollectionTest extends TestCase
     finally
     {
       group.delete();
-      group2.delete();      
+      group2.delete();
       collection.delete();
-      geoEntity.delete();
-    }    
+    }
 
   }
-  
+
   public void testGeoDateUniquness()
   {
-    GeoEntity geoEntity = new GeoEntity();
-    geoEntity.setGeoId("0");
-    geoEntity.setEntityName("Test Site");
-    geoEntity.addTerrain(Terrain.NON_SENTINEL_SITE);
-    geoEntity.apply();
-
     MosquitoCollection collection = new MosquitoCollection();
-    collection.setGeoEntity(geoEntity);
+    collection.setGeoEntity(nonSentinelSite);
     collection.setDateCollected(new Date());
     collection.addCollectionMethod(CollectionMethod.WINDOW_TRAP);
     collection.apply();
@@ -224,45 +216,39 @@ public class MosquitoCollectionTest extends TestCase
     try
     {
       duplicate = new MosquitoCollection();
-      duplicate.setGeoEntity(geoEntity);
+      duplicate.setGeoEntity(nonSentinelSite);
       duplicate.setDateCollected(new Date());
       duplicate.addCollectionMethod(CollectionMethod.WINDOW_TRAP);
-      duplicate.apply();      
-      
+      duplicate.apply();
+
       fail("Able to create mulutiple MosquitoCollections with the same GeoEntity and Date");
     }
-    catch(DuplicateDataDatabaseException e)
+    catch (DuplicateDataDatabaseException e)
     {
-      //This is expected
+      // This is expected
     }
     finally
     {
-      if(duplicate != null && duplicate.isAppliedToDB())
+      if (duplicate != null && duplicate.isAppliedToDB())
       {
         duplicate.delete();
       }
+
       collection.delete();
-      geoEntity.delete();
     }
   }
 
   public void testSearchByGeoEntityAndDate()
   {
-    GeoEntity geoEntity = new GeoEntity();
-    geoEntity.setGeoId("0");
-    geoEntity.setEntityName("Test Site");
-    geoEntity.addTerrain(Terrain.NON_SENTINEL_SITE);
-    geoEntity.apply();
-
     MosquitoCollection collection = new MosquitoCollection();
-    collection.setGeoEntity(geoEntity);
+    collection.setGeoEntity(sentinelSite);
     collection.setDateCollected(new Date());
     collection.addCollectionMethod(CollectionMethod.WINDOW_TRAP);
-    collection.apply();    
-    
+    collection.apply();
+
     try
     {
-      MosquitoCollection search = MosquitoCollection.searchByGeoEntityAndDate(geoEntity, new Date());
+      MosquitoCollection search = MosquitoCollection.searchByGeoEntityAndDate(sentinelSite, new Date());
 
       assertNotNull(search);
       assertEquals(collection.getId(), search.getId());
@@ -270,86 +256,50 @@ public class MosquitoCollectionTest extends TestCase
     finally
     {
       collection.delete();
-      geoEntity.delete();
     }
   }
-  
+
   public void testEmptySearchByGeoEntityAndDate()
   {
-    GeoEntity geoEntity = new GeoEntity();
-    geoEntity.setGeoId("0");
-    geoEntity.setEntityName("Test Site");
-    geoEntity.addTerrain(Terrain.NON_SENTINEL_SITE);
-    geoEntity.apply();
-
-    try
-    {
-      assertNull(MosquitoCollection.searchByGeoEntityAndDate(geoEntity, new Date()));
-    }
-    finally
-    {
-      geoEntity.delete();
-    }    
+    assertNull(MosquitoCollection.searchByGeoEntityAndDate(nonSentinelSite, new Date()));
   }
-  
+
   public void testWrongGeoEntitySearchByGeoEntityAndDate() throws ParseException
   {
-    GeoEntity geoEntity = new GeoEntity();
-    geoEntity.setGeoId("0");
-    geoEntity.setEntityName("Test Site");
-    geoEntity.addTerrain(Terrain.NON_SENTINEL_SITE);
-    geoEntity.apply();
-    
-    GeoEntity geoEntity2 = new GeoEntity();
-    geoEntity2.setGeoId("1");
-    geoEntity2.setEntityName("Test Site 2");
-    geoEntity2.addTerrain(Terrain.SENTINEL_SITE);
-    geoEntity2.apply();
-
     MosquitoCollection collection = new MosquitoCollection();
-    collection.setGeoEntity(geoEntity);
+    collection.setGeoEntity(sentinelSite);
     collection.setDateCollected(new Date());
     collection.addCollectionMethod(CollectionMethod.WINDOW_TRAP);
-    collection.apply();    
-    
+    collection.apply();
+
     try
     {
-      assertNull(MosquitoCollection.searchByGeoEntityAndDate(geoEntity2, new Date()));
+      assertNull(MosquitoCollection.searchByGeoEntityAndDate(nonSentinelSite, new Date()));
     }
     finally
     {
       collection.delete();
-      geoEntity.delete();
-      geoEntity2.delete();
     }
-    
+
   }
-  
+
   public void testWrongDateSearchByGeoEntityAndDate() throws ParseException
   {
     SimpleDateFormat dateTime = new SimpleDateFormat(DatabaseProperties.getDateFormat());
 
-    GeoEntity geoEntity = new GeoEntity();
-    geoEntity.setGeoId("0");
-    geoEntity.setEntityName("Test Site");
-    geoEntity.addTerrain(Terrain.NON_SENTINEL_SITE);
-    geoEntity.apply();
-
     MosquitoCollection collection = new MosquitoCollection();
-    collection.setGeoEntity(geoEntity);
+    collection.setGeoEntity(nonSentinelSite);
     collection.setDateCollected(dateTime.parse("2008-01-01"));
     collection.addCollectionMethod(CollectionMethod.WINDOW_TRAP);
-    collection.apply();    
-    
+    collection.apply();
+
     try
     {
-      assertNull(MosquitoCollection.searchByGeoEntityAndDate(geoEntity, dateTime.parse("2009-01-01")));
+      assertNull(MosquitoCollection.searchByGeoEntityAndDate(nonSentinelSite, dateTime.parse("2009-01-01")));
     }
     finally
     {
       collection.delete();
-      geoEntity.delete();
     }
-    
   }
 }
