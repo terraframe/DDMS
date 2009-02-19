b9a7a91b30edf1dd1d42b4495facba8708524ce6
nmceachen <nmceachen>
2009-02-19 02:38:17 +0000

    OBO Parse example
    git-svn-id: https://subversion.assembla.com/svn/mdss@201 e0e46c49-be69-4f5a-ad62-21024a331aea

---
+++ examples/OBOParse/generated/server/stub/org/obofoundry/Term.java
@@ -0,0 +1,12 @@
+package org.obofoundry;
+
+public abstract class Term extends TermBase implements com.terraframe.mojo.generation.loader.Reloadable
+{
+  private static final long serialVersionUID = 1229530369212L;
+  
+  public Term()
+  {
+    super();
+  }
+  
+}
---
+++ examples/OBOParse/generated/server/stub/org/obofoundry/default_namespace/MosquitoAnatomy.java
@@ -0,0 +1,12 @@
+package org.obofoundry.default_namespace;
+
+public class MosquitoAnatomy extends MosquitoAnatomyBase implements com.terraframe.mojo.generation.loader.Reloadable
+{
+  private static final long serialVersionUID = 1229530626054L;
+  
+  public MosquitoAnatomy()
+  {
+    super();
+  }
+  
+}
