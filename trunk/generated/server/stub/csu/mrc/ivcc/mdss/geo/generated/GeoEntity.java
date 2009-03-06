2269880370623e2544cd5dd53f993d9eb65862fc
jnaifeh@terraframe.com <jnaifeh@terraframe.com>
2009-03-06 21:47:15 +0000

    Re-added stubsource to GeoEntity
    git-svn-id: https://subversion.assembla.com/svn/mdss@394 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/generated/server/stub/csu/mrc/ivcc/mdss/geo/generated/GeoEntity.java
+++ trunk/generated/server/stub/csu/mrc/ivcc/mdss/geo/generated/GeoEntity.java
@@ -1,12 +1,46 @@
 package csu.mrc.ivcc.mdss.geo.generated;
 
-public abstract class GeoEntity extends GeoEntityBase implements com.terraframe.mojo.generation.loader.Reloadable
+
+import com.terraframe.mojo.dataaccess.InvalidIdException;
+import com.terraframe.mojo.query.OIterator;
+import com.terraframe.mojo.query.QueryFactory;
+
+import csu.mrc.ivcc.mdss.geo.generated.GeoEntityBase;
+import csu.mrc.ivcc.mdss.geo.generated.GeoEntityQuery;
+
+public class GeoEntity extends GeoEntityBase implements com.terraframe.mojo.generation.loader.Reloadable
 {
-  private static final long serialVersionUID = 1236366373361L;
+  private static final long serialVersionUID = 1234288139462L;
   
   public GeoEntity()
   {
     super();
+  }
+  
+  public static csu.mrc.ivcc.mdss.geo.generated.GeoEntity searchByGeoId(java.lang.String geoId)
+  {
+    GeoEntity geoEntity = null;
+    QueryFactory factory = new QueryFactory();
+    GeoEntityQuery query = new GeoEntityQuery(factory);    
+
+    query.getGeoId().EQ(geoId);
+    
+    OIterator<? extends GeoEntity> iterator = query.getIterator();
+    
+    if(iterator.hasNext())
+    {
+      geoEntity = iterator.next();      
+    }
+    
+    iterator.close();
+    
+    if(geoEntity == null)
+    {
+      String msg = "A GeoEntity with the geoId [" + geoId + "] does not exist";
+      throw new InvalidIdException(msg, geoId);
+    }
+    
+    return geoEntity;
   }
   
 }
