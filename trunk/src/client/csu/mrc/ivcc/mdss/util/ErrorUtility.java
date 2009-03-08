b7a61bb35d491483642b6aff8ac16ce472bce347
jsmethie@terraframe.com <jsmethie@terraframe.com>
2009-03-08 21:56:07 +0000

    -Added error handling for MosquitoCollections
    git-svn-id: https://subversion.assembla.com/svn/mdss@432 e0e46c49-be69-4f5a-ad62-21024a331aea

---
+++ trunk/src/client/csu/mrc/ivcc/mdss/util/ErrorUtility.java
@@ -0,0 +1,33 @@
+package csu.mrc.ivcc.mdss.util;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.terraframe.mojo.ProblemExceptionDTO;
+
+public class ErrorUtility
+{
+  public static final String ERROR_MESSAGE_ARRAY = "errorMessageArray";
+  
+  public static final String ERROR_MESSAGE = "errorMessage";
+  
+  public static void prepareProblems(ProblemExceptionDTO e, HttpServletRequest req)
+  {
+    List<String> messages = e.getProblemMessages();
+    messages.add(0, e.getLocalizedMessage());
+    String[] messagesArr = messages.toArray(new String[messages.size()]);
+    
+    req.setAttribute(ErrorUtility.ERROR_MESSAGE_ARRAY, messagesArr);
+  }
+  
+  public static void prepareThrowable(Throwable t, HttpServletRequest req)
+  {
+    req.setAttribute(ErrorUtility.ERROR_MESSAGE, t.getLocalizedMessage());
+  }
+  
+  public static void prepareMessages()
+  {
+    
+  }
+}
