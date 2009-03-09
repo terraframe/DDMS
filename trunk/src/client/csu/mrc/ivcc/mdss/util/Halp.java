e360f2acfa5b4c9757103979b7f1a05f536988e2
darrellt@gmail.com <darrellt@gmail.com>
2009-03-09 04:13:55 +0000

    fix halp
    git-svn-id: https://subversion.assembla.com/svn/mdss@452 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/src/client/csu/mrc/ivcc/mdss/util/Halp.java
+++ trunk/src/client/csu/mrc/ivcc/mdss/util/Halp.java
@@ -1,34 +1,47 @@
 package csu.mrc.ivcc.mdss.util;
 
-import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import csu.mrc.ivcc.mdss.mo.AbstractTermDTO;
+import csu.mrc.ivcc.mdss.mo.SpecieDTO;
 
+public class Halp implements com.terraframe.mojo.generation.loader.Reloadable {
 
-public class Halp
-{
-	 public static String join(ArrayList<String> s, String delimiter) {
-	      StringBuilder builder = new StringBuilder();
-	      Iterator<String> iter = s.iterator();
-	      while (iter.hasNext()) {
-	         builder.append(iter.next());
-	          if (iter.hasNext()) {
-	              builder.append(delimiter);
-	          }
-	      }
-	      return builder.toString();
-	 }
-	 
-	public static String getDropDownMap(AbstractTermDTO[] terms) throws JSONException {
-		 JSONObject map = new JSONObject();
-		 for(AbstractTermDTO term : terms)
-		 {
-		    map.put(term.getDisplayLabel(),term.getId());
-		 } 
-		 return map.toString();
-	 }
-	 
+	public final static String CLASS = "mdss.ivcc.mrc.csu.util.Halp";
+
+	public static String join(List<String> s, String delimiter) {
+		StringBuilder builder = new StringBuilder();
+		Iterator<String> iter = s.iterator();
+		
+		while (iter.hasNext()) {
+			builder.append(iter.next());
+			if (iter.hasNext()) {
+				builder.append(delimiter);
+			}
+		}
+		return builder.toString();
+	}
+
+	public static String getDropDownMap(AbstractTermDTO[] terms)
+			throws JSONException {
+		JSONObject map = new JSONObject();
+		for (AbstractTermDTO term : terms) {
+			map.put(term.getDisplayLabel(), term.getId());
+		}
+		return map.toString();
+	}
+
+	public static String getDropDownMap2(List<SpecieDTO> terms)
+			throws JSONException {
+		JSONObject map = new JSONObject();
+		for (SpecieDTO term : terms) {
+			map.put(term.getDisplayLabel(), term.getId());
+		}
+		return map.toString();
+	}
+
 }
