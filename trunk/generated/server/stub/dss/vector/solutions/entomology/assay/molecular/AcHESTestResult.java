f1d3316aefc0bf7f021bea4a2aaa42758b4de54d
darrellt@gmail.com <darrellt@gmail.com>
2009-04-05 20:37:44 +0000

    Change mosqutio collection  to add collection method search params
    git-svn-id: https://subversion.assembla.com/svn/mdss@852 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/generated/server/stub/dss/vector/solutions/entomology/assay/molecular/AcHESTestResult.java
+++ trunk/generated/server/stub/dss/vector/solutions/entomology/assay/molecular/AcHESTestResult.java
@@ -2,7 +2,7 @@ package dss.vector.solutions.entomology.assay.molecular;
 
 public class AcHESTestResult extends AcHESTestResultBase implements com.terraframe.mojo.generation.loader.Reloadable
 {
-  private static final long serialVersionUID = 1237823577578L;
+  private static final long serialVersionUID = 1237829480331L;
   
   public AcHESTestResult()
   {
--- trunk/generated/server/stub/dss/vector/solutions/entomology/assay/molecular/AcHEWTestResult.java
+++ trunk/generated/server/stub/dss/vector/solutions/entomology/assay/molecular/AcHEWTestResult.java
@@ -2,7 +2,7 @@ package dss.vector.solutions.entomology.assay.molecular;
 
 public class AcHEWTestResult extends AcHEWTestResultBase implements com.terraframe.mojo.generation.loader.Reloadable
 {
-  private static final long serialVersionUID = 1237823575501L;
+  private static final long serialVersionUID = 1237829478189L;
   
   public AcHEWTestResult()
   {
