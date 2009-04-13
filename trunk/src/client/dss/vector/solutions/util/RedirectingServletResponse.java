6e8529b4d10a5a0c876a20607aa6a217d075625b
darrellt@gmail.com <darrellt@gmail.com>
2009-04-13 03:29:22 +0000

    fix redirecting servlet response for utf-8
    git-svn-id: https://subversion.assembla.com/svn/mdss@929 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/src/client/dss/vector/solutions/util/RedirectingServletResponse.java
+++ trunk/src/client/dss/vector/solutions/util/RedirectingServletResponse.java
@@ -1,9 +1,12 @@
 package dss.vector.solutions.util;
 
+import java.io.CharArrayWriter;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
- 
+import java.io.UnsupportedEncodingException;
+
 import javax.servlet.ServletOutputStream;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
@@ -13,72 +16,82 @@ import javax.servlet.http.HttpServletResponseWrapper;
 
 /**
  * @author Darrell Taylor
- * 
+ *
  * This class is used to render a jsp to a string for inside out rendering.
  *
  */
 public class RedirectingServletResponse extends HttpServletResponseWrapper {
-	 
+
     RedirectServletStream out;
     HttpServletResponse response;
- 
-    /**
-     * @param arg0
-     */
+    PrintWriter printWriter;
+    OutputStreamWriter osw;
+
     public RedirectingServletResponse(HttpServletResponse response, OutputStream out) {
         super(response);
         this.response = response;
         this.out = new RedirectServletStream(out);
+        try {
+            this.osw = new OutputStreamWriter(out,"UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        this.printWriter = new PrintWriter(osw,true);
     }
- 
+
     /* (non-Javadoc)
      * @see javax.servlet.ServletResponse#flushBuffer()
      */
-    //public void flushBuffer() throws IOException {
-   // 	out.flush();        
-   // }
- 
+    public void flushBuffer() throws IOException {
+        printWriter.flush();
+    }
+
     /* (non-Javadoc)
      * @see javax.servlet.ServletResponse#getOutputStream()
      */
     public ServletOutputStream getOutputStream() throws IOException {
         return out;
     }
- 
+
     /* (non-Javadoc)
      * @see javax.servlet.ServletResponse#getWriter()
      */
     public PrintWriter getWriter() throws IOException {
-        return new PrintWriter(out);
+        return printWriter;
     }
-    
-    
+
+
     /* (non-Javadoc)
 	 * @see javax.servlet.ServletResponseWrapper#getResponse()
 	 */
 	public ServletResponse getResponse() {
 		return super.getResponse();
 	}
- 
+
 	/* (non-Javadoc)
 	 * @see javax.servlet.ServletResponseWrapper#setResponse(javax.servlet.ServletResponse)
 	 */
 	public void setResponse(ServletResponse arg0) {
 		super.setResponse(arg0);
 	}
- 
- 
- 
+
+
+
 	private static class RedirectServletStream extends ServletOutputStream {
-        OutputStream out;
- 
+	    OutputStream out;
+
         RedirectServletStream(OutputStream out) {
             this.out = out;
         }
- 
+
+        //public void write(char[] c, int off, int len)
+        //{
+         //   out.write(c,off,len);
+       // }
+
         public void write(int param) throws java.io.IOException {
             out.write(param);
         }
     }
- 
+
 }
