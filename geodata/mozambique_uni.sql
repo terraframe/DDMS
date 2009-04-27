47ed6581b395250dc0596ebab8140d108629933a
galgal <galgal>
2009-04-27 17:44:05 +0000

    geodata / mozambique_uni.sql - an SQL script to modify the geodata delivered by MRC Insert geodata "universals" - categories of geoentities In particular, "river" and "road" must be singular.
    git-svn-id: https://subversion.assembla.com/svn/mdss@1123 e0e46c49-be69-4f5a-ad62-21024a331aea

---
+++ geodata/mozambique_uni.sql
@@ -0,0 +1,47 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Started on 2009-04-24 15:57:21 MDT
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+SET search_path = public, pg_catalog;
+
+--
+-- TOC entry 2531 (class 0 OID 212832)
+-- Dependencies: 2232
+-- Data for Name: universal; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO universal (universal_id, universal) VALUES (1, 'Country');
+INSERT INTO universal (universal_id, universal) VALUES (4, 'Administrative post');
+INSERT INTO universal (universal_id, universal) VALUES (2, 'Province');
+INSERT INTO universal (universal_id, universal) VALUES (6, 'City');
+INSERT INTO universal (universal_id, universal) VALUES (7, 'Town');
+INSERT INTO universal (universal_id, universal) VALUES (8, 'Village');
+INSERT INTO universal (universal_id, universal) VALUES (9, 'Health Facility');
+INSERT INTO universal (universal_id, universal) VALUES (11, 'Exit Trap');
+INSERT INTO universal (universal_id, universal) VALUES (12, 'Spray Zone');
+INSERT INTO universal (universal_id, universal) VALUES (15, 'Reserve');
+INSERT INTO universal (universal_id, universal) VALUES (5, 'Locality');
+INSERT INTO universal (universal_id, universal) VALUES (16, 'River');
+INSERT INTO universal (universal_id, universal) VALUES (17, 'Railway');
+INSERT INTO universal (universal_id, universal) VALUES (18, 'National Road');
+INSERT INTO universal (universal_id, universal) VALUES (19, 'Road');
+INSERT INTO universal (universal_id, universal) VALUES (3, 'District');
+INSERT INTO universal (universal_id, universal) VALUES (13, 'Spray Locality');
+INSERT INTO universal (universal_id, universal) VALUES (10, 'Sentinal Site');
+INSERT INTO universal (universal_id, universal) VALUES (14, 'Permanent Water Body');
+
+
+-- Completed on 2009-04-24 15:57:21 MDT
+
+--
+-- PostgreSQL database dump complete
+--
+
