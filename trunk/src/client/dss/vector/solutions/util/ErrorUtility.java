2711a801dd372578fdad9e9aa7a60795d9d50f7a
darrellt@gmail.com <darrellt@gmail.com>
2009-05-03 07:01:25 +0000

    make error utility add developer message so a stack trace can be printed out for caught errors
    git-svn-id: https://subversion.assembla.com/svn/mdss@1224 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/src/client/dss/vector/solutions/util/ErrorUtility.java
+++ trunk/src/client/dss/vector/solutions/util/ErrorUtility.java
@@ -8,12 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import com.terraframe.mojo.AttributeNotificationDTO;
 import com.terraframe.mojo.ProblemExceptionDTO;
 import com.terraframe.mojo.business.ProblemDTOIF;
+import com.terraframe.mojo.dataaccess.ProgrammingErrorExceptionDTO;
 
 public class ErrorUtility
 {
   public static final String ERROR_MESSAGE_ARRAY = "errorMessageArray";
 
   public static final String ERROR_MESSAGE       = "errorMessage";
+  public static final String DEVELOPER_MESSAGE       = "developerMessage";
+
 
   public static void prepareProblems(ProblemExceptionDTO e, HttpServletRequest req)
   {
@@ -38,6 +41,21 @@ public class ErrorUtility
   public static void prepareThrowable(Throwable t, HttpServletRequest req)
   {
     req.setAttribute(ErrorUtility.ERROR_MESSAGE, t.getLocalizedMessage());
+
+    if(t instanceof ProgrammingErrorExceptionDTO)
+    {
+      try
+      {
+        ProgrammingErrorExceptionDTO pee = (ProgrammingErrorExceptionDTO) t;
+        req.setAttribute(ErrorUtility.DEVELOPER_MESSAGE, pee.getDeveloperMessage());
+      }
+      catch (Exception e)
+      {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+
   }
 
   public static void forceProblems(ProblemExceptionDTO e, HttpServletRequest req)
