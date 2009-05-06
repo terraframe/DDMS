1dead06b6c62485b262ec2c3666425f5774fe627
jsmethie@terraframe.com <jsmethie@terraframe.com>
2009-05-06 23:05:56 +0000

    -Fixed PermissionEntity relationship problem -Added default.converter property so that global conversion defaults can be specified for the request scraper
    git-svn-id: https://subversion.assembla.com/svn/mdss@1329 e0e46c49-be69-4f5a-ad62-21024a331aea

---
+++ trunk/src/client/dss/vector/solutions/util/DefaultConverter.java
@@ -0,0 +1,53 @@
+package dss.vector.solutions.util;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Locale;
+
+import com.terraframe.mojo.ProblemExceptionDTO;
+import com.terraframe.mojo.business.ProblemDTOIF;
+import com.terraframe.mojo.controller.DateParseProblemDTO;
+import com.terraframe.mojo.controller.StandardConverter;
+
+public class DefaultConverter extends StandardConverter
+{
+
+  public DefaultConverter(Class<?> c)
+  {
+    super(c);
+  }
+
+  @Override
+  public String format(Object object, Locale locale)
+  {
+    if (object instanceof Date)
+    {
+      DateFormat format = DateFormat.getDateInstance(DateFormat.SHORT, locale);
+
+      return format.format((Date) object);
+    }
+    return super.format(object, locale);
+  }
+
+  @Override
+  public Object parse(String value, Locale locale)
+  {
+    if (c.equals(Date.class))
+    {
+      DateFormat format = DateFormat.getDateInstance(DateFormat.SHORT, locale);
+
+      try
+      {
+        return format.parse(value);
+      }
+      catch (Exception e)
+      {
+        //Continue on to the Standard Parsing
+      }
+    }
+    
+    return super.parse(value, locale);
+  }
+}
