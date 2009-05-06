6af85ace12eba6f7ddb90008fd8357728fe6e057
darrellt@gmail.com <darrellt@gmail.com>
2009-05-06 22:56:50 +0000

    Fix dateOfBirth in personViewDTO
    git-svn-id: https://subversion.assembla.com/svn/mdss@1327 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/generated/client/stub/dss/vector/solutions/PersonViewDTO.java
+++ trunk/generated/client/stub/dss/vector/solutions/PersonViewDTO.java
@@ -1,12 +1,23 @@
 package dss.vector.solutions;
 
+import com.terraframe.mojo.util.Converter;
+
+import dss.vector.solutions.util.DateConverter;
+
 public class PersonViewDTO extends PersonViewDTOBase
  implements com.terraframe.mojo.generation.loader.Reloadable{
   private static final long serialVersionUID = 1240852495972L;
-  
+
   public PersonViewDTO(com.terraframe.mojo.constants.ClientRequestIF clientRequest)
   {
     super(clientRequest);
   }
-  
+
+
+  public Converter getDateOfBirthConverter()
+  {
+    String label = getDateOfBirthMd().getDisplayLabel();
+
+    return new DateConverter(label);
+  }
 }
