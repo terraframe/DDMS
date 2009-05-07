164c7839839070948511b4ee9d674fe7851ccb0a
jnaifeh@terraframe.com <jnaifeh@terraframe.com>
2009-05-07 02:52:28 +0000

    Fixed problem where null was returned for all primitive inputs.
    git-svn-id: https://subversion.assembla.com/svn/mdss@1336 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/src/client/dss/vector/solutions/util/DefaultConverter.java
+++ trunk/src/client/dss/vector/solutions/util/DefaultConverter.java
@@ -1,14 +1,9 @@
 package dss.vector.solutions.util;
 
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.util.Date;
-import java.util.LinkedList;
 import java.util.Locale;
 
-import com.terraframe.mojo.ProblemExceptionDTO;
-import com.terraframe.mojo.business.ProblemDTOIF;
-import com.terraframe.mojo.controller.DateParseProblemDTO;
 import com.terraframe.mojo.controller.StandardConverter;
 
 public class DefaultConverter extends StandardConverter
@@ -34,21 +29,20 @@ public class DefaultConverter extends StandardConverter
   @Override
   public Object parse(String value, Locale locale)
   {
+    if (c.equals(Date.class))
+    {
+      DateFormat format = DateFormat.getDateInstance(DateFormat.SHORT, locale);
+
+      try
+      {
+        return format.parse(value);
+      }
+      catch (Exception e)
+      {
+        //Continue on to the Standard Parsing
+      }
+    }
+
     return super.parse(value, locale);
-//    if (c.equals(Date.class))
-//    {
-//      DateFormat format = DateFormat.getDateInstance(DateFormat.SHORT, locale);
-//
-//      try
-//      {
-//        return format.parse(value);
-//      }
-//      catch (Exception e)
-//      {
-//        //Continue on to the Standard Parsing
-//      }
-//    }
-//    
-//    return super.parse(value, locale);
   }
 }
