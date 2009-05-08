e7b64b30b9008f039b6461fbc5ab5f7ae9d0aaad
darrellt@gmail.com <darrellt@gmail.com>
2009-05-08 01:36:24 +0000

    Clean up of commented code
    git-svn-id: https://subversion.assembla.com/svn/mdss@1376 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/generated/client/stub/dss/vector/solutions/irs/ResourceTargetController.java
+++ trunk/generated/client/stub/dss/vector/solutions/irs/ResourceTargetController.java
@@ -9,6 +9,8 @@ import dss.vector.solutions.irs.SprayOperatorDTO;
 import dss.vector.solutions.PersonDTO;
 import dss.vector.solutions.irs.ResourceTargetViewDTO;
 
+//TODO: delete unused methods from metadata
+
 public class ResourceTargetController extends ResourceTargetControllerBase implements com.terraframe.mojo.generation.loader.Reloadable
 {
   public static final String JSP_DIR          = "WEB-INF/dss/vector/solutions/irs/ResourceTarget/";
@@ -25,13 +27,6 @@ public class ResourceTargetController extends ResourceTargetControllerBase imple
   public void viewAll() throws java.io.IOException, javax.servlet.ServletException
   {
     com.terraframe.mojo.constants.ClientRequestIF clientRequest = super.getClientRequest();
-
-    //SprayTeamDTO newTeam = new SprayTeamDTO(clientRequest);
-    //newTeam.setTeamCode("12345");
-    //newTeam.apply();
-    //newTeam = SprayTeamDTO.lock(clientRequest, newTeam.getId());
-    //newTeam.addSprayTeamMembers(SprayOperatorDTO.getAllInstances(super.getClientSession().getRequest(), "keyName", true, 0, 0).getResultSet().get(0));
-    //newTeam.apply();
 
     List<SprayTeamDTO> sprayTeams = (List<SprayTeamDTO>) SprayTeamDTO.getAllInstances(clientRequest, SprayTeamDTO.TEAMID, true, 0, 0).getResultSet();
 
@@ -72,11 +67,13 @@ public class ResourceTargetController extends ResourceTargetControllerBase imple
     {
       targetIds.add(teamMember.getId());
     }
+
     // add the member's Team or All Teams
     for (SprayTeamDTO team : sprayTeams)
     {
       targetIds.add(team.getId());
     }
+
     ResourceTargetViewDTO[] resourceTargetViews = ResourceTargetViewDTO.getResourceTargets(clientRequest, (String[]) targetIds.toArray(new String[targetIds.size()]), year);
 
     req.setAttribute("resourceTargetViews", resourceTargetViews);
@@ -88,85 +85,4 @@ public class ResourceTargetController extends ResourceTargetControllerBase imple
     this.viewAll();
   }
 
-  /*
-   * public void newInstance() throws java.io.IOException,
-   * javax.servlet.ServletException {
-   * com.terraframe.mojo.constants.ClientRequestIF clientRequest =
-   * super.getClientRequest(); dss.vector.solutions.irs.ResourceTargetDTO dto =
-   * new dss.vector.solutions.irs.ResourceTargetDTO(clientRequest);
-   * req.setAttribute("item", dto); req.setAttribute("page_title",
-   * "Create ResourceTargetController"); render("createComponent.jsp"); } public
-   * void failNewInstance() throws java.io.IOException,
-   * javax.servlet.ServletException { this.viewAll(); } public void
-   * cancel(dss.vector.solutions.irs.ResourceTargetDTO dto) throws
-   * java.io.IOException, javax.servlet.ServletException { dto.unlock();
-   * this.view(dto.getId()); } public void
-   * failCancel(dss.vector.solutions.irs.ResourceTargetDTO dto) throws
-   * java.io.IOException, javax.servlet.ServletException { resp.sendError(500);
-   * } public void create(dss.vector.solutions.irs.ResourceTargetDTO dto) throws
-   * java.io.IOException, javax.servlet.ServletException { try { dto.apply();
-   * this.view(dto.getId()); } catch(com.terraframe.mojo.ProblemExceptionDTO e)
-   * { this.failCreate(dto); } } public void
-   * failCreate(dss.vector.solutions.irs.ResourceTargetDTO dto) throws
-   * java.io.IOException, javax.servlet.ServletException {
-   * req.setAttribute("dss_vector_solutions_irs_ResourceTarget_targeter",
-   * dss.vector
-   * .solutions.irs.TargeterDTO.getAllInstances(super.getClientSession(
-   * ).getRequest(), "keyName", true, 0, 0).getResultSet());
-   * req.setAttribute("item", dto); req.setAttribute("page_title",
-   * "Create ResourceTargetController"); render("createComponent.jsp"); } public
-   * void viewPage(java.lang.String sortAttribute, java.lang.Boolean
-   * isAscending, java.lang.Integer pageSize, java.lang.Integer pageNumber)
-   * throws java.io.IOException, javax.servlet.ServletException {
-   * com.terraframe.mojo.constants.ClientRequestIF clientRequest =
-   * super.getClientRequest(); dss.vector.solutions.irs.ResourceTargetQueryDTO
-   * query =
-   * dss.vector.solutions.irs.ResourceTargetDTO.getAllInstances(clientRequest,
-   * sortAttribute, isAscending, pageSize, pageNumber);
-   * req.setAttribute("query", query); req.setAttribute("page_title",
-   * "View All ResourceTargetController Objects");
-   * render("viewAllComponent.jsp"); } public void failViewPage(java.lang.String
-   * sortAttribute, java.lang.String isAscending, java.lang.String pageSize,
-   * java.lang.String pageNumber) throws java.io.IOException,
-   * javax.servlet.ServletException { resp.sendError(500); } public void
-   * edit(java.lang.String id) throws java.io.IOException,
-   * javax.servlet.ServletException { dss.vector.solutions.irs.ResourceTargetDTO
-   * dto =
-   * dss.vector.solutions.irs.ResourceTargetDTO.lock(super.getClientRequest(),
-   * id); req.setAttribute("dss_vector_solutions_irs_ResourceTarget_targeter",
-   * dss
-   * .vector.solutions.irs.TargeterDTO.getAllInstances(super.getClientSession(
-   * ).getRequest(), "keyName", true, 0, 0).getResultSet());
-   * req.setAttribute("item", dto); req.setAttribute("page_title",
-   * "Edit ResourceTargetController"); render("editComponent.jsp"); } public
-   * void failEdit(java.lang.String id) throws java.io.IOException,
-   * javax.servlet.ServletException { this.view(id); } public void
-   * update(dss.vector.solutions.irs.ResourceTargetDTO dto) throws
-   * java.io.IOException, javax.servlet.ServletException { try { dto.apply();
-   * this.view(dto.getId()); } catch(com.terraframe.mojo.ProblemExceptionDTO e)
-   * { this.failUpdate(dto); } } public void
-   * failUpdate(dss.vector.solutions.irs.ResourceTargetDTO dto) throws
-   * java.io.IOException, javax.servlet.ServletException {
-   * req.setAttribute("dss_vector_solutions_irs_ResourceTarget_targeter",
-   * dss.vector
-   * .solutions.irs.TargeterDTO.getAllInstances(super.getClientSession(
-   * ).getRequest(), "keyName", true, 0, 0).getResultSet());
-   * req.setAttribute("item", dto); req.setAttribute("page_title",
-   * "Update ResourceTargetController"); render("editComponent.jsp"); }
-   *
-   *
-   *
-   * public void delete(dss.vector.solutions.irs.ResourceTargetDTO dto) throws
-   * java.io.IOException, javax.servlet.ServletException { try { dto.delete();
-   * this.viewAll(); } catch(com.terraframe.mojo.ProblemExceptionDTO e) {
-   * this.failDelete(dto); } } public void
-   * failDelete(dss.vector.solutions.irs.ResourceTargetDTO dto) throws
-   * java.io.IOException, javax.servlet.ServletException {
-   * req.setAttribute("dss_vector_solutions_irs_ResourceTarget_targeter",
-   * dss.vector
-   * .solutions.irs.TargeterDTO.getAllInstances(super.getClientSession(
-   * ).getRequest(), "keyName", true, 0, 0).getResultSet());
-   * req.setAttribute("item", dto); req.setAttribute("page_title",
-   * "Edit ResourceTargetController"); render("editComponent.jsp"); }
-   */
 }
