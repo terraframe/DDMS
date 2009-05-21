498415d8327c1df400295c98b8d634cda6320e4f
jsmethie@terraframe.com <jsmethie@terraframe.com>
2009-05-21 19:38:40 +0000

    -Fixed person age calculation
    git-svn-id: https://subversion.assembla.com/svn/mdss@1608 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/generated/server/stub/dss/vector/solutions/intervention/monitor/PersonView.java
+++ trunk/generated/server/stub/dss/vector/solutions/intervention/monitor/PersonView.java
@@ -10,7 +10,8 @@ import dss.vector.solutions.intervention.HumanSex;
 import dss.vector.solutions.intervention.RDTResponse;
 import dss.vector.solutions.intervention.RDTResult;
 
-public class PersonView extends PersonViewBase implements com.terraframe.mojo.generation.loader.Reloadable
+public class PersonView extends PersonViewBase implements
+    com.terraframe.mojo.generation.loader.Reloadable
 {
   private static final long serialVersionUID = 1239989843132L;
 
@@ -24,43 +25,49 @@ public class PersonView extends PersonViewBase implements com.terraframe.mojo.ge
   public void apply()
   {
     Person person = this.populatePerson();
- 
+
     person.apply();
-    
+
     person.populateView(this);
   }
 
   @Override
   public void delete()
   {
-    if(this.hasConcrete())
+    if (this.hasConcrete())
     {
       Person.get(this.getConcreteId()).delete();
     }
   }
-  
+
   private Person populatePerson()
   {
     Person person = new Person();
 
-    if(this.hasConcrete())
+    if (this.hasConcrete())
     {
       person = Person.lock(this.getConcreteId());
     }
 
-    if(this.getDob() != null)
+    if (this.getDob() != null)
     {
       person.setDob(this.getDob());
     }
-    else if(this.getAge() != null)
-    {
-      //Must calculate the date of birth from the age
-      Calendar c1 = Calendar.getInstance();
-      c1.set(Calendar.DAY_OF_YEAR, 1);
-      c1.set(Calendar.MONTH, Calendar.JANUARY);
-      c1.add(Calendar.YEAR, -this.getAge() + 1);
+    else if (this.getAge() != null)
+    {      
+      Calendar today = Calendar.getInstance();
+      Calendar dob = Calendar.getInstance();
+      dob.set(Calendar.DAY_OF_YEAR, 1);
+      dob.set(Calendar.MONTH, Calendar.JANUARY);
+      dob.add(Calendar.YEAR, -this.getAge());
 
-      person.setDob(c1.getTime());
+      if (today.get(Calendar.DAY_OF_YEAR) <= dob.get(Calendar.DAY_OF_YEAR))
+      {
+        dob.add(Calendar.YEAR, 1);
+      }
+
+      // Must calculate the date of birth from the age
+      person.setDob(dob.getTime());
     }
 
     person.setAnaemiaTreatment(this.getAnaemiaTreatment());
@@ -75,13 +82,20 @@ public class PersonView extends PersonViewBase implements com.terraframe.mojo.ge
     person.setRdtTreatment(this.getRdtTreatment());
     person.setSleptUnderNet(this.getSleptUnderNet());
 
-    for(BloodslideResponse r : this.getBloodslide()) person.addBloodslide(r);
-    for(FeverResponse r : this.getFever()) person.addFever(r);
-    for(FeverResponse r : this.getMalaria()) person.addMalaria(r);
-    for(FeverResponse r : this.getPayment()) person.addPayment(r);
-    for(RDTResponse r : this.getPerformedRDT()) person.addPerformedRDT(r);
-    for(RDTResult r : this.getRDTResult()) person.addRDTResult(r);
-    for(HumanSex r : this.getSex()) person.addSex(r);
+    for (BloodslideResponse r : this.getBloodslide())
+      person.addBloodslide(r);
+    for (FeverResponse r : this.getFever())
+      person.addFever(r);
+    for (FeverResponse r : this.getMalaria())
+      person.addMalaria(r);
+    for (FeverResponse r : this.getPayment())
+      person.addPayment(r);
+    for (RDTResponse r : this.getPerformedRDT())
+      person.addPerformedRDT(r);
+    for (RDTResult r : this.getRDTResult())
+      person.addRDTResult(r);
+    for (HumanSex r : this.getSex())
+      person.addSex(r);
 
     return person;
   }
