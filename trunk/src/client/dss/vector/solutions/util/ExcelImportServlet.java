aed156c1b1b716e972de689eb31d18d0080fbf5c
egrunzke@terraframe.com <egrunzke@terraframe.com>
2009-05-22 21:42:33 +0000

    Modified the ExcelImporter to create a two sheet response file.  The first sheet contains rows that failed to import and the second sheet contains error messages pertaining to the failed rows.  Successful imports forward to a success page instead of downloading the error file.
    git-svn-id: https://subversion.assembla.com/svn/mdss@1628 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/src/client/dss/vector/solutions/util/ExcelImportServlet.java
+++ trunk/src/client/dss/vector/solutions/util/ExcelImportServlet.java
@@ -6,22 +6,21 @@ import java.io.InputStream;
 import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
-import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
-import com.terraframe.mojo.ApplicationException;
 import com.terraframe.mojo.ProblemExceptionDTO;
-import com.terraframe.mojo.business.ProblemDTOIF;
 import com.terraframe.mojo.constants.ClientConstants;
 import com.terraframe.mojo.constants.ClientRequestIF;
 import com.terraframe.mojo.generation.loader.Reloadable;
+import com.terraframe.mojo.util.FileIO;
 
 public class ExcelImportServlet extends HttpServlet implements Reloadable
 {
@@ -49,21 +48,24 @@ public class ExcelImportServlet extends HttpServlet implements Reloadable
     {
       List<FileItem> items = upload.parseRequest(req);
       InputStream inputStream = new BufferedInputStream(items.get(0).getInputStream());
-      clientRequest.importExcelFile(inputStream);
+      InputStream errorStream = clientRequest.importExcelFile(inputStream);
+      if (errorStream.available()>0)
+      {
+        res.addHeader("Content-Disposition", "attachment;filename=\"errors.xls\"");
+        ServletOutputStream outputStream = res.getOutputStream();
+        FileIO.write(outputStream, errorStream);
+        return;
+      }
     }
     catch (ProblemExceptionDTO p)
     {
-      List<ProblemDTOIF> problems = p.getProblems();
-      req.setAttribute("problems", problems);
+      ErrorUtility.prepareProblems(p, req);
     }
     catch (Exception e)
     {
-      throw new ApplicationException(e);
+      ErrorUtility.prepareThrowable(e, req);
     }
-    finally
-    {
-      req.getRequestDispatcher("/WEB-INF/excelImportDone.jsp").forward(req, res);
-    }
+    req.getRequestDispatcher("/WEB-INF/excelImportDone.jsp").forward(req, res);
   }
   
   @Override
