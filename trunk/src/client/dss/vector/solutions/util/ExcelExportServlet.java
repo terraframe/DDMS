0b70d24cf5a43ecfd3f020c0068b5b13e079c105
egrunzke@terraframe.com <egrunzke@terraframe.com>
2009-07-24 22:54:30 +0000

    Better Excel Export error reporting.  Not awesome, but better.
    git-svn-id: https://subversion.assembla.com/svn/mdss@2314 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/src/client/dss/vector/solutions/util/ExcelExportServlet.java
+++ trunk/src/client/dss/vector/solutions/util/ExcelExportServlet.java
@@ -21,16 +21,25 @@ public class ExcelExportServlet extends HttpServlet implements Reloadable
   private static final long serialVersionUID = -4211492402501853068L;
 
   @Override
-  protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException
+  protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
   {
     ClientRequestIF clientRequest = (ClientRequestIF)req.getAttribute(ClientConstants.CLIENTREQUEST);
     String type = req.getParameter(ExcelImportServlet.TYPE);
 
     String[] split = type.split("\\.");
     String fileName = split[split.length-1];
-    InputStream inputStream = clientRequest.exportExcelFile(type, "setupExportListener", new String[]{});
-
-    FileDownloadUtil.writeXLS(res, fileName, inputStream);
+    try
+    {
+      InputStream inputStream = clientRequest.exportExcelFile(type, "setupExportListener", new String[]{});
+      
+      FileDownloadUtil.writeXLS(res, fileName, inputStream);
+    }
+    catch (Exception e)
+    {
+      ErrorUtility.prepareThrowable(e, req);
+      req.getRequestDispatcher("/WEB-INF/excelExportFail.jsp").forward(req, res);
+//      req.getRequestDispatcher(req.getHeader("referer")).forward(req, res);
+    }
   }
 
   @Override
