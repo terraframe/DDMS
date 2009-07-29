9c9b70fcaeeea5f7678798d978a57dbdbc4a6cea
darrellt@gmail.com <darrellt@gmail.com>
2009-07-29 02:27:56 +0000

    create temp table for zone spray
    git-svn-id: https://subversion.assembla.com/svn/mdss@2363 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/generated/server/stub/dss/vector/solutions/irs/ZoneSpray.java
+++ trunk/generated/server/stub/dss/vector/solutions/irs/ZoneSpray.java
@@ -1,5 +1,10 @@
 package dss.vector.solutions.irs;
 
+import com.terraframe.mojo.dataaccess.database.Database;
+import com.terraframe.mojo.system.metadata.MdBusiness;
+
+import dss.vector.solutions.Person;
+
 public class ZoneSpray extends ZoneSprayBase implements com.terraframe.mojo.generation.loader.Reloadable
 {
   private static final long serialVersionUID = 1240860676906L;
@@ -36,11 +41,11 @@ public class ZoneSpray extends ZoneSprayBase implements com.terraframe.mojo.gene
   public void populateView(ZoneSprayView view)
   {
     super.populateView(view);
-    
+
     view.setSupervisorName(this.getSupervisorName());
     view.setSupervisorSurname(this.getSupervisorSurname());
     view.setTarget(this.getTarget());
-    view.setSprayWeek(this.getSprayWeek());    
+    view.setSprayWeek(this.getSprayWeek());
   }
 
   @Override
@@ -52,6 +57,107 @@ public class ZoneSpray extends ZoneSprayBase implements com.terraframe.mojo.gene
   public static ZoneSprayView getView(String id)
   {
     return ZoneSpray.get(id).getView();
+  }
+
+  public static void createTempTable(String tableName,String activeIngredientView)
+  {
+    String sql = "DROP TABLE IF EXISTS " + tableName + ";\n";
+    sql += "CREATE TEMP TABLE " + tableName + " AS ";
+    sql += ZoneSpray.getTempTableSQL(activeIngredientView) + ";\n";
+    System.out.println(sql);
+    Database.parseAndExecute(sql);
+  }
+
+
+  public static String getTempTableSQL(String activeIngredientView)
+  {
+    String select = "SELECT spraystatus.id,\n";
+
+    select += "householdspraystatus."+HouseholdSprayStatus.HOUSEHOLDID+",\n";
+    select += "householdspraystatus."+HouseholdSprayStatus.STRUCTUREID+",\n";
+
+    select += "actorspray."+ActorSpray.TEAMLEADER+",\n";
+    select += "actorspray."+ActorSpray.TARGET+",\n";
+    select += "actorspray."+ActorSpray.RECEIVED+",\n";
+    select += "actorspray."+ActorSpray.RETURNED+",\n";
+    select += "actorspray."+ActorSpray.USED+",\n";
+    select += "actorspray."+ActorSpray.REFILLS+",\n";
+
+    select += "operatorspray."+OperatorSpray.SPRAYOPERATOR+",\n";
+    select += "sprayoperator.operatorid || ' - ' || person.firstname || ' - ' || person.lastname as sprayoperator_displaylabel,\n";
+    select += "operatorspray."+OperatorSpray.OPERATORSPRAYWEEK+",\n";
+
+    select += "spraydata."+SprayData.GEOENTITY+",\n";
+    select += "spraydata."+SprayData.SPRAYDATE+",\n";
+    select += "spraydata.spraymethod_c AS spraymethod,\n";
+    select += "metadatadisplaylabel_2.defaultLocale AS spraymethod_displaylabel,\n";
+    select += "spraydata."+SprayData.BRAND+",\n";
+    select += "spraydata.surfacetype_c AS surfacetype,\n";
+    select += "metadatadisplaylabel_1.defaultLocale AS surfacetype_displaylabel,\n";
+
+    select += "active_ingredient_per_can_view.brandname,\n";
+    select += "active_ingredient_per_can_view.active_ingredient_per_can,\n";
+
+    //--application rate is:
+    //--(# can refills * amount of active ingredient per can refill) / (# units sprayed *average size of unit).\n";
+    select += "(refills * active_ingredient_per_can_view.active_ingredient_per_can)/(spraystatus.rooms * areastandards.room) AS room_application_rate,\n";
+    select += "(refills * active_ingredient_per_can_view.active_ingredient_per_can)/(spraystatus.structures * areastandards.structurearea) AS structure_application_rate,\n";
+    select += "(refills * active_ingredient_per_can_view.active_ingredient_per_can)/(spraystatus.households * areastandards.household) AS household_application_rate,\n";
+    //--application ratio is:\n";
+    //--"--(# can refills (30)* ave m_ per can refill (10)*nozzle ratio (6)) / (total units sprayed (11, 12 or 13)*ave m_ per unit (38, 37 or 36))\n";
+    select += "(refills * areastandards.unitnozzleareacoverage * active_ingredient_per_can_view.nozzle_ratio) / (spraystatus.rooms * areastandards.room) AS room_application_ratio,\n";
+    select += "(refills * areastandards.unitnozzleareacoverage * active_ingredient_per_can_view.nozzle_ratio) / (spraystatus.rooms * areastandards.structurearea) AS structure_application_ratio,\n";
+    select += "(refills * areastandards.unitnozzleareacoverage * active_ingredient_per_can_view.nozzle_ratio) / (spraystatus.rooms * areastandards.household) AS household_application_ratio,\n";
+    //--operational coverage is:\n";
+    //--(Total units sprayed / Total units available) *100 (to calculate percentage of units sprayed). \n";
+
+    String from = " FROM ";
+    //get the main tables
+    from += MdBusiness.getMdBusiness(OperatorSpray.CLASS).getTableName() + " AS operatorspray, \n";
+    from += MdBusiness.getMdBusiness(ActorSpray.CLASS).getTableName() + " AS actorspray, \n";
+    from += MdBusiness.getMdBusiness(AbstractSpray.CLASS).getTableName() + " AS abstractspray,\n";
+    from += MdBusiness.getMdBusiness(HouseholdSprayStatus.CLASS).getTableName() + " AS householdspraystatus,\n";
+    from += MdBusiness.getMdBusiness(SprayStatus.CLASS).getTableName() + " AS spraystatus,\n";
+    from += MdBusiness.getMdBusiness(SprayData.CLASS).getTableName() + " AS spraydata,\n";
+    from += MdBusiness.getMdBusiness(SprayOperator.CLASS).getTableName() + " AS sprayoperator,\n";
+    from += MdBusiness.getMdBusiness(Person.CLASS).getTableName() + " AS person,\n";
+    from += MdBusiness.getMdBusiness(AreaStandards.CLASS).getTableName() + " AS areastandards,\n";
+    from += "surfacetype AS surfacetype,\n";
+    from += "surfacetypemaster AS surfacetypemaster,\n";
+    from += "spraymethod AS spraymethod,\n";
+    from += "spraymethodmaster AS spraymethodmaster,\n";
+    //get the enums
+    from += "enumeration_master AS enumeration_master_1\n";
+    from += " LEFT JOIN metadatadisplaylabel metadatadisplaylabel_1 ON enumeration_master_1.displayLabel = metadatadisplaylabel_1.id,\n";
+    from += "enumeration_master AS enumeration_master_2\n";
+    from += " LEFT JOIN metadatadisplaylabel metadatadisplaylabel_2 ON enumeration_master_2.displayLabel = metadatadisplaylabel_2.id,\n";
+    //get views
+    from += activeIngredientView + " AS active_ingredient_per_can_view,\n";
+
+    String where = "";
+    //join main tables
+    where += "AND spraydata.id = abstractspray.spraydata \n";
+    where += "AND abstractspray.id = actorspray.id \n";
+    where += "AND operatorspray.id = actorspray.id \n";
+    where += "AND householdspraystatus.id = spraystatus.id \n";
+    where += "AND spraystatus.spray = operatorspray.id \n";
+    where += "AND operatorspray.sprayoperator = sprayoperator.id \n";
+    where += "AND person.id = sprayoperator.person \n";
+    //join enums
+    where += "AND spraydata.surfacetype = surfacetype.set_id \n";
+    where += "AND surfacetypemaster.id = surfacetype.item_id \n";
+    where += "AND surfacetypemaster.id = enumeration_master_1.id \n";
+    where += "AND spraydata.spraymethod = spraymethod.set_id \n";
+    where += "AND spraymethodmaster.id = spraymethod.item_id \n";
+    where += "AND spraymethodmaster.id = enumeration_master_2.id \n";
+    //join views
+    where += "AND spraydata.brand = active_ingredient_per_can_view.id \n";
+
+    select = select.substring(0, select.length() - 2);
+    from = from.substring(0, from.length() - 2);
+    where = "WHERE " + where.substring(3, where.length() - 2);
+
+    return select + "\n" + from + "\n" + where;
   }
 
 }
