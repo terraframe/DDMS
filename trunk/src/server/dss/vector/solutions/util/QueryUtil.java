779be9bb0bbebe6b55b11ed5dfc78bf9ef85d660
darrellt@gmail.com <darrellt@gmail.com>
2009-07-30 21:30:12 +0000

    Fix join for map query when there are no specific restrictions
    git-svn-id: https://subversion.assembla.com/svn/mdss@2411 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/src/server/dss/vector/solutions/util/QueryUtil.java
+++ trunk/src/server/dss/vector/solutions/util/QueryUtil.java
@@ -34,6 +34,7 @@ import dss.vector.solutions.general.MalariaSeason;
 import dss.vector.solutions.geo.AllPaths;
 import dss.vector.solutions.geo.AllPathsQuery;
 import dss.vector.solutions.geo.GeoHierarchy;
+import dss.vector.solutions.geo.generated.Country;
 import dss.vector.solutions.geo.generated.GeoEntity;
 import dss.vector.solutions.geo.generated.GeoEntityQuery;
 import dss.vector.solutions.query.NoColumnsAddedException;
@@ -56,7 +57,7 @@ public class QueryUtil implements Reloadable
 
   private static final String START_DATE_RANGE  = "start_date_range";
 
-  private static final String RATIO  = "ratio_of_this_row_to_total_count";
+  private static final String RATIO             = "ratio_of_this_row_to_total_count";
 
   private static final String END_DATE_RANGE    = "end_date_range";
 
@@ -132,7 +133,7 @@ public class QueryUtil implements Reloadable
       valueQueryParser.addAttributeSelectable(thematicLayerType, GeoEntity.ENTITYNAME, GeoEntity.ENTITYNAME, QueryConstants.ENTITY_NAME_COLUMN);
 
       queryMap = valueQueryParser.parse();
-      
+
       // exclude any entity without spatial data
       Selectable geometrySelectable = valueQuery.getSelectable(attributeName);
       valueQuery.AND(geometrySelectable.NE(null));
@@ -141,42 +142,44 @@ public class QueryUtil implements Reloadable
       GeoEntityQuery geoEntityQuery = (GeoEntityQuery) queryMap.get(thematicLayerType);
       GeneratedEntityQuery generatedEntityQuery = queryMap.get(generatedQueryClass);
 
-      if(allPathsQuery == null)
+      if (allPathsQuery == null)
       {
-        //this case is for when they have not restricted to a specific geoEntity
+        // this case is for when they have not restricted to a specific
+        // geoEntity
         allPathsQuery = new AllPathsQuery(queryFactory);
 
-        //find all the parents that are of the type of the thematicLayer
+        // find all the parents that are of the type of the thematicLayer
         valueQuery.WHERE(allPathsQuery.getParentGeoEntity().EQ(geoEntityQuery));
 
-        //find all the rows where the children match geoEntities of the children we are looking for.
+        // find all the rows where the children match geoEntities of the
+        // children we are looking for.
         valueQuery.AND( ( (AttributeReference) generatedEntityQuery.aAttribute(geoEntityAttribute) ).EQ(allPathsQuery.getChildGeoEntity()));
-
 
       }
       else
       {
 
-        //This is the case for where we are restricting by one or more entitys
+        // This is the case for where we are restricting by one or more entitys
 
-        //first we make a seond all paths query
+        // first we make a seond all paths query
         AllPathsQuery allPathsParent = new AllPathsQuery(queryFactory);
 
-        //we narrow down the secoond all paths table to just rows that match entity restrictions
+        // we narrow down the secoond all paths table to just rows that match
+        // entity restrictions
         valueQuery.WHERE(allPathsParent.getParentGeoEntity().EQ(geoEntityQuery));
-        //we join the second all paths to the primary all paths
+        // we join the second all paths to the primary all paths
         valueQuery.WHERE(allPathsParent.getChildGeoEntity().EQ(allPathsQuery.getChildGeoEntity()));
-        //we join the primary all paths to the md business  which is located at a certain geoEntity
+        // we join the primary all paths to the md business which is located at
+        // a certain geoEntity
         valueQuery.AND( ( (AttributeReference) generatedEntityQuery.aAttribute(geoEntityAttribute) ).EQ(allPathsQuery.getChildGeoEntity()));
 
       }
-      
+
     }
     else
     {
       // Normal query (non-mapping)
       List<ValueQuery> leftJoinValueQueries = new LinkedList<ValueQuery>();
-      List<SelectableReference> joinGeoSelctables= new LinkedList<SelectableReference>();
       for (String selectedGeoEntityType : selectedUniversals)
       {
         GeoEntityQuery geoEntityQuery = new GeoEntityQuery(queryFactory);
@@ -190,8 +193,6 @@ public class QueryUtil implements Reloadable
         SelectableReference selectable3 = subAllPathsQuery.getChildGeoEntity("child_id");
 
         geoEntityVQ.SELECT(selectable1, selectable2, selectable3);
-
-        joinGeoSelctables.add((SelectableReference) geoEntityVQ.aAttribute(selectable3.getUserDefinedAlias()));
 
         List<MdBusinessDAOIF> allClasses = geoEntityMd.getAllSubClasses();
         Condition[] geoConditions = new Condition[allClasses.size()];
@@ -212,9 +213,17 @@ public class QueryUtil implements Reloadable
 
       AllPathsQuery allPathsQuery = (AllPathsQuery) queryMap.get(AllPaths.CLASS);
 
+      if (allPathsQuery == null)
+      {
+        // This case is for when they have not restricted by any specific  geoEntity
+        allPathsQuery = new AllPathsQuery(queryFactory);
+        valueQuery.FROM(allPathsQuery);
+        // we use the country universial to restrict the cross product
+        valueQuery.AND(allPathsQuery.getParentUniversal().EQ(MdBusiness.getMdBusiness(Country.CLASS).getId()));
+      }
       if (allPathsQuery != null)
       {
-        //this case is for when they have restricted to a specific geoEntity
+        // this case is for when they have restricted to a specific geoEntity
         List<SelectableSingle> leftJoinSelectables = new LinkedList<SelectableSingle>();
         for (ValueQuery leftJoinVQ : leftJoinValueQueries)
         {
@@ -230,16 +239,6 @@ public class QueryUtil implements Reloadable
         GeneratedEntityQuery generatedEntityQuery = queryMap.get(generatedQueryClass);
         valueQuery.AND( ( (AttributeReference) generatedEntityQuery.aAttribute(geoEntityAttribute) ).EQ(allPathsQuery.getChildGeoEntity()));
       }
-      else
-      {
-        //This case is for when they have not restricted by any specific geoEntity
-        GeneratedEntityQuery generatedEntityQuery = queryMap.get(generatedQueryClass);
-        for (SelectableReference geoSelectable : joinGeoSelctables)
-        {
-          // join the subselect to prevent cross products
-          valueQuery.AND( ((SelectableReference)generatedEntityQuery.aAttribute(geoEntityAttribute)).EQ(geoSelectable));
-        }
-      }
 
     }
 
@@ -248,15 +247,17 @@ public class QueryUtil implements Reloadable
 
   public static ValueQuery setQueryDates(String xml, ValueQuery valueQuery, SelectableMoment dateAttribute)
   {
-
     String da = dateAttribute.getQualifiedName();
+    return setQueryDates(xml, valueQuery, da);
+  }
 
+  public static ValueQuery setQueryDates(String xml, ValueQuery valueQuery, String da)
+  {
     if (xml.indexOf(DATEGROUP_SEASON) > 0)
     {
       SelectableSQLCharacter dateGroup = (SelectableSQLCharacter) valueQuery.getSelectable(DATEGROUP_SEASON);
       String table = MdBusiness.getMdBusiness(MalariaSeason.CLASS).getTableName();
-      dateGroup.setSQL("SELECT " + MalariaSeason.SEASONNAME + " FROM " + table + " AS ms "
-      + " WHERE ms." + MalariaSeason.STARTDATE + " < " + da + " AND ms." + MalariaSeason.ENDDATE + " > " + da);
+      dateGroup.setSQL("SELECT " + MalariaSeason.SEASONNAME + " FROM " + table + " AS ms " + " WHERE ms." + MalariaSeason.STARTDATE + " < " + da + " AND ms." + MalariaSeason.ENDDATE + " > " + da);
     }
 
     if (xml.indexOf(DATEGROUP_EPIWEEK) > 0)
@@ -295,26 +296,23 @@ public class QueryUtil implements Reloadable
       SelectableSQLCharacter dateGroup = (SelectableSQLCharacter) valueQuery.getSelectable(DATEGROUP_SEASON);
 
       String table = MdBusiness.getMdBusiness(MalariaSeason.CLASS).getTableName();
-      dateGroup.setSQL("SELECT " + MalariaSeason.SEASONNAME + " FROM " + table + " AS ms"
-      + " WHERE ms." + MalariaSeason.STARTDATE + " < " + sd + " AND ms." + MalariaSeason.ENDDATE + " > " + ed);
+      dateGroup.setSQL("SELECT " + MalariaSeason.SEASONNAME + " FROM " + table + " AS ms" + " WHERE ms." + MalariaSeason.STARTDATE + " < " + sd + " AND ms." + MalariaSeason.ENDDATE + " > " + ed);
     }
 
     if (xml.indexOf(DATEGROUP_EPIWEEK) > 0)
     {
       SelectableSQLCharacter dateGroup = (SelectableSQLCharacter) valueQuery.getSelectable(DATEGROUP_EPIWEEK);
 
-      String dateGroupSql = "CASE WHEN (" + sd + " + interval '7 days') < " + ed + "  THEN '" + intervalNotValid + "'"
-      + "WHEN (extract(Day FROM " + sd + ") - extract(DOW FROM date_trunc('week'," + ed + "))) > extract(DOW FROM " + ed + ")"
-      + "THEN to_char(" + sd + ",'IW')" + "ELSE to_char(" + ed + ",'IW') END";
+      String dateGroupSql = "CASE WHEN (" + sd + " + interval '7 days') < " + ed + "  THEN '" + intervalNotValid + "'" + "WHEN (extract(Day FROM " + sd + ") - extract(DOW FROM date_trunc('week'," + ed + "))) > extract(DOW FROM " + ed + ")"
+          + "THEN to_char(" + sd + ",'IW')" + "ELSE to_char(" + ed + ",'IW') END";
       dateGroup.setSQL(dateGroupSql);
     }
 
     if (xml.indexOf(DATEGROUP_MONTH) > 0)
     {
       SelectableSQLCharacter dateGroup = (SelectableSQLCharacter) valueQuery.getSelectable(DATEGROUP_MONTH);
-      String dateGroupSql = "CASE WHEN (" + sd + " + interval '1 month') < " + ed + "  THEN '" + intervalNotValid + "'"
-      + "WHEN (extract(DAY FROM " + sd + ") - extract(DAY FROM date_trunc('month'," + ed + "))) > extract(DAY FROM " + ed + ")"
-      + "THEN to_char(" + sd + ",'MM')" + "ELSE to_char(" + ed + ",'MM') END";
+      String dateGroupSql = "CASE WHEN (" + sd + " + interval '1 month') < " + ed + "  THEN '" + intervalNotValid + "'" + "WHEN (extract(DAY FROM " + sd + ") - extract(DAY FROM date_trunc('month'," + ed + "))) > extract(DAY FROM " + ed + ")"
+          + "THEN to_char(" + sd + ",'MM')" + "ELSE to_char(" + ed + ",'MM') END";
       dateGroup.setSQL(dateGroupSql);
     }
 
@@ -322,18 +320,16 @@ public class QueryUtil implements Reloadable
     {
       SelectableSQLCharacter dateGroup = (SelectableSQLCharacter) valueQuery.getSelectable(DATEGROUP_QUARTER);
 
-      String dateGroupSql = "CASE WHEN (" + sd + " + interval '3 months') < " + ed + "  THEN '" + intervalNotValid + "'"
-      + "WHEN (extract(DOY FROM " + sd + ") - extract(DOY FROM date_trunc('quarter'," + ed + ")))" + " >  (extract(DOY FROM " + ed
-      + ") - extract(DOY FROM date_trunc('quarter'," + ed + ")))" + "THEN to_char(" + sd + ",'Q')" + "ELSE to_char(" + ed + ",'Q') END";
+      String dateGroupSql = "CASE WHEN (" + sd + " + interval '3 months') < " + ed + "  THEN '" + intervalNotValid + "'" + "WHEN (extract(DOY FROM " + sd + ") - extract(DOY FROM date_trunc('quarter'," + ed + ")))" + " >  (extract(DOY FROM " + ed
+          + ") - extract(DOY FROM date_trunc('quarter'," + ed + ")))" + "THEN to_char(" + sd + ",'Q')" + "ELSE to_char(" + ed + ",'Q') END";
       dateGroup.setSQL(dateGroupSql);
     }
 
     if (xml.indexOf(DATEGROUP_YEAR) > 0)
     {
       SelectableSQLCharacter dateGroup = (SelectableSQLCharacter) valueQuery.getSelectable(DATEGROUP_YEAR);
-      String dateGroupSql = "CASE WHEN (" + sd + " + interval '1 year') < " + ed + "  THEN '" + intervalNotValid + "'"
-      + "WHEN (extract(DOY FROM " + sd + ") - extract(DOY FROM date_trunc('year'," + ed + ")))" + " >  (extract(DOY FROM " + ed
-      + ") - extract(DOY FROM date_trunc('year'," + ed + ")))" + "THEN to_char(" + sd + ",'YYYY')" + "ELSE to_char(" + ed + ",'YYYY') END";
+      String dateGroupSql = "CASE WHEN (" + sd + " + interval '1 year') < " + ed + "  THEN '" + intervalNotValid + "'" + "WHEN (extract(DOY FROM " + sd + ") - extract(DOY FROM date_trunc('year'," + ed + ")))" + " >  (extract(DOY FROM " + ed
+          + ") - extract(DOY FROM date_trunc('year'," + ed + ")))" + "THEN to_char(" + sd + ",'YYYY')" + "ELSE to_char(" + ed + ",'YYYY') END";
       dateGroup.setSQL(dateGroupSql);
     }
 
@@ -379,15 +375,15 @@ public class QueryUtil implements Reloadable
       SelectableSQL ratio = (SelectableSQL) valueQuery.getSelectable(RATIO);
       Double sum = 0.0;
 
-      //specieRatio.setSQL("");
+      // specieRatio.setSQL("");
       ratio.setSQL(countSql);
 
-      for(ValueObject v :valueQuery.getIterator())
+      for (ValueObject v : valueQuery.getIterator())
       {
         sum += Double.parseDouble(v.getValue(RATIO));
       }
 
-      ratio.setSQL("to_char("+countSql+"/"+sum+",'99.99')");
+      ratio.setSQL("to_char(" + countSql + "/" + sum + ",'99.99')");
     }
 
     return valueQuery;
