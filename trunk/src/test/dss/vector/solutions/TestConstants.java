0970e45e9f75a026d17086922a4ed11dd6bb1299
jsmethie@terraframe.com <jsmethie@terraframe.com>
2009-09-08 15:59:44 +0000

    -Updated TestConstants
    git-svn-id: https://subversion.assembla.com/svn/mdss@2615 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: ddf0462f92954600bc9ab6815bcb814ab2b9ba6e

--- trunk/src/test/dss/vector/solutions/TestConstants.java
+++ trunk/src/test/dss/vector/solutions/TestConstants.java
@@ -4,45 +4,45 @@ public class TestConstants
 {
   public static final String PASSWORD       = "p4eWR%prafr=ju4a";
 
-  public static String       LEADER_ID      = "9718301-4513";
+  public static String       LEADER_ID      = "19718301-4513";
 
-  public static String       OPERATOR_ID    = "7918301-4731";
+  public static String       OPERATOR_ID    = "71918301-4731";
 
-  public static String       OPERATOR_ID_2  = "7918301-47312";
+  public static String       OPERATOR_ID_2  = "79118301-47312";
 
-  public static String       TEAM_ID        = "71930-4631";
+  public static String       TEAM_ID        = "719130-4631";
 
-  public static String       TEAM_ID_2      = "718930-464";
+  public static String       TEAM_ID_2      = "7189130-464";
 
-  public static String       BRAND_NAME     = "71309-1332";
+  public static String       BRAND_NAME     = "713091-1332";
 
-  public static String       BRAND_NAME_2   = "7130-91223";
+  public static String       BRAND_NAME_2   = "7130-911223";
 
-  public static String       BRAND_NAME_3   = "7130-19224";
+  public static String       BRAND_NAME_3   = "7130-191224";
 
-  public static String       NOZZLE_NAME    = "7110-49932";
+  public static String       NOZZLE_NAME    = "7110-499132";
 
-  public static String       NOZZLE_NAME_2  = "7101-48392";
+  public static String       NOZZLE_NAME_2  = "7101-483912";
 
-  public static String       SEASON_NAME    = "710-141329";
+  public static String       SEASON_NAME    = "710-1413291";
 
-  public static String       SEASON_NAME_2  = "710-411829";
+  public static String       SEASON_NAME_2  = "710-4118291";
 
-  public static String       TERM_NAME      = "97130-41128";
+  public static String       TERM_NAME      = "97130-411281";
 
-  public static String       TERM_NAME_2    = "79130-42218";
+  public static String       TERM_NAME_2    = "179130-42218";
 
-  public static String       TERM_NAME_3    = "71930-42821";
+  public static String       TERM_NAME_3    = "711930-42821";
 
-  public static String       GEO_ID         = "71291";
+  public static String       GEO_ID         = "711291";
 
-  public static String       GEO_ID_2       = "79219";
+  public static String       GEO_ID_2       = "792119";
 
-  public static String       HOUSEHOLD_ID   = "972459484";
+  public static String       HOUSEHOLD_ID   = "9724159484";
 
-  public static String       STRUCTURE_ID   = "7944-045842";
+  public static String       STRUCTURE_ID   = "7944-1045842";
 
-  public static String       STRUCTURE_ID_2 = "72945948";
+  public static String       STRUCTURE_ID_2 = "729459148";
 
   public static Integer      NUM_ROOMS      = 5;
 
