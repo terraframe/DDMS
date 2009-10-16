3f027c9ac0c9af1239a82db4b2fba089fd6dd0dc
darrellt@gmail.com <darrellt@gmail.com>
2009-10-16 09:23:29 +0000

    Queery Builder class to remove duplicate data
    git-svn-id: https://subversion.assembla.com/svn/mdss@2837 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 7682589ed4fcfff7dc5e871e267cae7ef9f71924

---
+++ trunk/generated/server/stub/dss/vector/solutions/query/QueryBuilder.java
@@ -0,0 +1,186 @@
+package dss.vector.solutions.query;
+
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import com.terraframe.mojo.business.rbac.Authenticate;
+import com.terraframe.mojo.dataaccess.transaction.Transaction;
+import com.terraframe.mojo.query.ValueQuery;
+import com.terraframe.mojo.query.ValueQueryCSVExporter;
+import com.terraframe.mojo.query.ValueQueryExcelExporter;
+
+import dss.vector.solutions.util.QueryConfig;
+
+public class QueryBuilder extends QueryBuilderBase implements com.terraframe.mojo.generation.loader.Reloadable
+{
+  private static final long serialVersionUID = 1255379414351L;
+  
+  public QueryBuilder()
+  {
+    super();
+  }
+  
+  private static ValueQuery getValueQuery(String queryClass, String queryXML, String[] selectedUniversals, Boolean includeGeometry, ThematicLayer thematicLayer)
+  {
+    
+    Class<?> clazz = null;
+    try
+    {
+      clazz = Class.forName(queryClass);
+    }
+    catch (ClassNotFoundException e1)
+    {
+      // TODO Auto-generated catch block
+      e1.printStackTrace();
+    }
+    Method xmlToValueQuery = null;
+    try
+    {
+      xmlToValueQuery = clazz.getMethod("xmlToValueQuery",String.class, String[].class, Boolean.class, ThematicLayer.class );
+    }
+    catch (SecurityException e1)
+    {
+      // TODO Auto-generated catch block
+      e1.printStackTrace();
+    }
+    catch (NoSuchMethodException e1)
+    {
+      // TODO Auto-generated catch block
+      e1.printStackTrace();
+    }
+
+    ValueQuery valueQuery = null;
+    try
+    {
+      valueQuery = (ValueQuery) xmlToValueQuery.invoke(clazz, queryXML, selectedUniversals, includeGeometry, thematicLayer);
+    }
+    catch (IllegalArgumentException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    catch (IllegalAccessException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    catch (InvocationTargetException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+
+    System.out.println(valueQuery.getSQL());
+
+    return valueQuery;
+  }
+  
+  
+  /**
+   * Queries 
+   *
+   * @param xml
+   */
+  @Transaction
+  @Authenticate
+  public static com.terraframe.mojo.query.ValueQuery getQueryResults(String queryClass, String queryXML, String config, String sortBy, Boolean ascending, Integer pageNumber, Integer pageSize)
+  {
+    
+    QueryConfig queryConfig = new QueryConfig(config);
+    String[] selectedUniversals = queryConfig.getSelectedUniversals();
+
+    ValueQuery valueQuery = getValueQuery(queryClass, queryXML, selectedUniversals, false, null);
+
+    valueQuery.restrictRows(pageSize, pageNumber);
+
+    System.out.println(valueQuery.getSQL());
+
+    return valueQuery;
+  }
+
+  @Transaction
+  public static String mapQuery(String queryClass, String xml, String config, String[] universalLayers, String savedSearchId)
+  {
+    if (savedSearchId == null || savedSearchId.trim().length() == 0)
+    {
+      String error = "Cannot map a query without a current SavedSearch instance.";
+      SavedSearchRequiredException ex = new SavedSearchRequiredException(error);
+      throw ex;
+    }
+
+    SavedSearch search = SavedSearch.get(savedSearchId);
+    QueryConfig queryConfig = new QueryConfig(config);
+
+    ThematicLayer thematicLayer = search.getThematicLayer();
+
+    if (thematicLayer == null || thematicLayer.getGeoHierarchy() == null)
+    {
+      String error = "Cannot create a map for search [" + search.getQueryName()
+          + "] without having selected a thematic layer.";
+      NoThematicLayerException ex = new NoThematicLayerException(error);
+      throw ex;
+    }
+
+    // Update ThematicLayer if the thematic layer type has changed or
+    // if one has not yet been defined.
+    String thematicLayerType = thematicLayer.getGeoHierarchy().getGeoEntityClass().definesType();
+    if (thematicLayer.getGeometryStyle() == null
+        || !thematicLayer.getGeoHierarchy().getQualifiedType().equals(thematicLayerType))
+    {
+      thematicLayer.changeLayerType(thematicLayerType);
+    }
+
+    String[] selectedUniversals = queryConfig.getSelectedUniversals();
+    ValueQuery query = getValueQuery(queryClass,xml, selectedUniversals, true, thematicLayer);
+
+    System.out.println(query.getSQL());
+
+    String layers = MapUtil.generateLayers(universalLayers, query, search, thematicLayer);
+
+    return layers;
+  }
+
+
+  @Transaction
+  public static InputStream exportQueryToExcel(String queryClass,String queryXML, String config, String savedSearchId)
+  {
+    QueryConfig queryConfig = new QueryConfig(config);
+    String[] selectedUniversals = queryConfig.getSelectedUniversals();
+
+    if (savedSearchId == null || savedSearchId.trim().length() == 0)
+    {
+      String error = "Cannot export to Excel without a current SavedSearch instance.";
+      SavedSearchRequiredException ex = new SavedSearchRequiredException(error);
+      throw ex;
+    }
+
+    SavedSearch search = SavedSearch.get(savedSearchId);
+
+    ValueQuery query = getValueQuery(queryClass, queryXML, selectedUniversals, false, null);
+
+    ValueQueryExcelExporter exporter = new ValueQueryExcelExporter(query, search.getQueryName());
+    return exporter.exportStream();
+  }
+
+  @Transaction
+  public static InputStream exportQueryToCSV(String queryClass, String queryXML, String config, String savedSearchId)
+  {
+    QueryConfig queryConfig = new QueryConfig(config);
+    String[] selectedUniversals = queryConfig.getSelectedUniversals();
+
+    if (savedSearchId == null || savedSearchId.trim().length() == 0)
+    {
+      String error = "Cannot export to CSV without a current SavedSearch instance.";
+      SavedSearchRequiredException ex = new SavedSearchRequiredException(error);
+      throw ex;
+    }
+
+    ValueQuery query = getValueQuery(queryClass, queryXML, selectedUniversals, false, null);
+
+    ValueQueryCSVExporter exporter = new ValueQueryCSVExporter(query);
+    return exporter.exportStream();
+  }
+  
+}
