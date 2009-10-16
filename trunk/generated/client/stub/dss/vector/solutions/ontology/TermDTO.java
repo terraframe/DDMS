c36e02e99292c2d29cd2cfd70c9563f5df090b72
jsmethie@terraframe.com <jsmethie@terraframe.com>
2009-10-16 19:01:08 +0000

    -Added a space between term name and term id on display label for TermDTO
    git-svn-id: https://subversion.assembla.com/svn/mdss@2864 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: a63cf130bbcec161a2149f5483785e4cc5558a1e

--- trunk/generated/client/stub/dss/vector/solutions/ontology/TermDTO.java
+++ trunk/generated/client/stub/dss/vector/solutions/ontology/TermDTO.java
@@ -22,6 +22,6 @@ public abstract class TermDTO extends TermDTOBase
   
   public String getDisplayLabel()
   {
-    return this.getTermName() + "(" + this.getTermId() + ")";
+    return this.getTermName() + " (" + this.getTermId() + ")";
   }
 }
