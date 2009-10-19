dbc197bca50081de08f3bace4c7b64d601f0a6dc
darrellt@gmail.com <darrellt@gmail.com>
2009-10-19 02:56:23 +0000

    git-svn-id: https://subversion.assembla.com/svn/mdss@2892 e0e46c49-be69-4f5a-ad62-21024a331aea
    Former-commit-id: ae022636eed51b1aad82e720aaa74a536d60b333

--- trunk/generated/server/stub/dss/vector/solutions/irs/SprayStatus.java
+++ trunk/generated/server/stub/dss/vector/solutions/irs/SprayStatus.java
@@ -324,6 +324,10 @@ public class SprayStatus extends SprayStatusBase implements com.terraframe.mojo.
     select += "(CAST(refills AS float) * active_ingredient_per_can_view.active_ingredient_per_can * (spraystatus.sprayedstructures/structure_total)) / NULLIF(spraystatus.sprayedstructures * areastandards.structurearea, 0) AS structure_application_rate,\n";
     select += "(CAST(refills AS float) * active_ingredient_per_can_view.active_ingredient_per_can * (spraystatus.sprayedhouseholds/ household_total)) / NULLIF(spraystatus.sprayedhouseholds * areastandards.household, 0) AS household_application_rate,\n";
 
+    select += "(CAST(refills AS float) * 1000.0 * active_ingredient_per_can_view.active_ingredient_per_can * (spraystatus.sprayedrooms / room_total)) / NULLIF(spraystatus.sprayedrooms * areastandards.room , 0) AS room_application_rate_mg,\n";
+    select += "(CAST(refills AS float) * 1000.0 * active_ingredient_per_can_view.active_ingredient_per_can * (spraystatus.sprayedstructures/structure_total)) / NULLIF(spraystatus.sprayedstructures * areastandards.structurearea, 0) AS structure_application_rate_mg,\n";
+    select += "(CAST(refills AS float) * 1000.0 * active_ingredient_per_can_view.active_ingredient_per_can * (spraystatus.sprayedhouseholds/ household_total)) / NULLIF(spraystatus.sprayedhouseholds * areastandards.household, 0) AS household_application_rate_mg,\n";
+    
     // --application ratio is:\n";
     // --"--(# can refills (30)* ave m_ per can refill (10)*nozzle ratio (6)) / (total units sprayed (11, 12 or 13)*ave m_ per unit (38, 37 or 36))\n";
     select += "((CAST(refills AS float) * (spraystatus.sprayedrooms / room_total)) * areastandards.unitnozzleareacoverage * active_ingredient_per_can_view.nozzle_ratio) / NULLIF(spraystatus.sprayedrooms      * areastandards.room, 0) AS room_application_ratio,\n";
