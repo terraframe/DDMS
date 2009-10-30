b17a4b7a58acc677c0ac43600314830ab381fa18
egrunzke@terraframe.com <egrunzke@terraframe.com>
2009-10-30 16:44:33 +0000

    Removed an errant constructor that was left behind from a typo in the xml definition
    git-svn-id: https://subversion.assembla.com/svn/mdss@2970 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 32b7d1e5a260580bc438b6f1c7701b97eb181d42

--- trunk/generated/client/stub/dss/vector/solutions/export/ITNCommunityDistributionExcelViewDTO.java
+++ trunk/generated/client/stub/dss/vector/solutions/export/ITNCommunityDistributionExcelViewDTO.java
@@ -9,15 +9,4 @@ public class ITNCommunityDistributionExcelViewDTO extends ITNCommunityDistributi
     super(clientRequest);
   }
   
-  /**
-  * Copy Constructor: Duplicates the values and attributes of the given BusinessDTO into a new DTO.
-  * 
-  * @param businessDTO The BusinessDTO to duplicate
-  * @param clientRequest The clientRequest this DTO should use to communicate with the server.
-  */
-  protected ITNCommunityDistributionExcelViewDTO(com.terraframe.mojo.business.BusinessDTO businessDTO, com.terraframe.mojo.constants.ClientRequestIF clientRequest)
-  {
-    super(businessDTO, clientRequest);
-  }
-  
 }
