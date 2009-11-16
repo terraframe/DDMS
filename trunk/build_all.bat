8066b0728f5a11560b675b396d1032d61bb9593b
darrellt@gmail.com <darrellt@gmail.com>
2009-11-16 19:05:23 +0000

    cd build script
    git-svn-id: https://subversion.assembla.com/svn/mdss@3185 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: b4262324498eceea525a458687c31f9dbb3879c6

---
+++ trunk/build_all.bat
@@ -0,0 +1,3 @@
+rem ant -buildfile cc-build.xml -Dcountry_uppercase=Mozambique -Dcountry=mozambique
+ant -buildfile cc-build.xml -Dcountry_uppercase=Zambia -Dcountry=zambia
+rem ant -buildfile cc-build.xml -Dcountry_uppercase=Malawi -Dcountry=malawi
