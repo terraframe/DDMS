484696e6efa218ccd14a2a5596462a13b022af5d
egrunzke@terraframe.com <egrunzke@terraframe.com>
2009-12-04 20:05:16 +0000

    Added wrapper for access to MDSS.properties that fixes an occasional problem where the bundle could not be found.
    git-svn-id: https://subversion.assembla.com/svn/mdss@3346 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 490803d53b076471a7bbf1ec7e61281be5410825

--- trunk/src/common/dss/vector/solutions/util/MDSSProperties.java
+++ trunk/src/common/dss/vector/solutions/util/MDSSProperties.java
@@ -5,15 +5,21 @@ import java.util.ResourceBundle;
 
 import com.terraframe.mojo.business.Business;
 
+/**
+ * A wrapper for access to the MDSS.properties bundle, this fixes isolated
+ * scenarios where the system would be unable to find the bundle
+ * 
+ * @author Eric
+ */
 public class MDSSProperties
 {
   private static ResourceBundle bundle = ResourceBundle.getBundle("MDSS", Locale.getDefault(), Business.class.getClassLoader());
-  
+
   public static String getString(String key)
   {
     return bundle.getString(key);
   }
-  
+
   public static Object getObject(String key)
   {
     return bundle.getObject(key);
