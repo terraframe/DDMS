8e86d04baec4ea06f744beffd0ad12846d0d2a03
jsmethie@terraframe.com <jsmethie@terraframe.com>
2009-12-10 19:34:16 +0000

    -Back end check for future dates in MosquitoCollection
    git-svn-id: https://subversion.assembla.com/svn/mdss@3388 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 69fd6ba28d864d7d318aef346608025137867c33

--- trunk/generated/server/stub/dss/vector/solutions/entomology/MosquitoCollection.java
+++ trunk/generated/server/stub/dss/vector/solutions/entomology/MosquitoCollection.java
@@ -1,11 +1,13 @@
 package dss.vector.solutions.entomology;
 
+import java.util.Date;
 import java.util.List;
 
 import com.terraframe.mojo.dataaccess.transaction.Transaction;
 import com.terraframe.mojo.query.OIterator;
 import com.terraframe.mojo.query.QueryFactory;
 
+import dss.vector.solutions.CurrentDateProblem;
 import dss.vector.solutions.Property;
 import dss.vector.solutions.entomology.assay.CollectionAssay;
 import dss.vector.solutions.entomology.assay.CollectionAssayQuery;
@@ -13,79 +15,79 @@ import dss.vector.solutions.entomology.assay.CollectionAssayQuery;
 public class MosquitoCollection extends MosquitoCollectionBase implements com.terraframe.mojo.generation.loader.Reloadable
 {
   private static final long serialVersionUID = 1894808272;
-  
+
   public MosquitoCollection()
   {
     super();
   }
-  
+
   @Override
   protected String buildKey()
   {
     return this.getId();
   }
-  
+
   @Override
   @Transaction
   public void delete()
   {
     // Delete all of the sub collections
     MosquitoCollectionView view = this.getView();
-    
+
     SubCollectionView[] collections = view.getSubCollections();
-    
-    for(SubCollectionView collection : collections)
+
+    for (SubCollectionView collection : collections)
     {
       collection.deleteConcrete();
     }
-    
+
     // Delete all of the Assays
     List<? extends CollectionAssay> list = this.getAssays();
-    
-    for(CollectionAssay assay : list)
+
+    for (CollectionAssay assay : list)
     {
       assay.delete();
     }
-    
+
     // Delete all infection and pooled infection assays
     InfectionAssayView[] infectionAssays = view.getInfectionAssays();
-    
-    for(InfectionAssayView assay : infectionAssays)
+
+    for (InfectionAssayView assay : infectionAssays)
     {
       assay.deleteConcrete();
     }
-    
+
     PooledInfectionAssayView[] pooledInfectionAssays = view.getPooledInfectionAssays();
-    
-    for(PooledInfectionAssayView assay : pooledInfectionAssays)
+
+    for (PooledInfectionAssayView assay : pooledInfectionAssays)
     {
       assay.deleteConcrete();
-    }    
-    
+    }
+
     // DELETE ALL MECHANISM ASSAYS
     BiochemicalAssayView[] biochemicalAssays = view.getBiochemicalAssays();
-    
-    for(BiochemicalAssayView assay : biochemicalAssays)
+
+    for (BiochemicalAssayView assay : biochemicalAssays)
     {
       assay.deleteConcrete();
     }
-        
+
     MolecularAssayView[] molecularAssays = view.getMolecularAssays();
-    
-    for(MolecularAssayView assay : molecularAssays)
+
+    for (MolecularAssayView assay : molecularAssays)
     {
       assay.deleteConcrete();
     }
-    
+
     super.delete();
   }
-  
+
   private List<? extends CollectionAssay> getAssays()
   {
-    CollectionAssayQuery query = new CollectionAssayQuery(new QueryFactory());    
+    CollectionAssayQuery query = new CollectionAssayQuery(new QueryFactory());
     query.WHERE(query.getCollection().EQ(this));
     OIterator<? extends CollectionAssay> it = query.getIterator();
-    
+
     try
     {
       return it.getAll();
@@ -95,19 +97,36 @@ public class MosquitoCollection extends MosquitoCollectionBase implements com.te
       it.close();
     }
   }
-  
+
   @Override
   public void apply()
   {
     this.populateCollectionId();
     this.populateLifeStageName();
-    
+    this.validateCollectionDate();
+
     super.apply();
+  }
+
+  @Override
+  public void validateCollectionDate()
+  {
+    if (this.getCollectionDate() != null && this.getCollectionDate().after(new Date()))
+    {
+      String msg = "It is impossible to have a test date after the current date";
+
+      CurrentDateProblem p = new CurrentDateProblem(msg);
+      p.setGivenDate(this.getCollectionDate());
+      p.setCurrentDate(new Date());
+      p.setNotification(this, COLLECTIONDATE);
+      p.apply();
+      p.throwIt();
+    }
   }
 
   private void populateLifeStageName()
   {
-    for(LifeStage stage  : this.getLifeStage())
+    for (LifeStage stage : this.getLifeStage())
     {
       this.setLifeStageName(stage.getEnumName());
     }
@@ -115,7 +134,7 @@ public class MosquitoCollection extends MosquitoCollectionBase implements com.te
 
   private void populateCollectionId()
   {
-    if(this.getCollectionId() == null || this.getCollectionId().equals(""))
+    if (this.getCollectionId() == null || this.getCollectionId().equals(""))
     {
       this.setCollectionId(Property.getNextId());
     }
@@ -147,6 +166,5 @@ public class MosquitoCollection extends MosquitoCollectionBase implements com.te
 
     return view;
   }
-  
 
 }
