356520736132b159277df81be65ec657edd4da6a
darrellt@gmail.com <darrellt@gmail.com>
2009-12-17 23:40:56 +0000

    Rework Query Entomology
    git-svn-id: https://subversion.assembla.com/svn/mdss@3479 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 9f190639b68dec6f8230a5d3e6274942adefe71b

--- trunk/generated/server/stub/dss/vector/solutions/entomology/InfectionAssay.java
+++ trunk/generated/server/stub/dss/vector/solutions/entomology/InfectionAssay.java
@@ -1,9 +1,20 @@
 package dss.vector.solutions.entomology;
 
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.terraframe.mojo.dataaccess.ProgrammingErrorException;
 import com.terraframe.mojo.dataaccess.transaction.Transaction;
+import com.terraframe.mojo.query.GeneratedEntityQuery;
+import com.terraframe.mojo.query.QueryFactory;
+import com.terraframe.mojo.query.ValueQuery;
 
 import dss.vector.solutions.Property;
 import dss.vector.solutions.RangeValueProblem;
+import dss.vector.solutions.intervention.monitor.IndividualIPT;
+import dss.vector.solutions.util.QueryUtil;
 
 public class InfectionAssay extends InfectionAssayBase implements com.terraframe.mojo.generation.loader.Reloadable
 {
@@ -96,4 +107,77 @@ public class InfectionAssay extends InfectionAssayBase implements com.terraframe
 
     return view;
   }
+  
+  /**
+   * Takes in an XML string and returns a ValueQuery representing the structured
+   * query in the XML.
+   * 
+   * @param xml
+   * @return
+   */
+  public static ValueQuery xmlToValueQuery(String xml, String config, Boolean includeGeometry)
+  {
+    JSONObject queryConfig;
+    try
+    {
+      queryConfig = new JSONObject(config);
+    }
+    catch (JSONException e1)
+    {
+      throw new ProgrammingErrorException(e1);
+    }
+
+    QueryFactory queryFactory = new QueryFactory();
+
+    ValueQuery valueQuery = new ValueQuery(queryFactory);
+
+    // IMPORTANT: Required call for all query screens.
+    Map<String, GeneratedEntityQuery> queryMap = QueryUtil.joinQueryWithGeoEntities(queryFactory, valueQuery, xml, queryConfig, includeGeometry, IndividualIPT.CLASS, IndividualIPT.FACILITY);
+
+    MosquitoCollectionQuery mosquitoCollectionQuery = (MosquitoCollectionQuery) queryMap.get(MosquitoCollection.CLASS);
+
+    InfectionAssayQuery infectionQuery = (InfectionAssayQuery) queryMap.get(InfectionAssay.CLASS);
+    if (infectionQuery != null)
+    {
+      valueQuery.WHERE(infectionQuery.getCollection().EQ(mosquitoCollectionQuery.getId()));
+      QueryUtil.joinTermAllpaths(valueQuery, InfectionAssay.CLASS, infectionQuery);
+    }
+
+    PooledInfectionAssayQuery pooledInfectionQuery = (PooledInfectionAssayQuery) queryMap.get(PooledInfectionAssay.CLASS);
+    if (pooledInfectionQuery != null)
+    {
+      valueQuery.WHERE(pooledInfectionQuery.getCollection().EQ(mosquitoCollectionQuery.getId()));
+      QueryUtil.joinTermAllpaths(valueQuery, PooledInfectionAssay.CLASS, pooledInfectionQuery);
+    }
+    
+    MolecularAssayQuery molecularQuery = (MolecularAssayQuery) queryMap.get(MolecularAssay.CLASS);
+    if (molecularQuery != null)
+    {
+      valueQuery.WHERE(molecularQuery.getCollection().EQ(mosquitoCollectionQuery.getId()));
+      QueryUtil.joinTermAllpaths(valueQuery, MolecularAssay.CLASS, molecularQuery);
+    }
+    
+    BiochemicalAssayQuery biochemicalQuery = (BiochemicalAssayQuery) queryMap.get(BiochemicalAssay.CLASS);
+    if (biochemicalQuery != null)
+    {
+      valueQuery.WHERE(biochemicalQuery.getCollection().EQ(mosquitoCollectionQuery.getId()));
+      QueryUtil.joinTermAllpaths(valueQuery, BiochemicalAssay.CLASS, biochemicalQuery);
+    }
+    
+
+    QueryUtil.joinGeoDisplayLabels(valueQuery, MosquitoCollection.CLASS, mosquitoCollectionQuery);
+
+    QueryUtil.joinTermAllpaths(valueQuery, MosquitoCollection.CLASS, mosquitoCollectionQuery);
+
+    QueryUtil.setTermRestrictions(valueQuery, queryMap);
+
+    QueryUtil.setNumericRestrictions(valueQuery, queryConfig);
+
+    QueryUtil.setQueryDates(xml, valueQuery, queryConfig, queryMap);
+
+    QueryUtil.setQueryRatio(xml, valueQuery, "COUNT(*)");
+
+    return valueQuery;
+  }
+
 }
