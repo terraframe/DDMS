f88fbbc30b49d1acaf490dc54dde5c52789d23d2
chris@teravation.com <chris@teravation.com>
2009-12-21 20:29:04 +0000

    New saved search helper to create the universal layers
    git-svn-id: https://subversion.assembla.com/svn/mdss@3503 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: d9da66d764365daaa4c8b7ef2ee7fc12f1539fad

---
+++ trunk/src/server/dss/vector/solutions/util/UniversalSearchHelper.java
@@ -0,0 +1,128 @@
+package dss.vector.solutions.util;
+
+import com.terraframe.mojo.dataaccess.transaction.Transaction;
+import com.terraframe.mojo.generation.loader.Reloadable;
+import com.terraframe.mojo.query.AND;
+import com.terraframe.mojo.query.Condition;
+import com.terraframe.mojo.query.OIterator;
+import com.terraframe.mojo.query.QueryFactory;
+import com.terraframe.mojo.session.StartSession;
+
+import dss.vector.solutions.MDSSInfo;
+import dss.vector.solutions.geo.GeoHierarchy;
+import dss.vector.solutions.geo.GeoHierarchyQuery;
+import dss.vector.solutions.geo.generated.Earth;
+import dss.vector.solutions.geo.generated.GeoEntity;
+import dss.vector.solutions.query.SavedSearch;
+import dss.vector.solutions.query.SavedSearchQuery;
+
+public class UniversalSearchHelper implements Reloadable {
+	@StartSession
+	public static void main(String[] args) {
+		UniversalSearchHelper helper = new UniversalSearchHelper();
+		helper.createAllSearches();
+	}
+
+	@Transaction
+	public void createAllSearches() {
+		QueryFactory f = new QueryFactory();
+		GeoHierarchyQuery q = new GeoHierarchyQuery(f);
+		
+		Condition condition = q.getGeoEntityClass().getKeyName().NE(GeoEntity.CLASS);
+		condition = AND.get(condition, q.getGeoEntityClass().getKeyName().NE(Earth.CLASS));
+
+		q.WHERE(condition);
+
+		OIterator<? extends GeoHierarchy> i = q.getIterator();
+
+		try {
+			while (i.hasNext()) {
+				GeoHierarchy universal = i.next();
+				this.createSearch(universal);
+			}
+		} finally {
+			i.close();
+		}
+	}
+	
+	@Transaction
+	public void createSearch(GeoHierarchy universal) {
+		System.out.println(universal);
+
+		SavedSearch search = new SavedSearch();
+		search.setQueryName(this.getShortClassName(universal));
+		search.setQueryType("GeoEntitySearch");
+		search.setConfig(this.getConfig(universal));
+		search.setQueryXml(this.getXML(universal));
+		
+		search.apply();
+	}
+	
+	@Transaction
+	public void deleteSearch(GeoHierarchy universal) {
+		QueryFactory f = new QueryFactory();
+		SavedSearchQuery q = new SavedSearchQuery(f);
+		q.WHERE(q.getQueryName().EQ(this.getShortClassName(universal)));
+
+		OIterator<? extends SavedSearch> i = q.getIterator();
+
+		try {
+			while (i.hasNext()) {
+				SavedSearch search = i.next();
+				search.delete();
+			}
+		} finally {
+			i.close();
+		}
+	}
+	
+	private String getConfig(GeoHierarchy universal) {	
+		return "{'date_attribute':{},'terms':{},'criteriaEntities':{},'selectedUniversals':{'dss.vector.solutions.geo.generated.Country':['dss.vector.solutions.geo.generated.Country']}}";
+	}
+	
+	private String getXML(GeoHierarchy universal) {
+		String universalClass = this.getLongClassName(universal);
+		String underscoredUniversalClass = universalClass.replace('.', '_');
+		StringBuilder sb = new StringBuilder();
+		sb.append("<query>");
+		sb.append("	<entities>");
+		sb.append("		<entity>");
+		sb.append("			<type>" + universalClass + "</type>");
+		sb.append("			<alias>" + universalClass + "</alias>");
+		sb.append("			<criteria></criteria>");
+		sb.append("		</entity>");
+		sb.append("	</entities>");
+		sb.append("	<select>");
+		sb.append("		<selectable>");
+		sb.append("			<attribute>");
+		sb.append("				<entityAlias>" + universalClass + "</entityAlias>");
+		sb.append("				<name>entityName</name>");
+		sb.append("				<userAlias>" + underscoredUniversalClass + "__entityName</userAlias>");
+		sb.append("				<userDisplayLabel></userDisplayLabel>");
+		sb.append("			</attribute>");
+		sb.append("		</selectable>");
+		sb.append("		<selectable>");
+		sb.append("			<attribute>");
+		sb.append("				<entityAlias>" + universalClass + "</entityAlias>");
+		sb.append("				<name>geoId</name>");
+		sb.append("				<userAlias>" + underscoredUniversalClass + "__geoId</userAlias>");
+		sb.append("				<userDisplayLabel></userDisplayLabel>");
+		sb.append("			</attribute>");
+		sb.append("		</selectable>");
+		sb.append("	</select>");
+		sb.append("	<groupby></groupby>");
+		sb.append("	<having></having>");
+		sb.append("	<orderby></orderby>");
+		sb.append("</query>");
+		return sb.toString();
+	}
+	
+	private String getShortClassName(GeoHierarchy universal) {
+		return this.getLongClassName(universal).substring(MDSSInfo.GENERATED_GEO_PACKAGE.length()+1);
+	}
+
+		
+	private String getLongClassName(GeoHierarchy universal) {
+		return universal.getGeoEntityClass().getKey();
+	}
+}
