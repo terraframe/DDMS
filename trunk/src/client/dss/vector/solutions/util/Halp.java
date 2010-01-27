b634dc6ad1f70b411dfae1b0e9ddba238f4fff92
jsmethie@terraframe.com <jsmethie@terraframe.com>
2010-01-27 19:16:47 +0000

    -Ticket #1118 (.5 hours)  * Bug when trying to load the default value of an attribute without read permissions
    git-svn-id: https://subversion.assembla.com/svn/mdss@3766 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 9ccd5d34ac76464bda70d55e1620be3d0037b61e

--- trunk/src/client/dss/vector/solutions/util/Halp.java
+++ trunk/src/client/dss/vector/solutions/util/Halp.java
@@ -932,7 +932,7 @@ public class Halp implements com.terraframe.mojo.generation.loader.Reloadable
         {
           if(!(t instanceof ClientReadAttributePermissionException))
           {
-            throw (RuntimeException) t;
+            throw (RuntimeException) t; 
           }
         }
         else
