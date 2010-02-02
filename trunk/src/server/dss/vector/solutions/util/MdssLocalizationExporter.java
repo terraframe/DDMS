53419fb284e4f57f59efb607f0c9e4ab78f38b6c
egrunzke@terraframe.com <egrunzke@terraframe.com>
2010-02-02 09:07:00 +0000

    First commit of Localization Exporter and Importer
    git-svn-id: https://subversion.assembla.com/svn/mdss@3851 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 25c001efa41f2e54f3a5d8a9f9e877f113766ddb

---
+++ trunk/src/server/dss/vector/solutions/util/MdssLocalizationExporter.java
@@ -0,0 +1,349 @@
+package dss.vector.solutions.util;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRichTextString;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import com.terraframe.mojo.SystemException;
+import com.terraframe.mojo.constants.ServerProperties;
+import com.terraframe.mojo.dataaccess.MdLocalStructDAOIF;
+import com.terraframe.mojo.dataaccess.io.FileReadException;
+import com.terraframe.mojo.dataaccess.io.XMLParseException;
+import com.terraframe.mojo.dataaccess.metadata.MdLocalizableDAO;
+import com.terraframe.mojo.generation.loader.TerraFrameClassLoader;
+import com.terraframe.mojo.query.OR;
+import com.terraframe.mojo.query.QueryFactory;
+import com.terraframe.mojo.system.metadata.MdAttribute;
+import com.terraframe.mojo.system.metadata.MdAttributeConcrete;
+import com.terraframe.mojo.system.metadata.MdAttributeQuery;
+import com.terraframe.mojo.system.metadata.MdClass;
+import com.terraframe.mojo.system.metadata.MdEnumeration;
+import com.terraframe.mojo.system.metadata.MdLocalizable;
+import com.terraframe.mojo.system.metadata.MdLocalizableQuery;
+import com.terraframe.mojo.system.metadata.MdType;
+import com.terraframe.mojo.system.metadata.MdTypeQuery;
+import com.terraframe.mojo.system.metadata.MetaData;
+import com.terraframe.mojo.system.metadata.MetaDataDisplayLabel;
+import com.terraframe.mojo.system.metadata.MetaDataQuery;
+import com.terraframe.mojo.util.FileIO;
+import com.terraframe.mojo.util.LocalizeUtil;
+
+public class MdssLocalizationExporter
+{
+  public static final String CONTROL_PANEL_PROPERTIES = "Control Panel";
+  public static final String MDSS_PROPERTIES = "UI Text";
+  public static final String DISPLAY_LABELS = "Display Labels";
+  public static final String COMMON_EXCEPTIONS = "Common Exceptions";
+  public static final String CLIENT_EXCEPTIONS = "Client Exceptions";
+  public static final String SERVER_EXCEPTIONS = "Server Exceptions";
+  public static final String MD_EXCEPTIONS = "Custom Exceptions";
+  
+  /**
+   * The in memory representation of the xls file
+   */
+  private HSSFWorkbook workbook;
+  private HSSFSheet customSheet;
+  private HSSFSheet clientSheet;
+  private HSSFSheet serverSheet;
+  private HSSFSheet commonSheet;
+  private HSSFSheet labelSheet;
+  private HSSFSheet propertySheet;
+  private HSSFSheet controlPanelSheet;
+  private List<Locale> locales;
+  
+  public MdssLocalizationExporter()
+  {
+    workbook = new HSSFWorkbook();
+    customSheet = workbook.createSheet(MD_EXCEPTIONS);
+    serverSheet = workbook.createSheet(SERVER_EXCEPTIONS);
+    clientSheet = workbook.createSheet(CLIENT_EXCEPTIONS);
+    commonSheet = workbook.createSheet(COMMON_EXCEPTIONS);
+    labelSheet = workbook.createSheet(DISPLAY_LABELS);
+    propertySheet = workbook.createSheet(MDSS_PROPERTIES);
+    controlPanelSheet = workbook.createSheet(CONTROL_PANEL_PROPERTIES);
+    locales = new LinkedList<Locale>();
+    locales.add(Locale.ENGLISH);
+    locales.add(Locale.FRENCH);
+    locales.add(new Locale("pt"));
+  }
+  
+  public void export()
+  {
+    prepareExceptions();
+    prepareTypes();
+    prepareProperties("MDSS", propertySheet);
+    prepareProperties("serverExceptions", serverSheet);
+    prepareProperties("commonExceptions", commonSheet);
+    prepareProperties("clientExceptions", clientSheet);
+    prepareProperties("MdssControlPanel", controlPanelSheet);
+    prepareHeaders();
+  }
+
+  private void prepareHeaders()
+  {
+    HSSFSheet[] sheets = new HSSFSheet[]{customSheet, serverSheet, clientSheet, commonSheet, labelSheet, propertySheet, controlPanelSheet};
+    for (HSSFSheet sheet : sheets)
+    {
+      HSSFRow row = sheet.createRow(0);
+      int i=0;
+      sheet.autoSizeColumn((short)i);
+      row.createCell(i++).setCellValue(new HSSFRichTextString("Key"));
+      for (Locale l : locales)
+      {
+        sheet.autoSizeColumn((short)i);
+        row.createCell(i++).setCellValue(new HSSFRichTextString(l.toString()));
+      }
+    }
+  }
+  
+  private void prepareExceptions()
+  {
+    int r=1;
+    MdLocalizableQuery query = new MdLocalizableQuery(new QueryFactory());
+    query.ORDER_BY_ASC(query.getPackageName());
+    query.ORDER_BY_ASC(query.getTypeName());
+    for (MdLocalizable localizable : query.getIterator())
+    {
+      MdLocalizableDAO dao = (MdLocalizableDAO)MdLocalizableDAO.get(localizable.getId());
+      File xmlFile = dao.getXmlFile();
+      Map<String, String> templates;
+      try
+      {
+        templates = LocalizeUtil.getAllTemplates(xmlFile);
+      }
+      catch (IOException e)
+      {
+        throw new FileReadException(xmlFile, e);
+      }
+      catch (Exception e)
+      {
+        throw new XMLParseException(e);
+      }
+      
+      HSSFRow row = customSheet.createRow(r++);
+      int c = 0;
+      row.createCell(c++).setCellValue(new HSSFRichTextString(localizable.definesType()));
+      for (Locale l : locales)
+      {
+        HSSFCell cell = row.createCell(c++);
+        String message = templates.get(l.toString());
+        if (message!=null)
+        {
+          cell.setCellValue(new HSSFRichTextString(message));
+        }
+      }
+    }
+  }
+  
+  private void prepareTypes()
+  {
+    int r=1;
+    QueryFactory qf = new QueryFactory();
+    MetaDataQuery query = new MetaDataQuery(qf);
+    query.ORDER_BY_ASC(query.getKeyName());
+    for (MetaData md : query.getIterator())
+    {
+      if (!md.getSiteMaster().equalsIgnoreCase(ServerProperties.getDomain()))
+      {
+        continue;
+      }
+      
+      MetaDataDisplayLabel label = null;
+      if (md instanceof MdClass)
+      {
+        label = ((MdClass) md).getDisplayLabel();
+      }
+      if (md instanceof MdEnumeration)
+      {
+        label = ((MdEnumeration) md).getDisplayLabel();
+      }
+      if (md instanceof MdAttribute)
+      {
+        MdAttribute mdAttribute = (MdAttribute) md;
+        String attributeName = mdAttribute.getValue(MdAttributeConcrete.ATTRIBUTENAME);
+        if (attributeName.equalsIgnoreCase(MetaData.ID) ||
+            attributeName.equalsIgnoreCase(MetaData.CREATEDATE) ||
+            attributeName.equalsIgnoreCase(MetaData.CREATEDBY) ||
+            attributeName.equalsIgnoreCase(MetaData.ENTITYDOMAIN) ||
+            attributeName.equalsIgnoreCase(MetaData.ID) ||
+            attributeName.equalsIgnoreCase(MetaData.KEYNAME) ||
+            attributeName.equalsIgnoreCase(MetaData.LASTUPDATEDATE) ||
+            attributeName.equalsIgnoreCase(MetaData.LASTUPDATEDBY) ||
+            attributeName.equalsIgnoreCase(MetaData.LOCKEDBY) ||
+            attributeName.equalsIgnoreCase(MetaData.OWNER) ||
+            attributeName.equalsIgnoreCase(MetaData.SEQ) ||
+            attributeName.equalsIgnoreCase(MetaData.SITEMASTER) ||
+            attributeName.equalsIgnoreCase(MetaData.TYPE))
+        {
+          continue;
+        }
+          
+        label = mdAttribute.getDisplayLabel();
+      }
+      
+      if (label==null)
+      {
+        continue;
+      }
+      MdLocalStructDAOIF mdLocalStruct = label.getMdClass();
+      
+      HSSFRow row = labelSheet.createRow(r++);
+      int c=0;
+      row.createCell(c++).setCellValue(new HSSFRichTextString(md.getKeyName()));
+      for (Locale l : locales)
+      {
+        HSSFCell cell = row.createCell(c++);
+        String localeString = l.toString();
+        
+        if (c==2)
+        {
+          localeString = MetaDataDisplayLabel.DEFAULTLOCALE;
+        }
+        else if (mdLocalStruct.definesAttribute(localeString)==null)
+        {
+          continue;
+        }
+        
+        String value = label.getValue(localeString);
+        if (value.trim().length()>0)
+        {
+          cell.setCellValue(new HSSFRichTextString(value));
+        }
+      }
+    }
+  }
+  
+  @SuppressWarnings("unchecked")
+  private void prepareProperties(String bundleName, HSSFSheet sheet)
+  {
+    URL url = Thread.currentThread().getContextClassLoader().getResource(bundleName + ".properties");
+    File base = null;
+    File dir = null;
+    List<String> lines = null;
+    try
+    {
+      base = new File(url.toURI());
+      dir = base.getParentFile();
+      lines = FileIO.readLines(base);
+    }
+    catch (Exception e)
+    {
+      if (base != null)
+      {
+        throw new FileReadException(base, e);
+      }
+      else
+      {
+        throw new SystemException(e);
+      }
+    }
+    
+    int r = 1;
+    // First, read the keys from the default
+    for (Entry<String, String> e : getProperties(lines).entrySet())
+    {
+      HSSFRow row = sheet.createRow(r++);
+      row.createCell(0).setCellValue(new HSSFRichTextString(e.getKey()));
+      row.createCell(1).setCellValue(new HSSFRichTextString(e.getValue()));
+    }
+    
+    // Now read each locale
+    int c=0;
+    for (Locale l : locales)
+    {
+      c++;
+      if (c==1)
+      {
+        continue;
+      }
+      
+      File localFile = new File(dir, "MDSS_" + l.toString() + ".properties");
+      if (!localFile.exists())
+      {
+        continue;
+      }
+      
+      Map<String, String> properties = null;
+      try
+      {
+        properties = getProperties(FileIO.readLines(localFile));
+      }
+      catch (IOException e)
+      {
+        throw new FileReadException(localFile, e);
+      }
+      
+      Iterator<HSSFRow> rowIterator = sheet.rowIterator();
+      while (rowIterator.hasNext())
+      {
+        HSSFRow row = rowIterator.next();
+        String key = row.getCell(0).getRichStringCellValue().toString();
+        String value = properties.get(key);
+        
+        if (value!=null)
+        {
+          row.createCell(c).setCellValue(new HSSFRichTextString(value));
+        }
+      }
+    }
+  }
+  
+  private Map<String, String> getProperties(List<String> lines)
+  {
+    LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
+    for (String line : lines)
+    {
+      // Check to see if this line is a comment
+      if (line.trim().startsWith("#"))
+      {
+        continue;
+      }
+      
+      String[] split = line.split("=", 2);
+      if (split.length!=2)
+      {
+        continue;
+      }
+      map.put(split[0], split [1]);
+    }
+    return map;
+  }
+
+  /**
+   * Writes the workbook to a byte array, which can then be written to the
+   * filesystem or streamed across the net.
+   *
+   * @return
+   */
+  public byte[] write()
+  {
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    BufferedOutputStream buffer = new BufferedOutputStream(bytes);
+    try
+    {
+      workbook.write(buffer);
+      buffer.flush();
+      buffer.close();
+      return bytes.toByteArray();
+    }
+    catch (IOException e)
+    {
+      throw new SystemException(e);
+    }
+  }
+}
---
+++ trunk/src/server/dss/vector/solutions/util/MdssLocalizationImporter.java
@@ -0,0 +1,324 @@
+package dss.vector.solutions.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigDecimal;
+import java.net.URISyntaxException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+
+import com.terraframe.mojo.SystemException;
+import com.terraframe.mojo.constants.MdLocalizableInfo;
+import com.terraframe.mojo.constants.SupportedLocaleInfo;
+import com.terraframe.mojo.dataaccess.ProgrammingErrorException;
+import com.terraframe.mojo.dataaccess.io.FileReadException;
+import com.terraframe.mojo.dataaccess.io.FileWriteException;
+import com.terraframe.mojo.dataaccess.metadata.MdLocalizableDAO;
+import com.terraframe.mojo.dataaccess.metadata.SupportedLocaleDAO;
+import com.terraframe.mojo.dataaccess.transaction.Transaction;
+import com.terraframe.mojo.session.StartSession;
+import com.terraframe.mojo.system.metadata.MdAttribute;
+import com.terraframe.mojo.system.metadata.MdLocalizable;
+import com.terraframe.mojo.system.metadata.MdType;
+import com.terraframe.mojo.system.metadata.MetaData;
+import com.terraframe.mojo.system.metadata.MetaDataDisplayLabel;
+import com.terraframe.mojo.util.FileIO;
+
+public class MdssLocalizationImporter
+{
+  private HSSFSheet customSheet;
+  private HSSFSheet clientSheet;
+  private HSSFSheet serverSheet;
+  private HSSFSheet commonSheet;
+  private HSSFSheet labelSheet;
+  private HSSFSheet propertySheet;
+  private HSSFSheet controlPanelSheet;
+  private List<Locale> locales;
+  
+  @StartSession
+  public static void main(String[] args) throws FileNotFoundException
+  {
+    long start = System.currentTimeMillis();
+    
+    MdssLocalizationImporter mli = new MdssLocalizationImporter();
+    mli.read(new FileInputStream("testLoc.xls"));
+
+    long stop = System.currentTimeMillis();
+    System.out.println("Execution time: " + ( stop - start ) / 1000.0 + " seconds");
+  }
+  
+  public MdssLocalizationImporter()
+  {
+    locales = new LinkedList<Locale>();
+  }
+  
+  @StartSession
+  public void read(InputStream stream)
+  {
+    openStream(stream);
+    
+    parseLocales();
+    
+    updateProperties("MDSS", propertySheet);
+    updateProperties("serverExceptions", serverSheet);
+    updateProperties("commonExceptions", commonSheet);
+    updateProperties("clientExceptions", clientSheet);
+    updateProperties("MdssControlPanel", controlPanelSheet);
+    updateExceptions();
+    updateLabels();
+  }
+
+  @SuppressWarnings("unchecked")
+  private void parseLocales()
+  {
+    HSSFRow row = propertySheet.getRow(0);
+    Iterator<HSSFCell> cellIterator = row.cellIterator();
+    cellIterator.next();
+    while (cellIterator.hasNext())
+    {
+      HSSFCell cell = cellIterator.next();
+      locales.add(new Locale(cell.getRichStringCellValue().getString()));
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  private void updateProperties(String bundle, HSSFSheet sheet)
+  {
+    File dir = null;
+    try
+    {
+      dir = FileIO.getDirectory(bundle + ".properties");
+    }
+    catch (URISyntaxException e)
+    {
+      e.printStackTrace();
+    }
+    
+    int c = 0;
+    for (Locale l : locales)
+    {
+      c++;
+      String data = new String();
+      Iterator<HSSFRow> rowIterator = sheet.rowIterator();
+      rowIterator.next();
+      while(rowIterator.hasNext())
+      {
+        HSSFRow row = rowIterator.next();
+        String key = row.getCell(0).getRichStringCellValue().getString();
+        HSSFCell cell = row.getCell(c);
+        if (cell==null)
+        {
+          continue;
+        }
+        
+        data += key + '=' + getStringValue(cell) + '\n';
+      }
+      
+      String suffix = l.toString();
+      if (suffix.equals(Locale.ENGLISH.toString()))
+      {
+        suffix = new String();
+      }
+      else
+      {
+        suffix = '_' + suffix;
+      }
+      
+      File file = new File(dir, bundle + suffix + ".properties");
+      try
+      {
+        FileIO.write(file, data);
+      }
+      catch (IOException e)
+      {
+        throw new FileWriteException(file, e);
+      }
+    }
+  }
+
+  private String getStringValue(HSSFCell cell)
+  {
+    if (cell==null)
+    {
+      return null;
+    }
+    else if(cell.getCellType() == HSSFCell.CELL_TYPE_NUMERIC)
+    {
+      return (new BigDecimal(cell.getNumericCellValue())).toString();
+    }
+    else if (cell.getCellType() == HSSFCell.CELL_TYPE_BOOLEAN)
+    {
+      return new Boolean(cell.getBooleanCellValue()).toString();
+    }
+    else
+    {
+      return cell.getRichStringCellValue().getString().trim();
+    }
+  }
+  
+  @SuppressWarnings("unchecked")
+  @Transaction
+  private void updateLabels()
+  {
+    ensureInstalledLocales();
+    
+    Iterator<HSSFRow> rowIterator = labelSheet.rowIterator();
+    rowIterator.next();
+    while (rowIterator.hasNext())
+    {
+      HSSFRow row = rowIterator.next();
+      String key = getStringValue(row.getCell(0));
+      
+      MetaData metadata = MetaData.getByKey(key);
+      
+      MetaDataDisplayLabel label = null;
+      if (metadata instanceof MdType)
+      {
+        label = ((MdType) metadata).getDisplayLabel();
+      }
+      if (metadata instanceof MdAttribute)
+      {
+        label = ((MdAttribute) metadata).getDisplayLabel();
+      }
+      
+      int c=1;
+      for (Locale l : locales)
+      {
+        String value = getStringValue(row.getCell(c++));
+        if (value!=null)
+        {
+          if (l.equals(Locale.ENGLISH))
+          {
+            label.setDefaultLocale(value);
+          }
+          else
+          {
+            label.setValue(l, value);
+          }
+        }
+      }
+      metadata.apply();
+    }
+  }
+  
+  @Transaction
+  private void ensureInstalledLocales()
+  {
+    for (Locale l : locales)
+    {
+      String localeString = l.toString();
+      
+      if (localeString.equals(Locale.ENGLISH.toString()))
+      {
+        continue;
+      }
+      
+      try
+      {
+        SupportedLocaleDAO.getEnumeration(SupportedLocaleInfo.CLASS, localeString);
+      }
+      catch (ProgrammingErrorException e)
+      {
+        SupportedLocaleDAO locale = SupportedLocaleDAO.newInstance();
+        locale.setValue(SupportedLocaleInfo.NAME, localeString);
+        locale.setValue(SupportedLocaleInfo.DISPLAY_LABEL, l.getDisplayName(Locale.ENGLISH));
+        locale.setValue(SupportedLocaleInfo.LOCALE_LABEL, l.getDisplayName(Locale.ENGLISH));
+        locale.apply();
+      }
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  private void updateExceptions()
+  {
+    Iterator<HSSFRow> rowIterator = customSheet.rowIterator();
+    rowIterator.next();
+    while (rowIterator.hasNext())
+    {
+      HSSFRow row = rowIterator.next();
+      
+      String key = row.getCell(0).getRichStringCellValue().getString();
+      MdLocalizableDAO dao = (MdLocalizableDAO)MdLocalizableDAO.get(MdLocalizable.CLASS, key);
+      
+      String xmlString;
+      File xmlFile = dao.getXmlFile();
+      if (xmlFile.exists())
+      {
+        try
+        {
+          xmlString = FileIO.readString(xmlFile);
+        }
+        catch (IOException e)
+        {
+          throw new FileReadException(xmlFile, e);
+        }
+      }
+      else
+      {
+        xmlString = dao.getValue(MdLocalizableInfo.MESSAGES);
+      }
+      
+      int start = xmlString.indexOf("<locale");
+      int end = xmlString.lastIndexOf("/locale>");
+      String prefix = xmlString.substring(0, start).trim();
+      String suffix = "\n" + xmlString.substring(end + 8).trim();
+      
+      int c = 1;
+      String middle = new String();
+      for (Locale l : locales)
+      {
+        String value = getStringValue(row.getCell(c++));
+        if (value!=null)
+        {
+          middle += "\n  <locale language=\"" + l.toString() + "\">" + value + "</locale>";
+        }
+      }
+      
+      try
+      {
+        FileIO.write(xmlFile, prefix + middle + suffix);
+      }
+      catch (IOException e)
+      {
+        throw new FileWriteException(xmlFile, e);
+      }
+    }
+  }
+
+  /**
+   * Opens the stream and parses the sheet based on name
+   *
+   * @param stream
+   */
+  private void openStream(InputStream stream)
+  {
+    try
+    {
+      POIFSFileSystem fileSystem = new POIFSFileSystem(stream);
+      HSSFWorkbook workbook = new HSSFWorkbook(fileSystem);
+      customSheet = workbook.getSheet(MdssLocalizationExporter.MD_EXCEPTIONS);
+      clientSheet = workbook.getSheet(MdssLocalizationExporter.CLIENT_EXCEPTIONS);
+      serverSheet = workbook.getSheet(MdssLocalizationExporter.SERVER_EXCEPTIONS);
+      commonSheet = workbook.getSheet(MdssLocalizationExporter.COMMON_EXCEPTIONS);
+      labelSheet = workbook.getSheet(MdssLocalizationExporter.DISPLAY_LABELS);
+      propertySheet = workbook.getSheet(MdssLocalizationExporter.MDSS_PROPERTIES);
+      controlPanelSheet = workbook.getSheet(MdssLocalizationExporter.CONTROL_PANEL_PROPERTIES);
+    }
+    catch (IOException e)
+    {
+      throw new SystemException(e);
+    }
+  }
+
+}
