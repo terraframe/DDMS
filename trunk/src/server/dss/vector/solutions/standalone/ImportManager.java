534ed39f81d50c870078c213160f18ac160d6127
jsmethie@terraframe.com <jsmethie@terraframe.com>
2010-03-24 21:10:11 +0000

    -Synchronization update  * Prevent import of application files on the master server.
    git-svn-id: https://subversion.assembla.com/svn/mdss@4407 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 4ce866bd3d10b412014c9c581c85ac735978e8bf

--- trunk/src/server/dss/vector/solutions/standalone/ImportManager.java
+++ trunk/src/server/dss/vector/solutions/standalone/ImportManager.java
@@ -1,5 +1,6 @@
 package dss.vector.solutions.standalone;
 
+import java.beans.PropertyChangeListener;
 import java.io.File;
 
 import javax.swing.SwingWorker;
@@ -8,12 +9,14 @@ import com.runwaysdk.constants.CommonProperties;
 import com.runwaysdk.dataaccess.transaction.TransactionImportManager;
 import com.runwaysdk.session.StartSession;
 
+import dss.vector.solutions.InstallProperties;
+
 public class ImportManager extends SwingWorker<Void, Void>
 {
-  private File file;
-  
+  private File        file;
+
   private ImportPanel component;
-  
+
   public ImportManager(File file, ImportPanel panel)
   {
     this.file = file;
@@ -24,10 +27,10 @@ public class ImportManager extends SwingWorker<Void, Void>
   protected Void doInBackground() throws Exception
   {
     this.importFile();
-    
+
     return null;
   }
-  
+
   @StartSession
   private void importFile()
   {
@@ -38,8 +41,17 @@ public class ImportManager extends SwingWorker<Void, Void>
       {
         throw new RuntimeException("Provide a file to import.");
       }
+
+      TransactionImportManager manager = new TransactionImportManager(file.getAbsolutePath());
       
-      TransactionImportManager.importTransactions(CommonProperties.getTransactionXMLschemaLocation(), file.getAbsolutePath(), component);
+      // IMPORTANT: We do not want to import the application files if this
+      // is the master server.  The master server is the authorative copy
+      // of the application file artifacts.
+      manager.setImportApplicationFiles(!InstallProperties.isMaster());
+
+      manager.addPropertyChangeListener(component);
+      manager.importTransactions();
+
     }
     catch (Exception e)
     {
