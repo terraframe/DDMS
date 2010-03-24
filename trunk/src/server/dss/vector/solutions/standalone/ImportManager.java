dafc82331d0e64b7a4a5c8a320c15301ed27598d
jsmethie@terraframe.com <jsmethie@terraframe.com>
2010-03-24 21:12:57 +0000

    -Synchronization update  * Prevent import of application files on the master server.
    git-svn-id: https://subversion.assembla.com/svn/mdss@4409 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 932ff242efbbbd60aec075f840fd35f52b1f6c39

--- trunk/src/server/dss/vector/solutions/standalone/ImportManager.java
+++ trunk/src/server/dss/vector/solutions/standalone/ImportManager.java
@@ -1,15 +1,14 @@
 package dss.vector.solutions.standalone;
 
-import java.beans.PropertyChangeListener;
 import java.io.File;
 
 import javax.swing.SwingWorker;
 
-import com.runwaysdk.constants.CommonProperties;
 import com.runwaysdk.dataaccess.transaction.TransactionImportManager;
 import com.runwaysdk.session.StartSession;
 
 import dss.vector.solutions.InstallProperties;
+import dss.vector.solutions.util.MDSSProperties;
 
 public class ImportManager extends SwingWorker<Void, Void>
 {
@@ -39,7 +38,7 @@ public class ImportManager extends SwingWorker<Void, Void>
 
       if (file == null || !file.exists() || file.isDirectory())
       {
-        throw new RuntimeException("Provide a file to import.");
+        throw new RuntimeException(MDSSProperties.getString("Provide_Import_File"));
       }
 
       TransactionImportManager manager = new TransactionImportManager(file.getAbsolutePath());
