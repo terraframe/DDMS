50e7ef541b8974167ddbe3b49daf9b29c65335a5
egrunzke@terraframe.com <egrunzke@terraframe.com>
2010-04-29 17:12:29 +0000

    Changed to ensure that all master installs share the same domain
    git-svn-id: https://subversion.assembla.com/svn/mdss@4544 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 4b83ce4057440cb402254284fc911f4ad1ca9da6 [formerly 563bf5fb1ade2833cfb0548b90c5874a864ce4f7]
    Former-commit-id: 57ee1cbdff73a852de3262576f98e4086827dd2c

--- trunk/src/server/dss/vector/solutions/util/PostInstallSetup.java
+++ trunk/src/server/dss/vector/solutions/util/PostInstallSetup.java
@@ -9,8 +9,20 @@ public class PostInstallSetup
 {
   public static void main(String[] args) throws Exception
   {
-    editFile(args[0] + ".mdss.vector.solutions", "domain", "C:/MDSS/tomcat6/webapps/MDSS/WEB-INF/classes/terraframe.properties");
-    editFile(args[1], "master", "C:/MDSS/tomcat6/webapps/MDSS/WEB-INF/classes/install.properties");
+    String installationNumber = args[0];
+    editFile(installationNumber + ".mdss.vector.solutions", "domain", "C:/MDSS/tomcat6/webapps/MDSS/WEB-INF/classes/terraframe.properties");
+    
+    Boolean isMaster = Boolean.parseBoolean(args[1]);
+    if (isMaster)
+    {
+      editFile("true", "master", "C:/MDSS/tomcat6/webapps/MDSS/WEB-INF/classes/install.properties");
+      editFile("0.mdss.vector.solutions", "domain", "C:/MDSS/tomcat6/webapps/MDSS/WEB-INF/classes/terraframe.properties");
+    }
+    else
+    {
+      editFile("false", "master", "C:/MDSS/tomcat6/webapps/MDSS/WEB-INF/classes/install.properties");
+      editFile(installationNumber + ".mdss.vector.solutions", "domain", "C:/MDSS/tomcat6/webapps/MDSS/WEB-INF/classes/terraframe.properties");
+    }
   }
   
   private static void editFile(String newValue, String key, String path) throws IOException
