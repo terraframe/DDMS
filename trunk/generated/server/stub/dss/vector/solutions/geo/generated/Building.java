7eae9a8bf11176ef7cbfad2743b492b6062d6797
chris@teravation.com <chris@teravation.com>
2010-05-05 15:50:43 +0000

    Refactored the urban flag into the base (7000) and redid the country-specific spreadsheets/importer/schemas.
    git-svn-id: https://subversion.assembla.com/svn/mdss@4573 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 32c41acae373627970a617050946d56c6a4e429b [formerly a7b2705b3a713ae6017fbb232849654a9acff398]
    Former-commit-id: a0edb5746ddc7fd1402340232ed2ef62501e2a60

---
+++ trunk/generated/server/stub/dss/vector/solutions/geo/generated/Building.java
@@ -0,0 +1,12 @@
+package dss.vector.solutions.geo.generated;
+
+public class Building extends BuildingBase implements com.runwaysdk.generation.loader.Reloadable
+{
+  private static final long serialVersionUID = 733655098;
+  
+  public Building()
+  {
+    super();
+  }
+  
+}
