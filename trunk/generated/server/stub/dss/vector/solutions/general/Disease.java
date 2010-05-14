dce63a12475f5c23a26c69134aad507e0e89ddcd
chris@teravation.com <chris@teravation.com>
2010-05-14 16:20:08 +0000

    Disease refactoring
    git-svn-id: https://subversion.assembla.com/svn/mdss@4654 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 5ef224d14256124724ec864039492b7f41556dea [formerly 98f5d4710dff40a7e342158d0b1c9eb4314e7299]
    Former-commit-id: c78b33acb25a0632c9a08bf7b566dbf232d333a9

--- trunk/generated/server/stub/dss/vector/solutions/general/Disease.java
+++ trunk/generated/server/stub/dss/vector/solutions/general/Disease.java
@@ -4,6 +4,17 @@ public class Disease extends DiseaseBase implements com.runwaysdk.generation.loa
 {
   private static final long serialVersionUID = -1180941084;
   
+  public static final String MALARIA = "MALARIA";
+  public static final String DENGUE = "DENGUE";
+  
+  public static Disease getMalaria() {
+	  return Disease.getByKey(MALARIA);
+  }
+  
+  public static Disease getDengue() {
+	  return Disease.getByKey(DENGUE);
+  }
+  
   public Disease()
   {
     super();
