e43e8974758f4f1b0de9074828152cabe154b37d
chris@teravation.com <chris@teravation.com>
2010-05-14 16:42:20 +0000

    Disease refactoring
    git-svn-id: https://subversion.assembla.com/svn/mdss@4655 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: a09eeeea79180c7d370e8dcbce2c9c163cee3a65 [formerly 02d1898503d47c629febb581c7e32d29a674dbeb]
    Former-commit-id: 09c944e055f0876fc2814cb74fcab72f151f856c

--- trunk/generated/server/stub/dss/vector/solutions/general/Disease.java
+++ trunk/generated/server/stub/dss/vector/solutions/general/Disease.java
@@ -1,23 +1,33 @@
 package dss.vector.solutions.general;
 
-public class Disease extends DiseaseBase implements com.runwaysdk.generation.loader.Reloadable
-{
-  private static final long serialVersionUID = -1180941084;
-  
-  public static final String MALARIA = "MALARIA";
-  public static final String DENGUE = "DENGUE";
-  
-  public static Disease getMalaria() {
-	  return Disease.getByKey(MALARIA);
-  }
-  
-  public static Disease getDengue() {
-	  return Disease.getByKey(DENGUE);
-  }
-  
-  public Disease()
-  {
-    super();
-  }
-  
+import com.runwaysdk.session.Session;
+
+import dss.vector.solutions.MDSSUser;
+
+public class Disease extends DiseaseBase implements com.runwaysdk.generation.loader.Reloadable {
+	private static final long serialVersionUID = -1180941084;
+
+	public static final String MALARIA = "MALARIA";
+	public static final String DENGUE = "DENGUE";
+
+	public static Disease getMalaria() {
+		return Disease.getByKey(MALARIA);
+	}
+
+	public static Disease getDengue() {
+		return Disease.getByKey(DENGUE);
+	}
+
+	public static Disease getCurrent() {
+		String id = Session.getCurrentSession().getUser().getId();
+		MDSSUser user = MDSSUser.get(id);
+
+		String name = user.getDiseaseName();
+		return Disease.getByKey(name);
+	}
+
+	public Disease() {
+		super();
+	}
+
 }
