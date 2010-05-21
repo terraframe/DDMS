2b31e5b0def845e33c3383d2f379c0702f3d7416
darrellt@gmail.com <darrellt@gmail.com>
2010-05-21 18:15:05 +0000

    git-svn-id: https://subversion.assembla.com/svn/mdss@4723 e0e46c49-be69-4f5a-ad62-21024a331aea
    Former-commit-id: 9d5d424a2a5364afd51320f286a53aa6a820d68e [formerly 85b1c356c940e5077a836547b6b100831eafbf7a]
    Former-commit-id: f307c453c70fb0bdfda64dcf0f8d205551c36daf

--- trunk/generated/server/stub/dss/vector/solutions/entomology/PupalCollection.java
+++ trunk/generated/server/stub/dss/vector/solutions/entomology/PupalCollection.java
@@ -2,14 +2,24 @@ package dss.vector.solutions.entomology;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.runwaysdk.dataaccess.MdEntityDAOIF;
+import com.runwaysdk.dataaccess.ProgrammingErrorException;
 import com.runwaysdk.dataaccess.transaction.Transaction;
+import com.runwaysdk.query.GeneratedEntityQuery;
 import com.runwaysdk.query.OIterator;
 import com.runwaysdk.query.QueryFactory;
+import com.runwaysdk.query.ValueQuery;
 
 import dss.vector.solutions.LocalProperty;
 import dss.vector.solutions.general.Disease;
 import dss.vector.solutions.general.MalariaSeasonDateProblem;
+import dss.vector.solutions.query.Layer;
+import dss.vector.solutions.util.QueryUtil;
 
 public class PupalCollection extends PupalCollectionBase implements com.runwaysdk.generation.loader.Reloadable
 {
@@ -122,6 +132,95 @@ public class PupalCollection extends PupalCollectionBase implements com.runwaysd
     view.populateView(this, null);
     
     return view;
+  }
+  
+
+  /**
+   * Takes in an XML string and returns a ValueQuery representing the structured
+   * query in the XML.
+   * 
+   * @param xml
+   * @return
+   */
+  public static ValueQuery xmlToValueQuery(String xml, String config, Layer layer)
+  {
+    JSONObject queryConfig;
+    try
+    {
+      queryConfig = new JSONObject(config);
+    }
+    catch (JSONException e1)
+    {
+      throw new ProgrammingErrorException(e1);
+    }
+
+    QueryFactory queryFactory = new QueryFactory();
+
+    ValueQuery valueQuery = new ValueQuery(queryFactory);
+
+    // IMPORTANT: Required call for all query screens.
+    Map<String, GeneratedEntityQuery> queryMap = QueryUtil.joinQueryWithGeoEntities(queryFactory, valueQuery, xml, queryConfig, layer);
+
+    PupalCollectionQuery collectionQuery = ( PupalCollectionQuery  ) queryMap.get(PupalCollection.CLASS);
+    PupalPremiseQuery premiseQuery = ( PupalPremiseQuery  ) queryMap.get(PupalPremise.CLASS);
+    PupalContainerQuery collectionContainerQuery = ( PupalContainerQuery  ) queryMap.get(CollectionContainer.CLASS);
+    if(premiseQuery != null)
+    {
+      QueryUtil.joinTermAllpaths(valueQuery, PupalPremise.CLASS, premiseQuery );
+      valueQuery.WHERE(premiseQuery.getCollection().EQ(collectionQuery));
+    }
+
+    if(collectionContainerQuery != null)
+    {
+      QueryUtil.joinTermAllpaths(valueQuery, CollectionContainer.CLASS, collectionContainerQuery );
+     //valueQuery.WHERE(collectionContainerQuery.hasParent(premiseQuery));
+    }
+
+    QueryUtil.joinGeoDisplayLabels(valueQuery, PupalCollection.CLASS, collectionQuery );
+
+    QueryUtil.setTermRestrictions(valueQuery, queryMap);
+
+    QueryUtil.setNumericRestrictions(valueQuery, queryConfig);
+        
+    boolean needsJoin = false; 
+    
+    if(collectionContainerQuery == null)
+    {
+      collectionContainerQuery = new PupalContainerQuery(queryFactory);   
+    }
+    
+    MdEntityDAOIF md = collectionContainerQuery.getMdClassIF();
+    
+    String numberContainers = QueryUtil.getColumnName(md, CollectionContainer.NUMBERCONTAINERS);
+    String numberwithwater = QueryUtil.getColumnName(md, CollectionContainer.NUMBERCONTAINERS);
+    String numberdestroyed = QueryUtil.getColumnName(md, CollectionContainer.NUMBERCONTAINERS);
+    String numberwithlarvicide  = QueryUtil.getColumnName(md, CollectionContainer.NUMBERCONTAINERS);
+    String numberimmatures = QueryUtil.getColumnName(md, CollectionContainer.NUMBERCONTAINERS);
+    String numberlarvae = QueryUtil.getColumnName(md, CollectionContainer.NUMBERCONTAINERS);
+    String numberpupae = QueryUtil.getColumnName(md, CollectionContainer.NUMBERCONTAINERS);
+    String numberlarvaecollected = QueryUtil.getColumnName(md, CollectionContainer.NUMBERCONTAINERS);
+    String numberpupaecollected = QueryUtil.getColumnName(md, CollectionContainer.NUMBERCONTAINERS);
+    
+    
+    
+    String numberExamined = QueryUtil.getColumnName(md, CollectionPremise.NUMBEREXAMINED);
+    String numberInhabitants = QueryUtil.getColumnName(md, CollectionPremise.NUMBERINHABITANTS);
+    String numberWithImmatures = QueryUtil.getColumnName(md, CollectionPremise.NUMBERWITHIMMATURES);
+    String numberWithLarvae = QueryUtil.getColumnName(md, CollectionPremise.NUMBERWITHLARVAE);
+    String numberWithPupae = QueryUtil.getColumnName(md, CollectionPremise.NUMBERWITHPUPAE);
+    String premiseSize = QueryUtil.getColumnName(md, CollectionPremise.PREMISESIZE);
+    String premiseType = QueryUtil.getColumnName(md, CollectionPremise.PREMISETYPE);
+    
+    
+    
+    
+    needsJoin = QueryUtil.setSelectabeSQL(valueQuery, "hi_lp", "SUM("+numberimmatures+")/SUM("+numberWithImmatures+")*100") || needsJoin;
+    needsJoin = QueryUtil.setSelectabeSQL(valueQuery, "hi_l", "SUM("+numberlarvae+")/SUM("+numberWithImmatures+")*100") || needsJoin;
+    needsJoin = QueryUtil.setSelectabeSQL(valueQuery, "hi_p", "SUM("+numberpupae+")/SUM("+numberWithImmatures+")*100")|| needsJoin;
+    
+    return QueryUtil.setQueryDates(xml, valueQuery, collectionQuery, ImmatureCollection.STARTDATE, ImmatureCollection.ENDDATE);
+    
+
   }
 
 }
