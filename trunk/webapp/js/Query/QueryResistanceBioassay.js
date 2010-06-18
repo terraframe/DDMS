58b3546b56667e073d964220a1a10dcb3c2c0a2f
darrellt@gmail.com <darrellt@gmail.com>
2010-06-18 22:16:53 +0000

    git-svn-id: https://subversion.assembla.com/svn/mdss@4875 e0e46c49-be69-4f5a-ad62-21024a331aea
    Former-commit-id: 673f197d0999574cd587343d1c9bfaf3363efac0 [formerly 65ec0dbc4e75445b3612a24e808f6ee0b0aec5c5]
    Former-commit-id: ba2f0d63bc1c20615778a2189b0705416fb5bc8c

---
+++ trunk/webapp/js/Query/QueryResistanceBioassay.js
@@ -0,0 +1,114 @@
+Mojo.Meta.newClass('MDSS.QueryResistanceBioassay', {
+
+  Extends: MDSS.QueryBaseNew,
+  
+  Instance : {
+  
+    initialize : function(selectableGroups, queryList)
+    {
+
+  		this._groupByClass = Mojo.$.dss.vector.solutions.entomology.MosquitoCollection;
+  		this._mainQueryClass = this._groupByClass.CLASS;
+	
+      this._mosquitoCollection = new this._groupByClass();
+
+      this._commonQueryClasses = [
+                                 // Mojo.$.dss.vector.solutions.general.Insecticide.CLASS,
+                                // dss.vector.solutions.entomology.assay.AbstractAssay.CLASS,
+                                 ];
+      this._geoEntityAttribs = [
+                             {
+                               keyName :  this._groupByClass.CLASS+'.'+this._groupByClass.GEOENTITY,
+                               display : this._mosquitoCollection.getGeoEntityMd().getDisplayLabel()
+                             },
+                             
+                           ];
+      
+      this._dateAttribs = [
+                           {
+                          	 klass :  this._groupByClass,
+                             accessor : this._groupByClass.COLLECTIONDATE,
+                           },
+                          
+                          ];
+      
+      this._queryType = this._mainQueryClass;
+      
+      this.$initialize(selectableGroups, queryList);   
+      
+      //remove collection from exclusion classes so collection selectables will not be unchecked when switching assay types
+      this._exclusionClasses.shift();
+       
+      //we overide the xmlToValueQueryClass since it is not the same as the mainQueryClass for this page 
+      this._xmlToValueQueryClass = dss.vector.solutions.entomology.TimeResponseAssay.CLASS;
+      
+      var picker = this.getGeoPicker();      
+      picker.setPolitical(false);
+      picker.setSprayTargetAllowed(false);
+ 
+      },
+      
+      _getBrowserRootClass : function(attribute)
+      {
+        var type = attribute.getType();
+        if(type === 'dss.vector.solutions.entomology.MosquitoCollection')
+        {
+          return 'dss.vector.solutions.entomology.SearchMosquitoCollectionView';
+        }
+        else
+        {
+          return attribute.getType();
+        }
+      },
+				
+       /**
+       * Helper method to add Entomology attributes to selectables and as a column.
+       */
+      _addVisibleAttribute : function(attribute)
+      {
+        var attributeName = attribute.getAttributeName();
+
+        if(attribute.mainQueryClass)
+        {
+          this._mainQueryClass = attribute.mainQueryClass;
+        }
+
+        if(attribute.getType() == 'sqlcharacter'){
+          var selectable = new MDSS.QueryXML.Selectable(new MDSS.QueryXML.Sqlcharacter('', attributeName, attribute.getKey(),attribute.getDisplayLabel(),attribute._isAggregate));
+          selectable.attribute = attribute;
+          var column = new YAHOO.widget.Column({ key: attribute.getKey(),label: attribute.getDisplayLabel()});
+           column.attribute = attribute;
+        }else
+        if(attribute.getType() == 'sqlinteger'){
+          var selectable = new MDSS.QueryXML.Selectable(new MDSS.QueryXML.Sqlinteger('', attributeName, attribute.getKey(),attribute.getDisplayLabel(),attribute._isAggregate));
+          selectable.attribute = attribute;
+          var column = new YAHOO.widget.Column({ key: attribute.getKey(),label: attribute.getDisplayLabel()});
+           column.attribute = attribute;
+      	}else
+        if(attribute.getType() == 'sqlfloat'){
+          var selectable = new MDSS.QueryXML.Selectable(new MDSS.QueryXML.Sqlfloat('', attributeName, attribute.getKey(),attribute.getDisplayLabel(),attribute._isAggregate));
+          selectable.attribute = attribute;
+          var column = new YAHOO.widget.Column({ key: attribute.getKey(),label: attribute.getDisplayLabel()});
+           column.attribute = attribute;
+        }else
+        if(attribute.getType() == 'sqldouble'){
+          var selectable = new MDSS.QueryXML.Selectable(new MDSS.QueryXML.Sqldouble('', attributeName, attribute.getKey(),attribute.getDisplayLabel(),attribute._isAggregate));
+          selectable.attribute = attribute;
+          var column = new YAHOO.widget.Column({ key: attribute.getKey(),label: attribute.getDisplayLabel()});
+           column.attribute = attribute;
+        }
+        else
+        {
+          var selectable = attribute.getSelectable(true);
+          selectable.attribute = attribute;
+          var column = new YAHOO.widget.Column(attribute.getColumnObject());
+           column.attribute = attribute;
+        }
+
+        column = this._queryPanel.insertColumn(column);
+
+        this._visibleSelectables[attribute.getKey()] = selectable;
+      },
+
+    }
+});
