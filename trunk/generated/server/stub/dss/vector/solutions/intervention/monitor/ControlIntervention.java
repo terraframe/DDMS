480f472a9454a4d578c32027522e81dbdf27da05
darrellt@gmail.com <darrellt@gmail.com>
2010-07-02 22:13:40 +0000

    Intervention Control Query
    git-svn-id: https://subversion.assembla.com/svn/mdss@4967 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 330d9de26cea6355f7ccb31ff0f1bb30f79ab4f0 [formerly 95bc2a77a408f9f2273dff39820ecd4b50927895]
    Former-commit-id: 71f3dd88b8325968de8f1faec9efcba874ae5498

--- trunk/generated/server/stub/dss/vector/solutions/intervention/monitor/ControlIntervention.java
+++ trunk/generated/server/stub/dss/vector/solutions/intervention/monitor/ControlIntervention.java
@@ -2,12 +2,26 @@ package dss.vector.solutions.intervention.monitor;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.runwaysdk.dataaccess.ProgrammingErrorException;
 import com.runwaysdk.dataaccess.transaction.Transaction;
+import com.runwaysdk.query.Function;
+import com.runwaysdk.query.GeneratedEntityQuery;
 import com.runwaysdk.query.OIterator;
 import com.runwaysdk.query.QueryFactory;
+import com.runwaysdk.query.Selectable;
+import com.runwaysdk.query.SelectableSQL;
+import com.runwaysdk.query.ValueQuery;
 
 import dss.vector.solutions.general.Disease;
+import dss.vector.solutions.irs.InsecticideBrand;
+import dss.vector.solutions.irs.InsecticideBrandQuery;
+import dss.vector.solutions.query.Layer;
+import dss.vector.solutions.util.QueryUtil;
 
 public class ControlIntervention extends ControlInterventionBase implements com.runwaysdk.generation.loader.Reloadable
 {
@@ -198,6 +212,81 @@ public class ControlIntervention extends ControlInterventionBase implements com.
     view.populateView(this);
 
     return view;
+  }
+  
+  
+  /**
+   * Takes in an XML string and returns a ValueQuery representing the structured
+   * query in the XML.
+   * 
+   * @param xml
+   * @return
+   */
+  public static ValueQuery xmlToValueQuery(String xml, String config, Layer layer)
+  {
+    JSONObject queryConfig;
+    try
+    {
+      queryConfig = new JSONObject(config);
+    }
+    catch (JSONException e1)
+    {
+      throw new ProgrammingErrorException(e1);
+    }
+
+    QueryFactory queryFactory = new QueryFactory();
+
+    ValueQuery valueQuery = new ValueQuery(queryFactory);
+
+    // IMPORTANT: Required call for all query screens.
+    Map<String, GeneratedEntityQuery> queryMap = QueryUtil.joinQueryWithGeoEntities(queryFactory, valueQuery, xml, queryConfig, layer);
+
+    
+    ControlInterventionQuery controlInterventionQuery  = (ControlInterventionQuery) queryMap.get(ControlIntervention.CLASS);
+    
+    IndividualPremiseVisitQuery individualPremiseVisitQuery  = (IndividualPremiseVisitQuery) queryMap.get(IndividualPremiseVisit.CLASS);
+    IndividualPremiseVisitMethodQuery individualPremiseVisitMethodQuery  = (IndividualPremiseVisitMethodQuery) queryMap.get(IndividualPremiseVisitMethod.CLASS);
+    
+    AggregatedPremiseReasonQuery aggregatedPremiseReasonQuery  = (AggregatedPremiseReasonQuery) queryMap.get(AggregatedPremiseReason.CLASS);
+    AggregatedPremiseVisitQuery aggregatedPremiseVisitQuery  = (AggregatedPremiseVisitQuery) queryMap.get(AggregatedPremiseVisit.CLASS);
+    
+    PersonInterventionQuery personInterventionQuery  = (PersonInterventionQuery) queryMap.get(PersonIntervention.CLASS);
+    PersonInterventionMethodQuery personInterventionMethodQuery  = (PersonInterventionMethodQuery) queryMap.get(PersonInterventionMethod.CLASS);
+    
+    InsecticideInterventionQuery insecticideInterventionQuery  = (InsecticideInterventionQuery) queryMap.get(InsecticideIntervention.CLASS);
+    InsecticideBrandQuery insecticideBrandQuery  = (InsecticideBrandQuery) queryMap.get(InsecticideBrand.CLASS);
+    
+
+
+    QueryUtil.joinGeoDisplayLabels(valueQuery, ControlIntervention.CLASS, controlInterventionQuery );
+
+    QueryUtil.setTermRestrictions(valueQuery, queryMap);
+
+    QueryUtil.setNumericRestrictions(valueQuery, queryConfig);
+      
+
+    
+    
+    return QueryUtil.setQueryDates(xml, valueQuery, controlInterventionQuery, controlInterventionQuery.getStartDate(), controlInterventionQuery.getEndDate());
+
+  }
+  
+  static boolean getSelectabeTermRelationSQL(ValueQuery valueQuery, String ref, String sql)
+  {
+    if (valueQuery.hasSelectableRef(ref))
+    {
+      Selectable s = valueQuery.getSelectableRef(ref);
+
+      while (s instanceof Function)
+      {
+        Function f = (Function) s;
+        s = f.getSelectable();
+      }
+      
+      ( (SelectableSQL) s ).setSQL(sql);
+      return true;
+    }
+    return false;
   }
 
 }
