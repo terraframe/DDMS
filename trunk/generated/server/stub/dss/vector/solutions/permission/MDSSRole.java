f2b1628b717eca7a42bf5af96c84e4080e567475
jsmethie@terraframe.com <jsmethie@terraframe.com>
2010-07-05 15:49:16 +0000

    -Copy permissions from GUIVisibility to new Roles on Role creation
    git-svn-id: https://subversion.assembla.com/svn/mdss@4971 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 0a41acccae46e0f01a5486b992d535395e186208 [formerly 76f682de90c884849f6443823e6c241e3a16f6a0]
    Former-commit-id: 047317b956c2257eaead862fce9433f3685f9184

--- trunk/generated/server/stub/dss/vector/solutions/permission/MDSSRole.java
+++ trunk/generated/server/stub/dss/vector/solutions/permission/MDSSRole.java
@@ -1,65 +1,105 @@
 package dss.vector.solutions.permission;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
+import com.runwaysdk.business.rbac.Operation;
+import com.runwaysdk.business.rbac.OperationManager;
+import com.runwaysdk.business.rbac.RoleDAO;
+import com.runwaysdk.business.rbac.RoleDAOIF;
+import com.runwaysdk.dataaccess.MetadataDAOIF;
+import com.runwaysdk.dataaccess.RelationshipDAOIF;
 import com.runwaysdk.dataaccess.transaction.Transaction;
 import com.runwaysdk.query.OIterator;
 import com.runwaysdk.query.QueryFactory;
 import com.runwaysdk.system.Roles;
 
+import dss.vector.solutions.MDSSRoleInfo;
+
 public class MDSSRole extends MDSSRoleBase implements com.runwaysdk.generation.loader.Reloadable
 {
   private static final long serialVersionUID = 1185298795;
-  
+
   public MDSSRole()
   {
     super();
   }
-  
+
+  @Override
+  @Transaction
+  public void apply()
+  {
+    boolean isNew = !this.isAppliedToDB() && this.isNew();
+
+    super.apply();
+
+    // When new roles are defined we must copy over all of the existing
+    // permissions for GUIVisibility.
+    
+    if (isNew)
+    {
+      RoleDAO role = RoleDAO.get(this.getRole().getId()).getBusinessDAO();
+      RoleDAOIF guiVisibility = RoleDAO.findRole(MDSSRoleInfo.GUI_VISIBILITY);
+      
+      Set<RelationshipDAOIF> permissions = guiVisibility.getPermissions();
+      
+      for (RelationshipDAOIF permission : permissions)
+      {
+        MetadataDAOIF metadata = (MetadataDAOIF) permission.getChild();
+        
+        Set<Operation> operations = OperationManager.getOperations(permission);
+        List<Operation> _operations = new LinkedList<Operation>(operations);
+        
+        role.grantPermission(_operations, metadata.getId());
+      }
+    }
+  }
+
   @Override
   @Transaction
   public void delete()
   {
     this.getRole().delete();
-    
+
     super.delete();
   }
-  
+
   @Override
   public MDSSRoleView lockView()
   {
     this.lock();
-    
+
     return this.getView();
-  }  
-  
+  }
+
   public MDSSRoleView unlockView()
   {
     this.unlock();
-    
+
     return this.getView();
   }
-  
+
   public MDSSRoleView getView()
   {
     MDSSRoleView view = new MDSSRoleView();
-    
+
     view.populateView(this);
-    
+
     return view;
-  }  
-  
+  }
+
   public static Roles[] getRoles()
   {
     List<Roles> list = new ArrayList<Roles>();
     MDSSRoleQuery query = new MDSSRoleQuery(new QueryFactory());
-    
+
     OIterator<? extends MDSSRole> it = query.getIterator();
-    
+
     try
     {
-      while(it.hasNext())
+      while (it.hasNext())
       {
         MDSSRole role = it.next();
         list.add(role.getRole());
@@ -69,7 +109,7 @@ public class MDSSRole extends MDSSRoleBase implements com.runwaysdk.generation.l
     {
       it.close();
     }
-    
+
     return list.toArray(new Roles[list.size()]);
-  }  
+  }
 }
