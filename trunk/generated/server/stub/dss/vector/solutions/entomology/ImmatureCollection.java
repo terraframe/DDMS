d52b11c5a069559860847e0de15360055c89ec27
jnaifeh@terraframe.com <jnaifeh@terraframe.com>
2010-07-06 18:11:56 +0000

    Fixed 904 % contribution calculations.
    git-svn-id: https://subversion.assembla.com/svn/mdss@4982 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 3c9b681cdd29a965295e8c66561aa1ad1c3f4f77 [formerly 3082ea7b25f76d4b6134d5e0d8c9e03a7cdf0190]
    Former-commit-id: 7539741ef6ac695a631085633ac0808d122d40a4

--- trunk/generated/server/stub/dss/vector/solutions/entomology/ImmatureCollection.java
+++ trunk/generated/server/stub/dss/vector/solutions/entomology/ImmatureCollection.java
@@ -275,9 +275,10 @@ public class ImmatureCollection extends ImmatureCollectionBase implements com.ru
 
     needsJoin = QueryUtil.setSelectabeSQL(valueQuery, "percent_water_holding_larvae", "SUM(" + numberlarvae + ")/NULLIF(SUM(" + numberwithwater + "), 0.0)*100.0") || needsJoin;
     needsJoin = QueryUtil.setSelectabeSQL(valueQuery, "percent_water_holding_pupae", "SUM(" + numberpupae + ")/NULLIF(SUM(" + numberwithwater + "), 0.0)*100.0") || needsJoin;
-    needsJoin = QueryUtil.setSelectabeSQL(valueQuery, "percent_immature_contribution", "SUM(" + numberimmatures + ")/NULLIF(SUM(SUM(" + numberimmatures + ")) OVER (), 0.0)*100.0") || needsJoin;
-    needsJoin = QueryUtil.setSelectabeSQL(valueQuery, "percent_larve_contribution", "SUM(" + numberlarvae + ")/NULLIF(SUM(SUM(" + numberimmatures + ")) OVER (), 0.0)*100.0") || needsJoin;
-    needsJoin = QueryUtil.setSelectabeSQL(valueQuery, "percent_pupae_contribution", "SUM(" + numberpupae + ")/NULLIF(SUM(SUM(" + numberimmatures + ")) OVER (), 0.0)*100.0") || needsJoin;
+    needsJoin = QueryUtil.setSelectabeSQL(valueQuery, "percent_immature_contribution", "(SUM(" + numberlarvaecollected + ")+SUM(" + numberpupaecollected + "))/" +
+    		"NULLIF((SUM(SUM(" + numberlarvaecollected + ")) OVER ()) + (SUM(SUM(" + numberpupaecollected + ")) OVER ()), 0.0)*100.0") || needsJoin;
+    needsJoin = QueryUtil.setSelectabeSQL(valueQuery, "percent_larve_contribution", "SUM(" + numberlarvaecollected + ")/NULLIF(SUM(SUM(" + numberlarvaecollected + ")) OVER (), 0.0)*100.0") || needsJoin;
+    needsJoin = QueryUtil.setSelectabeSQL(valueQuery, "percent_pupae_contribution", "SUM(" + numberpupaecollected + ")/NULLIF(SUM(SUM(" + numberpupaecollected + ")) OVER (), 0.0)*100.0") || needsJoin;
 
     if (needsJoin)
     {
