99b7f4f0642c94d05a71acba9f8cb886f1ca51a6
egrunzke@terraframe.com <egrunzke@terraframe.com>
2010-07-29 21:59:07 +0000

    Added Mdss Log to replace sysouts
    git-svn-id: https://subversion.assembla.com/svn/mdss@5264 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 10d7a3c7a9418b3c05b6bba7be928201d36937b1 [formerly 152485669c84f26ecc2c8ade3ea83e728ce8fb7e]
    Former-commit-id: 2b204a92c10c0e029a4c8fe7194ec8b91a799968

---
+++ trunk/src/server/dss/vector/solutions/MdssLog.java
@@ -0,0 +1,106 @@
+package dss.vector.solutions;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class MdssLog
+{
+  // Made final so the Log is read-only 
+  private static final Log log;
+  
+  // Static initialization happens on load in the vm
+  static
+  {
+    log = LogFactory.getLog("developer");
+  }
+
+  public static void debug(Object message, Throwable t)
+  {
+    log.debug(message, t);
+  }
+
+  public static void debug(Object message)
+  {
+    log.debug(message);
+  }
+
+  public static void error(Object message, Throwable t)
+  {
+    log.error(message, t);
+  }
+
+  public static void error(Object message)
+  {
+    log.error(message);
+  }
+
+  public static void fatal(Object message, Throwable t)
+  {
+    log.fatal(message, t);
+  }
+
+  public static void fatal(Object message)
+  {
+    log.fatal(message);
+  }
+
+  public static void info(Object message, Throwable t)
+  {
+    log.info(message, t);
+  }
+
+  public static void info(Object message)
+  {
+    log.info(message);
+  }
+
+  public boolean isDebugEnabled()
+  {
+    return log.isDebugEnabled();
+  }
+
+  public boolean isErrorEnabled()
+  {
+    return log.isErrorEnabled();
+  }
+
+  public boolean isFatalEnabled()
+  {
+    return log.isFatalEnabled();
+  }
+
+  public boolean isInfoEnabled()
+  {
+    return log.isInfoEnabled();
+  }
+
+  public boolean isTraceEnabled()
+  {
+    return log.isTraceEnabled();
+  }
+
+  public boolean isWarnEnabled()
+  {
+    return log.isWarnEnabled();
+  }
+
+  public static void trace(Object message, Throwable t)
+  {
+    log.trace(message, t);
+  }
+
+  public static void trace(Object message)
+  {
+    log.trace(message);
+  }
+
+  public static void warn(Object message, Throwable t)
+  {
+    log.warn(message, t);
+  }
+
+  public static void warn(Object message)
+  {
+    log.warn(message);
+  }
+}
