fea0931872016eba9945ca31d921466fa16c03a3
jnaifeh@terraframe.com <jnaifeh@terraframe.com>
2010-09-02 06:31:25 +0000

    Fixed zone spray supervisor in IRS QB
    git-svn-id: https://subversion.assembla.com/svn/mdss@5371 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 582e542fe3a4cc54761c13eb6f5ac6a365591c99 [formerly 06e93ac3531fc13fc0a8c6f3cf514519ae643e29]
    Former-commit-id: dde0ee59afd2895f2e7422436ad250caf1501ed2

--- trunk/src/server/dss/vector/solutions/irs/ActualZoneSprayTarget.java
+++ trunk/src/server/dss/vector/solutions/irs/ActualZoneSprayTarget.java
@@ -11,41 +11,71 @@ import dss.vector.solutions.util.QueryUtil;
 public class ActualZoneSprayTarget extends ActualTargetUnion implements Reloadable
 {
   private String teamLeaderCol;
+
   private String sprayTeamCol;
+
   private String diseaseCol;
-  
+
   private String roomsCol;
+
   private String structuresCol;
+
   private String householdsCol;
+
   private String sprayedRoomsCol;
+
   private String sprayedStructuresCol;
+
   private String sprayedHouseholdsCol;
+
   private String prevSprayedStructuresCol;
+
   private String prevSprayedHouseholdsCol;
+
   private String peopleCol;
+
   private String bedNetsCol;
+
   private String roomsWithBedNetsCol;
+
   private String lockedCol;
+
   private String refusedCol;
+
   private String otherCol;
+
   private String receivedCol;
+
   private String usedCol;
+
   private String refillsCol;
+
   private String returnCol;
+
   private String zoneSprayTable;
+
   private String teamSprayStatusTable;
+
   private String sprayCol;
+
   private String supervisorCol;
-  
+
+  private String supervisorTable;
+
+  private String supervisorPersonCol;
+
   public ActualZoneSprayTarget()
   {
     super();
-    
+
+    supervisorTable = MdEntityDAO.getMdEntityDAO(Supervisor.CLASS).getTableName();
+    supervisorPersonCol = QueryUtil.getColumnName(Supervisor.getPersonMd());
+
     MdEntityDAOIF zoneSprayMd = MdEntityDAO.getMdEntityDAO(ZoneSpray.CLASS);
     zoneSprayTable = zoneSprayMd.getTableName();
     diseaseCol = QueryUtil.getColumnName(ZoneSpray.getDiseaseMd());
     supervisorCol = QueryUtil.getColumnName(ZoneSpray.getSupervisorMd());
-    
+
     MdEntityDAOIF teamSprayStatusMd = MdEntityDAO.getMdEntityDAO(TeamSprayStatus.CLASS);
     teamSprayStatusTable = teamSprayStatusMd.getTableName();
     sprayCol = QueryUtil.getColumnName(teamSprayStatusMd, TeamSprayStatus.SPRAY);
@@ -61,8 +91,10 @@ public class ActualZoneSprayTarget extends ActualTargetUnion implements Reloadab
     sprayedRoomsCol = QueryUtil.getColumnName(teamSprayStatusMd, TeamSprayStatus.SPRAYEDROOMS);
     sprayedStructuresCol = QueryUtil.getColumnName(teamSprayStatusMd, TeamSprayStatus.SPRAYEDSTRUCTURES);
     sprayedHouseholdsCol = QueryUtil.getColumnName(teamSprayStatusMd, TeamSprayStatus.SPRAYEDHOUSEHOLDS);
-    prevSprayedStructuresCol = QueryUtil.getColumnName(teamSprayStatusMd, TeamSprayStatus.PREVSPRAYEDSTRUCTURES);
-    prevSprayedHouseholdsCol = QueryUtil.getColumnName(teamSprayStatusMd, TeamSprayStatus.PREVSPRAYEDHOUSEHOLDS);
+    prevSprayedStructuresCol = QueryUtil.getColumnName(teamSprayStatusMd,
+        TeamSprayStatus.PREVSPRAYEDSTRUCTURES);
+    prevSprayedHouseholdsCol = QueryUtil.getColumnName(teamSprayStatusMd,
+        TeamSprayStatus.PREVSPRAYEDHOUSEHOLDS);
     peopleCol = QueryUtil.getColumnName(teamSprayStatusMd, TeamSprayStatus.PEOPLE);
     bedNetsCol = QueryUtil.getColumnName(teamSprayStatusMd, TeamSprayStatus.BEDNETS);
     roomsWithBedNetsCol = QueryUtil.getColumnName(teamSprayStatusMd, TeamSprayStatus.ROOMSWITHBEDNETS);
@@ -70,74 +102,75 @@ public class ActualZoneSprayTarget extends ActualTargetUnion implements Reloadab
     refusedCol = QueryUtil.getColumnName(teamSprayStatusMd, TeamSprayStatus.REFUSED);
     otherCol = QueryUtil.getColumnName(teamSprayStatusMd, TeamSprayStatus.OTHER);
   }
-  
+
   public String setId(Alias alias)
   {
     return set(this.zoneSprayTable, this.idCol, alias);
   }
-  
+
   public String setAggregationLevel(Alias alias)
   {
     return set("'3'::TEXT", alias);
   }
-  
+
   @Override
   public String setSprayTeamDefaultLocale(Alias alias)
   {
-    return set("(SELECT st." + teamIdCol + " FROM " + sprayTeamTable + 
-        " st WHERE st.id = " + teamSprayStatusTable + "." + sprayTeamCol + ")", alias);
+    return set("(SELECT st." + teamIdCol + " FROM " + sprayTeamTable + " st WHERE st.id = "
+        + teamSprayStatusTable + "." + sprayTeamCol + ")", alias);
   }
-  
+
   @Override
   public String setZoneSuperVisorDefaultLocale(Alias alias)
   {
-    return set("(SELECT  p." + firstNameCol + " || ' ' || p." + lastNameCol + " FROM " 
-        + personTable + " AS p WHERE p.id = " + zoneSprayTable + "." + supervisorCol + ")", alias);
+    return set("(SELECT  p." + firstNameCol + " || ' ' || p." + lastNameCol + " FROM " + personTable
+        + " AS p WHERE p.id = " + supervisorTable + "." + supervisorPersonCol + ")", alias);
   }
-  
+
   @Override
   public String setSprayLeaderDefaultLocale(Alias alias)
   {
-    return set("(SELECT tm." + memberIdCol + " || ' - ' || p." + firstNameCol + " || ' ' || p." + lastNameCol + " FROM " + teamMemberTable + " tm , " + personTable + 
-        " AS p WHERE p.id = tm."+personCol+" AND tm.id = " + teamSprayStatusTable + "." + teamLeaderCol + ")", alias);
+    return set("(SELECT tm." + memberIdCol + " || ' - ' || p." + firstNameCol + " || ' ' || p."
+        + lastNameCol + " FROM " + teamMemberTable + " tm , " + personTable + " AS p WHERE p.id = tm."
+        + personCol + " AND tm.id = " + teamSprayStatusTable + "." + teamLeaderCol + ")", alias);
   }
-  
+
   @Override
   public String setRooms(Alias alias)
   {
     return set(teamSprayStatusTable, roomsCol, alias);
   }
-  
+
   @Override
   public String setStructures(Alias alias)
   {
     return set(teamSprayStatusTable, structuresCol, alias);
   }
-  
+
   @Override
   public String setHouseholds(Alias alias)
   {
     return set(teamSprayStatusTable, householdsCol, alias);
   }
-  
+
   @Override
   public String setSprayedRooms(Alias alias)
   {
     return set(teamSprayStatusTable, sprayedRoomsCol, alias);
   }
-  
+
   @Override
   public String setSprayedStructures(Alias alias)
   {
     return set(teamSprayStatusTable, sprayedStructuresCol, alias);
   }
-  
+
   @Override
   public String setSprayedHouseholds(Alias alias)
   {
     return set(teamSprayStatusTable, sprayedHouseholdsCol, alias);
   }
-  
+
   @Override
   public String setPrevSprayedStructures(Alias alias)
   {
@@ -149,25 +182,25 @@ public class ActualZoneSprayTarget extends ActualTargetUnion implements Reloadab
   {
     return set(teamSprayStatusTable, prevSprayedHouseholdsCol, alias);
   }
-  
+
   @Override
   public String setPeople(Alias alias)
   {
     return set(teamSprayStatusTable, peopleCol, alias);
   }
-  
+
   @Override
   public String setBedNets(Alias alias)
   {
     return set(teamSprayStatusTable, bedNetsCol, alias);
   }
-  
+
   @Override
   public String setRoomsWithBedNets(Alias alias)
   {
     return set(teamSprayStatusTable, roomsWithBedNetsCol, alias);
   }
-  
+
   @Override
   public String setLocked(Alias alias)
   {
@@ -179,106 +212,109 @@ public class ActualZoneSprayTarget extends ActualTargetUnion implements Reloadab
   {
     return set(teamSprayStatusTable, refusedCol, alias);
   }
-  
+
   @Override
   public String setOther(Alias alias)
   {
     return set(teamSprayStatusTable, otherCol, alias);
   }
-  
+
   @Override
   public String setDisease(Alias alias)
   {
     return set(zoneSprayTable, diseaseCol, alias);
   }
-  
+
   @Override
   public String setReceived(Alias alias)
   {
     return set(teamSprayStatusTable, receivedCol, alias);
   }
-  
+
   @Override
   public String setUsed(Alias alias)
   {
     return set(teamSprayStatusTable, usedCol, alias);
   }
-  
+
   @Override
   public String setRefills(Alias alias)
   {
     return set(teamSprayStatusTable, refillsCol, alias);
   }
-  
+
   @Override
   public String setReturned(Alias alias)
   {
     return set(teamSprayStatusTable, returnCol, alias);
   }
-  
+
   @Override
   public String setRoomUnsprayed(Alias alias)
   {
-    return set("("+roomsCol+" - "+sprayedRoomsCol+")", alias);
+    return set("(" + roomsCol + " - " + sprayedRoomsCol + ")", alias);
   }
-  
+
   @Override
   public String setStructureUnsprayed(Alias alias)
   {
-    return set("("+structuresCol+" - "+sprayedStructuresCol+")", alias);
+    return set("(" + structuresCol + " - " + sprayedStructuresCol + ")", alias);
   }
-  
+
   @Override
   public String setHouseholdUnsprayed(Alias alias)
   {
-    return set("("+householdsCol+" - "+sprayedHouseholdsCol+")", alias);
+    return set("(" + householdsCol + " - " + sprayedHouseholdsCol + ")", alias);
   }
-  
+
   @Override
   public String setSprayedRoomsShare(Alias alias)
   {
     return set("1", alias);
   }
-  
+
   @Override
   public String setSprayedStructuresShare(Alias alias)
   {
     return set("1", alias);
   }
-  
+
   @Override
   public String setSprayedHouseholdsShare(Alias alias)
   {
     return set("1", alias);
   }
-  
+
   @Override
   public String from()
   {
     String from = "";
-    from += zoneSprayTable + " AS " + zoneSprayTable + " LEFT JOIN "+teamSprayStatusTable + " AS " + teamSprayStatusTable + " ON \n";
-    from += zoneSprayTable + "."+idCol+" = " + teamSprayStatusTable + "." + sprayCol + ",\n";
+    from += zoneSprayTable + " AS " + zoneSprayTable + " LEFT JOIN " + teamSprayStatusTable + " AS "
+        + teamSprayStatusTable + " ON \n";
+    from += zoneSprayTable + "." + idCol + " = " + teamSprayStatusTable + "." + sprayCol + " LEFT JOIN "
+        + supervisorTable + " " + supervisorTable + " ON " + supervisorTable + "." + idCol + " = "
+        + zoneSprayTable + "." + supervisorCol + ", \n";
     from += abstractSprayTable + " AS " + abstractSprayTable + "\n";
     from += " LEFT JOIN ";
     from += malariaSeasonTable + " AS sprayseason ";
-    
+
     String seasonDiseaseCol = QueryUtil.getColumnName(MalariaSeason.getDiseaseMd());
     String diseaseId = Disease.getCurrent().getId();
-    from += "ON " + abstractSprayTable + "." + sprayDateCol + " BETWEEN sprayseason." + startDateCol + 
-      " AND sprayseason." + endDateCol + " AND '"+diseaseId+"' = sprayseason."+seasonDiseaseCol+" \n";
-    
-    
+    from += "ON " + abstractSprayTable + "." + sprayDateCol + " BETWEEN sprayseason." + startDateCol
+        + " AND sprayseason." + endDateCol + " AND '" + diseaseId + "' = sprayseason."
+        + seasonDiseaseCol + " \n";
+
     return from;
   }
-  
+
   @Override
   public String where()
   {
     String where = "";
     where += "" + abstractSprayTable + ".id = " + zoneSprayTable + ".id \n";
-    where += "AND "+zoneSprayTable+"."+diseaseCol+" = '"+diseaseId+"' \n";
-    
+    where += "AND " + zoneSprayTable + "." + diseaseCol + " = '" + diseaseId + "' \n";
+
     return where;
   }
-  
+
 }
