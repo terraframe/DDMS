45b1809d4dab914ac1c7108f5ec88a7d9d6c0c0e
jnaifeh@terraframe.com <jnaifeh@terraframe.com>
2010-10-02 16:24:54 +0000

    Fixed cross-product when joining planned and area targets
    git-svn-id: https://subversion.assembla.com/svn/mdss@5532 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: a89d499554407931db7a6ff5bddda9383d9754f0 [formerly 2f9c03d5c6f3d11c9cede0d9d9b519b64af05622]
    Former-commit-id: 63bd9956e37f564682b37d980c36007263592ebc

--- trunk/src/server/dss/vector/solutions/irs/AreaJoin.java
+++ trunk/src/server/dss/vector/solutions/irs/AreaJoin.java
@@ -27,8 +27,8 @@ public class AreaJoin extends TargetJoin implements Reloadable
       String sql = "";
 
       sql += a + " FULL OUTER JOIN " + p + " \n";
-      sql += "ON " + TargetJoin.PLANNED_ALIAS + "." + Alias.GEO_ENTITY + " = "
-          + TargetJoin.ACTUAL_ALIAS + "." + Alias.GEO_ENTITY + " \n";
+//      sql += "ON " + TargetJoin.PLANNED_ALIAS + "." + Alias.GEO_ENTITY + " = "
+//          + TargetJoin.ACTUAL_ALIAS + "." + Alias.GEO_ENTITY + " \n";
       sql += "ON " + TargetJoin.PLANNED_ALIAS + "." + Alias.TARGET_WEEK + " = "
           + TargetJoin.ACTUAL_ALIAS + "." + Alias.TARGET_WEEK + " \n";
       sql += "AND " + TargetJoin.PLANNED_ALIAS + "." + Alias.DISEASE + " = " + TargetJoin.ACTUAL_ALIAS
