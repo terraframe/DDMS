960a79cb64fd808a85d8392fe1b6846420063d81
jsmethie@terraframe.com <jsmethie@terraframe.com>
2010-10-06 17:29:11 +0000

    -Updated Post Install to change the domain of the manager to that of the webapp
    git-svn-id: https://subversion.assembla.com/svn/mdss@5545 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 52a52c832a5bd8e2376ad1cd261ab01eda87cfbd [formerly 562f810331a950cd4c7d939593acd67e0d6d521c]
    Former-commit-id: 1b6160019e75752e11a693a8286c0fdac2b08db9

--- trunk/src/server/dss/vector/solutions/util/PostInstallSetup.java
+++ trunk/src/server/dss/vector/solutions/util/PostInstallSetup.java
@@ -18,13 +18,12 @@ public class PostInstallSetup
     {
       editFile("true", "master", "C:/MDSS/tomcat6/webapps/DDMS/WEB-INF/classes/install.properties");
       editFile("0.mdss.ivcc.com", "domain", "C:/MDSS/tomcat6/webapps/DDMS/WEB-INF/classes/terraframe.properties");
-      editFile("0.mdss.ivcc.com", "domain", "C:/MDSS/tomcat6/webapps/MDSS/WEB-INF/classes/terraframe.properties");
       editFile("0.mdss.ivcc.com", "domain", "C:/MDSS/manager/profiles/manager/common/terraframe.properties");
     }
     else
     {
       editFile("false", "master", "C:/MDSS/tomcat6/webapps/DDMS/WEB-INF/classes/install.properties");
-      editFile(installationNumber + ".mdss.ivcc.com", "domain", "C:/DDMS/tomcat6/webapps/MDSS/WEB-INF/classes/terraframe.properties");
+      editFile(installationNumber + ".mdss.ivcc.com", "domain", "C:/DDMS/tomcat6/webapps/DDMS/WEB-INF/classes/terraframe.properties");
       editFile(installationNumber + ".mdss.ivcc.com", "domain", "C:/MDSS/manager/profiles/manager/common/terraframe.properties");
     }
   }
