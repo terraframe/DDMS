bdadd9dda89fc7fab503c1f93b1a4d0f6f9e9aaf
jsmethie@terraframe.com <jsmethie@terraframe.com>
2010-12-16 17:28:51 +0000

    -Installer test
    git-svn-id: https://subversion.assembla.com/svn/mdss@5685 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 3b9843944e5297d924193ca71410ac2e975da10f [formerly 7322ea12e04717d5afb74a085b28e74ec970a7b3]
    Former-commit-id: 3677e7f7cc9dfbda335e427aaf308b8fa785a292

--- trunk/src/test/dss/vector/solutions/UeberTestSuite.java
+++ trunk/src/test/dss/vector/solutions/UeberTestSuite.java
@@ -4,10 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import dss.vector.solutions.installer.InstallerTest;
 import dss.vector.solutions.selenium.SeleniumTestSuite;
 
 @RunWith(Suite.class)
-@SuiteClasses( {SeleniumTestSuite.class})
+@SuiteClasses( {SeleniumTestSuite.class, InstallerTest.class})
 public class UeberTestSuite
 {
 }
