ef3cccd3f1e4f20fe95c5b144957a351178b5cb7
jsmethie@terraframe.com <jsmethie@terraframe.com>
2011-01-13 22:59:38 +0000

    -Ticket #2267(.5 hours)  * Fixed control flow on MenuItemController
    git-svn-id: https://subversion.assembla.com/svn/mdss@5712 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 67821f1d66ca3ec5e0cf2c609adf91995e76b61d [formerly 0f7b23ee947977edea98ee01cfbaa50758d26304]
    Former-commit-id: 74cdad6358372d811380d4fe0ac2a48243b8a9a7

--- trunk/generated/client/stub/dss/vector/solutions/general/MenuItemController.java
+++ trunk/generated/client/stub/dss/vector/solutions/general/MenuItemController.java
@@ -3,247 +3,292 @@ package dss.vector.solutions.general;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import dss.vector.solutions.MDSSUserDTO;
+import com.runwaysdk.constants.ClientRequestIF;
+
+import dss.vector.solutions.util.AttributeUtil;
+import dss.vector.solutions.util.ErrorUtility;
 import dss.vector.solutions.util.RedirectUtility;
 
 public class MenuItemController extends MenuItemControllerBase implements com.runwaysdk.generation.loader.Reloadable
 {
-  public static final String JSP_DIR = "WEB-INF/dss/vector/solutions/general/MenuItem/";
-  
+  public static final String  JSP_DIR          = "WEB-INF/dss/vector/solutions/general/MenuItem/";
+
   private static final String EDIT_DISEASE_JSP = "editDisease.jsp";
-  
-  public static final String LAYOUT = "/layout.jsp";
-  
-  private static final long serialVersionUID = 492263447;
-  
-  public MenuItemController(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp, java.lang.Boolean isAsynchronous)
+
+  public static final String  LAYOUT           = "/layout.jsp";
+
+  private static final long   serialVersionUID = 492263447;
+
+  public MenuItemController(HttpServletRequest req, HttpServletResponse resp, Boolean isAsynchronous)
   {
     super(req, resp, isAsynchronous, JSP_DIR, LAYOUT);
   }
-  
-  public void cancel(dss.vector.solutions.general.MenuItemDTO dto) throws java.io.IOException, javax.servlet.ServletException
+
+  public void cancel(MenuItemDTO dto) throws IOException, ServletException
   {
     dto.unlock();
     this.view(dto.getId());
   }
-  public void failCancel(dss.vector.solutions.general.MenuItemDTO dto) throws java.io.IOException, javax.servlet.ServletException
+
+  public void failCancel(MenuItemDTO dto) throws IOException, ServletException
   {
     this.edit(dto.getId());
   }
-  public void create(dss.vector.solutions.general.MenuItemDTO dto) throws java.io.IOException, javax.servlet.ServletException
+
+  public void create(MenuItemDTO dto) throws IOException, ServletException
   {
     try
     {
       dto.apply();
       this.view(dto.getId());
     }
-    catch(java.lang.Throwable t)
+    catch (Throwable t)
     {
-      boolean redirect = dss.vector.solutions.util.ErrorUtility.prepareThrowable(t, req, resp, this.isAsynchronous());
+      boolean redirect = ErrorUtility.prepareThrowable(t, req, resp, this.isAsynchronous());
       if (!redirect)
       {
         this.failCreate(dto);
       }
     }
   }
-  public void failCreate(dss.vector.solutions.general.MenuItemDTO dto) throws java.io.IOException, javax.servlet.ServletException
+
+  public void failCreate(MenuItemDTO dto) throws IOException, ServletException
   {
-    req.setAttribute("term", dss.vector.solutions.util.AttributeUtil.getValue(dss.vector.solutions.general.MenuItemDTO.TERM, dto));
-    req.setAttribute("url", dss.vector.solutions.general.SystemURLDTO.getAllInstances(super.getClientSession().getRequest(), "keyName", true, 0, 0).getResultSet());
+    req.setAttribute("term", AttributeUtil.getValue(MenuItemDTO.TERM, dto));
+    req.setAttribute("url", SystemURLDTO.getAllInstances(super.getClientSession().getRequest(), "keyName", true, 0, 0).getResultSet());
     req.setAttribute("item", dto);
     render("createComponent.jsp");
   }
-  public void delete(dss.vector.solutions.general.MenuItemDTO dto) throws java.io.IOException, javax.servlet.ServletException
+
+  public void delete(MenuItemDTO dto) throws IOException, ServletException
   {
     try
     {
       dto.delete();
       this.viewAll();
     }
-    catch(java.lang.Throwable t)
+    catch (Throwable t)
     {
-      boolean redirect = dss.vector.solutions.util.ErrorUtility.prepareThrowable(t, req, resp, this.isAsynchronous());
+      boolean redirect = ErrorUtility.prepareThrowable(t, req, resp, this.isAsynchronous());
       if (!redirect)
       {
         this.failDelete(dto);
       }
     }
   }
-  public void failDelete(dss.vector.solutions.general.MenuItemDTO dto) throws java.io.IOException, javax.servlet.ServletException
+
+  public void failDelete(MenuItemDTO dto) throws IOException, ServletException
   {
-    req.setAttribute("term", dss.vector.solutions.util.AttributeUtil.getValue(dss.vector.solutions.general.MenuItemDTO.TERM, dto));
-    req.setAttribute("url", dss.vector.solutions.general.SystemURLDTO.getAllInstances(super.getClientSession().getRequest(), "keyName", true, 0, 0).getResultSet());
+    req.setAttribute("term", AttributeUtil.getValue(MenuItemDTO.TERM, dto));
+    req.setAttribute("url", SystemURLDTO.getAllInstances(super.getClientSession().getRequest(), "keyName", true, 0, 0).getResultSet());
     req.setAttribute("item", dto);
     render("editComponent.jsp");
   }
-  public void edit(java.lang.String id) throws java.io.IOException, javax.servlet.ServletException
+
+  public void edit(String id) throws IOException, ServletException
   {
     try
     {
-      dss.vector.solutions.general.MenuItemDTO dto = dss.vector.solutions.general.MenuItemDTO.lock(super.getClientRequest(), id);
-      req.setAttribute("term", dss.vector.solutions.util.AttributeUtil.getValue(dss.vector.solutions.general.MenuItemDTO.TERM, dto));
-      req.setAttribute("url", dss.vector.solutions.general.SystemURLDTO.getAllInstances(super.getClientSession().getRequest(), "keyName", true, 0, 0).getResultSet());
+      MenuItemDTO dto = MenuItemDTO.lock(super.getClientRequest(), id);
+      req.setAttribute("term", AttributeUtil.getValue(MenuItemDTO.TERM, dto));
+      req.setAttribute("url", SystemURLDTO.getAllInstances(super.getClientSession().getRequest(), "keyName", true, 0, 0).getResultSet());
       req.setAttribute("item", dto);
       render("editComponent.jsp");
     }
-    catch(java.lang.Throwable t)
+    catch (Throwable t)
     {
-      boolean redirect = dss.vector.solutions.util.ErrorUtility.prepareThrowable(t, req, resp, this.isAsynchronous());
+      boolean redirect = ErrorUtility.prepareThrowable(t, req, resp, this.isAsynchronous());
       if (!redirect)
       {
         this.failEdit(id);
       }
     }
   }
-  public void failEdit(java.lang.String id) throws java.io.IOException, javax.servlet.ServletException
+
+  public void failEdit(String id) throws IOException, ServletException
   {
     this.view(id);
   }
-  public void newInstance() throws java.io.IOException, javax.servlet.ServletException
+
+  public void newInstance() throws IOException, ServletException
   {
     try
     {
-      com.runwaysdk.constants.ClientRequestIF clientRequest = super.getClientRequest();
-      dss.vector.solutions.general.MenuItemDTO dto = new dss.vector.solutions.general.MenuItemDTO(clientRequest);
-      req.setAttribute("term", dss.vector.solutions.util.AttributeUtil.getValue(dss.vector.solutions.general.MenuItemDTO.TERM, dto));
-      req.setAttribute("url", dss.vector.solutions.general.SystemURLDTO.getAllInstances(super.getClientSession().getRequest(), "keyName", true, 0, 0).getResultSet());
+      ClientRequestIF clientRequest = super.getClientRequest();
+      MenuItemDTO dto = new MenuItemDTO(clientRequest);
+      req.setAttribute("term", AttributeUtil.getValue(MenuItemDTO.TERM, dto));
+      req.setAttribute("url", SystemURLDTO.getAllInstances(super.getClientSession().getRequest(), "keyName", true, 0, 0).getResultSet());
       req.setAttribute("item", dto);
       render("createComponent.jsp");
     }
-    catch(java.lang.Throwable t)
+    catch (Throwable t)
     {
-      boolean redirect = dss.vector.solutions.util.ErrorUtility.prepareThrowable(t, req, resp, this.isAsynchronous());
+      boolean redirect = ErrorUtility.prepareThrowable(t, req, resp, this.isAsynchronous());
       if (!redirect)
       {
         this.failNewInstance();
       }
     }
   }
-  public void failNewInstance() throws java.io.IOException, javax.servlet.ServletException
+
+  public void failNewInstance() throws IOException, ServletException
   {
     this.viewAll();
   }
-  public void update(dss.vector.solutions.general.MenuItemDTO dto) throws java.io.IOException, javax.servlet.ServletException
+
+  public void update(MenuItemDTO dto) throws IOException, ServletException
   {
     try
     {
       dto.apply();
       this.view(dto.getId());
     }
-    catch(java.lang.Throwable t)
+    catch (Throwable t)
     {
-      boolean redirect = dss.vector.solutions.util.ErrorUtility.prepareThrowable(t, req, resp, this.isAsynchronous());
+      boolean redirect = ErrorUtility.prepareThrowable(t, req, resp, this.isAsynchronous());
       if (!redirect)
       {
         this.failUpdate(dto);
       }
     }
   }
-  public void failUpdate(dss.vector.solutions.general.MenuItemDTO dto) throws java.io.IOException, javax.servlet.ServletException
+
+  public void failUpdate(MenuItemDTO dto) throws IOException, ServletException
   {
-    req.setAttribute("term", dss.vector.solutions.util.AttributeUtil.getValue(dss.vector.solutions.general.MenuItemDTO.TERM, dto));
-    req.setAttribute("url", dss.vector.solutions.general.SystemURLDTO.getAllInstances(super.getClientSession().getRequest(), "keyName", true, 0, 0).getResultSet());
+    req.setAttribute("term", AttributeUtil.getValue(MenuItemDTO.TERM, dto));
+    req.setAttribute("url", SystemURLDTO.getAllInstances(super.getClientSession().getRequest(), "keyName", true, 0, 0).getResultSet());
     req.setAttribute("item", dto);
     render("editComponent.jsp");
   }
-  public void view(java.lang.String id) throws java.io.IOException, javax.servlet.ServletException
+
+  public void view(String id) throws IOException, ServletException
   {
     try
     {
-      dss.vector.solutions.util.RedirectUtility utility = new dss.vector.solutions.util.RedirectUtility(req, resp);
+      RedirectUtility utility = new RedirectUtility(req, resp);
       utility.put("id", id);
       utility.checkURL(this.getClass().getSimpleName(), "view");
-      com.runwaysdk.constants.ClientRequestIF clientRequest = super.getClientRequest();
-      dss.vector.solutions.general.MenuItemDTO dto = dss.vector.solutions.general.MenuItemDTO.get(clientRequest, id);
-      req.setAttribute("term", dss.vector.solutions.util.AttributeUtil.getValue(dss.vector.solutions.general.MenuItemDTO.TERM, dto));
-      req.setAttribute("url", dss.vector.solutions.general.SystemURLDTO.getAllInstances(super.getClientSession().getRequest(), "keyName", true, 0, 0).getResultSet());
+      ClientRequestIF clientRequest = super.getClientRequest();
+      MenuItemDTO dto = MenuItemDTO.get(clientRequest, id);
+      req.setAttribute("term", AttributeUtil.getValue(MenuItemDTO.TERM, dto));
+      req.setAttribute("url", SystemURLDTO.getAllInstances(super.getClientSession().getRequest(), "keyName", true, 0, 0).getResultSet());
       req.setAttribute("item", dto);
       render("viewComponent.jsp");
     }
-    catch(java.lang.Throwable t)
+    catch (Throwable t)
     {
-      boolean redirect = dss.vector.solutions.util.ErrorUtility.prepareThrowable(t, req, resp, this.isAsynchronous());
+      boolean redirect = ErrorUtility.prepareThrowable(t, req, resp, this.isAsynchronous());
       if (!redirect)
       {
         this.failView(id);
       }
     }
   }
-  public void failView(java.lang.String id) throws java.io.IOException, javax.servlet.ServletException
+
+  public void failView(String id) throws IOException, ServletException
   {
     this.viewAll();
   }
-  public void viewAll() throws java.io.IOException, javax.servlet.ServletException
+
+  public void viewAll() throws IOException, ServletException
   {
     new RedirectUtility(req, resp).checkURL(this.getClass().getSimpleName(), "viewAll");
 
     DiseaseDTO disease = DiseaseDTO.getCurrent(this.getClientRequest());
-    
+
     req.setAttribute("disease", disease);
-    
-    com.runwaysdk.constants.ClientRequestIF clientRequest = super.getClientRequest();
-    dss.vector.solutions.general.MenuItemViewQueryDTO query = dss.vector.solutions.general.MenuItemViewDTO.getViewsForDisease(clientRequest);
+
+    ClientRequestIF clientRequest = super.getClientRequest();
+    MenuItemViewQueryDTO query = MenuItemViewDTO.getViewsForDisease(clientRequest);
     req.setAttribute("query", query);
     render("viewAllComponent.jsp");
   }
-  public void failViewAll() throws java.io.IOException, javax.servlet.ServletException
+
+  public void failViewAll() throws IOException, ServletException
   {
     resp.sendError(500);
   }
-  
+
   @Override
   public void editDisease(String id) throws IOException, ServletException
   {
-    DiseaseDTO dto = DiseaseDTO.get(this.getClientRequest(), id);
-    editDisease(dto);
+    try
+    {
+      editDisease(DiseaseDTO.get(this.getClientRequest(), id));
+    }
+    catch (Throwable t)
+    {
+      boolean redirect = ErrorUtility.prepareThrowable(t, req, resp, this.isAsynchronous());
+
+      if (!redirect)
+      {
+        this.failEditDisease(id);
+      }
+    }
   }
-  
+
+  @Override
+  public void failEditDisease(String id) throws IOException, ServletException
+  {
+    req.getRequestDispatcher("/index.jsp").forward(req, resp);
+  }
+
   private void editDisease(DiseaseDTO dto) throws IOException, ServletException
   {
     dto.lock();
-    
+
     req.setAttribute("term", dto.getMenuRoot());
     req.setAttribute("item", dto);
-    
+
     render(EDIT_DISEASE_JSP);
   }
-  
+
   @Override
   public void updateDisease(DiseaseDTO dto) throws IOException, ServletException
   {
     try
     {
       dto.apply();
-    
+
       this.viewAll();
     }
-    catch(java.lang.Throwable t)
+    catch (Throwable t)
     {
-      boolean redirect = dss.vector.solutions.util.ErrorUtility.prepareThrowable(t, req, resp, this.isAsynchronous());
+      boolean redirect = ErrorUtility.prepareThrowable(t, req, resp, this.isAsynchronous());
+
       if (!redirect)
       {
-        this.editDisease(dto);
+        this.failUpdateDisease(dto);
       }
     }
   }
   
   @Override
+  public void failUpdateDisease(DiseaseDTO dto) throws IOException, ServletException
+  {
+    this.editDisease(dto);
+  }
+
+  @Override
   public void cancelDisease(String id) throws IOException, ServletException
   {
     DiseaseDTO.unlock(this.getClientRequest(), id);
-    
+
     this.viewAll();
   }
-  
-  public void viewPage(java.lang.String sortAttribute, java.lang.Boolean isAscending, java.lang.Integer pageSize, java.lang.Integer pageNumber) throws java.io.IOException, javax.servlet.ServletException
+
+  public void viewPage(String sortAttribute, Boolean isAscending, Integer pageSize, Integer pageNumber) throws IOException, ServletException
   {
-    com.runwaysdk.constants.ClientRequestIF clientRequest = super.getClientRequest();
-    dss.vector.solutions.general.MenuItemQueryDTO query = dss.vector.solutions.general.MenuItemDTO.getAllInstances(clientRequest, sortAttribute, isAscending, pageSize, pageNumber);
+    ClientRequestIF clientRequest = super.getClientRequest();
+    MenuItemQueryDTO query = MenuItemDTO.getAllInstances(clientRequest, sortAttribute, isAscending, pageSize, pageNumber);
     req.setAttribute("query", query);
     render("viewAllComponent.jsp");
   }
-  public void failViewPage(java.lang.String sortAttribute, java.lang.String isAscending, java.lang.String pageSize, java.lang.String pageNumber) throws java.io.IOException, javax.servlet.ServletException
+
+  public void failViewPage(String sortAttribute, String isAscending, String pageSize, String pageNumber) throws IOException, ServletException
   {
     resp.sendError(500);
   }
