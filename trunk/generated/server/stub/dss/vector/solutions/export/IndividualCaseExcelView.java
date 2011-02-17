ed2b82e8a7f141ac40381835db5a2938f6f7982b
egrunzke@terraframe.com <egrunzke@terraframe.com>
2011-02-17 21:01:34 +0000

    #2326 - Changed calculated date of birth to always be January 1st
    git-svn-id: https://subversion.assembla.com/svn/mdss@5843 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: e268c5bcd2d4b138acd0df4b935e1dde8884339e [formerly 55dcd7f79dcc1c11f07074787a882640a185dc3d]
    Former-commit-id: 33af5b2dacd0c5fd49d829b7205d5c69ebefee40

--- trunk/generated/server/stub/dss/vector/solutions/export/IndividualCaseExcelView.java
+++ trunk/generated/server/stub/dss/vector/solutions/export/IndividualCaseExcelView.java
@@ -8,14 +8,11 @@ import java.util.Locale;
 
 import com.runwaysdk.dataaccess.io.ExcelExporter;
 import com.runwaysdk.dataaccess.io.ExcelImporter.ImportContext;
-import com.runwaysdk.dataaccess.metadata.MdTypeDAO;
 import com.runwaysdk.dataaccess.transaction.Transaction;
 import com.runwaysdk.query.OIterator;
 import com.runwaysdk.query.QueryFactory;
 import com.runwaysdk.session.Session;
-import com.runwaysdk.system.metadata.MdType;
 
-import dss.vector.solutions.Patient;
 import dss.vector.solutions.Person;
 import dss.vector.solutions.PersonQuery;
 import dss.vector.solutions.PersonView;
@@ -172,11 +169,13 @@ public class IndividualCaseExcelView extends IndividualCaseExcelViewBase impleme
     {
       Calendar c = Calendar.getInstance();
       c.add(Calendar.YEAR, -1 * age2);
+      // Ticket #2326 says dob should be January 1st
+      c.set(Calendar.DAY_OF_YEAR, 1);
       this.setDateOfBirth(c.getTime());
     }
     else if (dob!=null && diagnosis!=null && age2==null)
     {
-      // We subtract from diagnosis date because we want to knwo the age at the time of diagnosis, not the age now
+      // We subtract from diagnosis date because we want to know the age at the time of diagnosis, not the age now
       long difference = diagnosis.getTime() - dob.getTime();
       // Divide by the number of milliseconds in a year
       long age = difference / 31556926000l;
