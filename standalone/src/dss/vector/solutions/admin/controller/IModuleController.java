86f63cf30d5cbae6a4befc59384d9eff30193e28
jsmethie@terraframe.com <jsmethie@terraframe.com>
2011-03-29 16:45:45 +0000

    -Updated jars to support deleting of existing files before restoring the webapp after an import
    git-svn-id: https://subversion.assembla.com/svn/mdss@5967 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: d4315398e9a35d36070b2e76296a230994a246f0 [formerly 5d3c2d28527dc405d47050d444ed6a6b9a74da6a]
    Former-commit-id: 08a490ac393c3bc03797dc9fd171d5827c6bd8f8

--- standalone/src/dss/vector/solutions/admin/controller/IModuleController.java
+++ standalone/src/dss/vector/solutions/admin/controller/IModuleController.java
@@ -9,11 +9,11 @@ public interface IModuleController
   public void addListener(IControllerListener listener);
 
   public void removeListener(IControllerListener listener);
-  
+
   public Integer getTimeout();
-  
+
   public boolean setTimeout(Integer timeout);
-  
+
   public void start(Integer timeout);
 
   public void stop();
@@ -21,16 +21,16 @@ public interface IModuleController
   public void backup(File file);
 
   public void restore(File file);
-  
+
   public void refresh();
 
   public void setLogLevel(LogLevel level);
-  
+
   public LogLevel getLogLevel();
-  
+
   public boolean isServerUp();
 
-  public void rebuildGeoPaths();  
+  public void rebuildGeoPaths();
 
-  public void rebuildTermPaths();  
+  public void rebuildTermPaths();
 }
--- standalone/src/dss/vector/solutions/admin/controller/SlaveConfiguration.java
+++ standalone/src/dss/vector/solutions/admin/controller/SlaveConfiguration.java
@@ -1,5 +1,11 @@
 package dss.vector.solutions.admin.controller;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import com.runwaysdk.constants.DeployProperties;
 import com.runwaysdk.manager.controller.ConfigurationAdapter;
 
 public class SlaveConfiguration extends ConfigurationAdapter
@@ -9,10 +15,49 @@ public class SlaveConfiguration extends ConfigurationAdapter
   {
     return true;
   }
-  
+
   @Override
   public boolean getImportApplicationFiles()
   {
     return true;
+  }
+
+  @Override
+  public Collection<String> getFilesToDeleteOnImport()
+  {
+    Collection<String> paths = new LinkedList<String>();
+
+    FileFilter filter = new FileFilter()
+    {
+      @Override
+      public boolean accept(File file)
+      {
+        String path = file.getAbsolutePath();
+        return ! ( path.contains(".svn") || path.contains(".properties") || path.contains(".xml") || path.contains(".xsd") || path.contains(".tld") );
+      }
+    };
+
+    File classesDirectory = new File(DeployProperties.getJspDir() + File.separator + "classes");
+    File sourceDirectory = new File(DeployProperties.getJspDir() + File.separator + "source");
+    File libDirectory = new File(DeployProperties.getJspDir() + File.separator + "lib");
+
+    addFiles(paths, filter, classesDirectory);
+    addFiles(paths, filter, sourceDirectory);
+    addFiles(paths, filter, libDirectory);
+
+    return paths;
+  }
+
+  private void addFiles(Collection<String> paths, FileFilter filter, File directory)
+  {
+    File root = new File(DeployProperties.getDeployPath());
+    String rootPath = root.getAbsolutePath();
+
+    File[] files = directory.listFiles(filter);
+
+    for (File file : files)
+    {
+      paths.add(file.getAbsolutePath().replace(rootPath, ""));
+    }
   }
 }
