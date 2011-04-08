3837f98bf5a04e06f6d408e7ccae66770d5cf33e
egrunzke@terraframe.com <egrunzke@terraframe.com>
2011-04-08 20:55:47 +0000

    Parameterized LocalizationMerger
    git-svn-id: https://subversion.assembla.com/svn/mdss@6022 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: e6c73b001c22a336e97f3a7200e8cd68d3297e78 [formerly 77a5a1b73957ebcd3686e95d759a836b506a2939]
    Former-commit-id: a35c9bfe2d17ad7a1c58baf5080d84a0d6dc78aa

--- trunk/src/server/dss/vector/solutions/localization/LocalizationMerger.java
+++ trunk/src/server/dss/vector/solutions/localization/LocalizationMerger.java
@@ -19,8 +19,23 @@ public class LocalizationMerger
   public static void main(String[] args) throws Exception
   {
     long start = System.currentTimeMillis();
-    
     File dir = new File("doc/localization/");
+    File mergedFile = new File("doc/MergedLocalization.xls");
+    
+    switch (args.length)
+    {
+      case 2:
+        mergedFile = new File(args[1]);
+      case 1:
+        dir = new File(args[0]);
+      case 0:
+        break;
+      default:
+        System.out.println("Usage: LocalizationMerger [dir] [outfile]");
+        System.out.println("  -dir: The directory of localization files to merge");
+        System.out.println("  -outfile: The output file");
+    }
+    
     File[] files = dir.listFiles();
     Comparator<File> c = new Comparator<File>()
     {
@@ -34,19 +49,18 @@ public class LocalizationMerger
     List<File> list = Arrays.asList(files);
     Collections.sort(list, c);
     
-    System.out.println("Importing files:");
+    System.out.println("Importing files from " + dir.getAbsolutePath());
     for (File file : list)
     {
-      System.out.println(file.getName());
+      System.out.println("  " + file.getName());
       MdssLocalizationImporter mli = new MdssLocalizationImporter();
       mli.read(new FileInputStream(file));
     }
     
-    String mergedFileName = "doc/MergedLocalization.xls";
-    System.out.println("Exporting Merged file to " + mergedFileName);
+    System.out.println("Exporting Merged file to " + mergedFile.getAbsolutePath());
     MdssLocalizationExporter exporter = new MdssLocalizationExporter();
     exporter.export();
-    FileIO.write(mergedFileName, exporter.write());
+    FileIO.write(mergedFile, exporter.write());
     
     long stop = System.currentTimeMillis();
     System.out.println("Execution time: " + ( stop - start ) / 1000.0 + " seconds");
