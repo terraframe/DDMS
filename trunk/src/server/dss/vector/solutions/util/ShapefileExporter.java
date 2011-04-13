1f652ddea1c833cb568b38dd59ec354d6f851885
jnaifeh@terraframe.com <jnaifeh@terraframe.com>
2011-04-13 19:14:11 +0000

    Fixed pathing issue for shapefile export
    git-svn-id: https://subversion.assembla.com/svn/mdss@6030 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: d4d41551850ac74e5a15dbf94c71119976473700 [formerly 38918b6e4d53b6bdc3d9261c1a985c48986543a5]
    Former-commit-id: 3f9c5587698785451fba7c83567021c29c9b0192

--- trunk/src/server/dss/vector/solutions/util/ShapefileExporter.java
+++ trunk/src/server/dss/vector/solutions/util/ShapefileExporter.java
@@ -20,132 +20,170 @@ import dss.vector.solutions.MdssLog;
 import dss.vector.solutions.query.Layer;
 import dss.vector.solutions.query.QueryConstants;
 
-public class ShapefileExporter implements Reloadable {
-	private static final String SHAPEFILE_EXTRACTION_CMD = "pgsql2shp";
-	private static final int BUFFER_SIZE = 4096;
+public class ShapefileExporter implements Reloadable
+{
+  private static final String SHAPEFILE_EXTRACTION_CMD = "pgsql2shp";
 
-	public void export(List<Layer> layers, OutputStream output) {
-		File dir = this.createTempDir("shapefile");
-		if (dir != null) {
-			for (Layer layer : layers) {
+  private static final int    BUFFER_SIZE              = 4096;
 
-				String name = layer.getLayerName();
-				String dbView = layer.getViewName();
+  public void export(List<Layer> layers, OutputStream output)
+  {
+    File dir = this.createTempDir("shapefile");
+    if (dir != null)
+    {
+      for (Layer layer : layers)
+      {
 
-				this.pgsql2shp(dir, name, dbView);
-			}
-			this.zipDir(dir, output);
-		}
-	}
+        String name = layer.getLayerName();
+        String dbView = layer.getViewName();
 
-	private void zipDir(File dir, OutputStream output) {
-		ZipOutputStream zos = new ZipOutputStream(output);
+        this.pgsql2shp(dir, name, dbView);
+      }
+      this.zipDir(dir, output);
+    }
+  }
 
-		byte[] buffer = new byte[BUFFER_SIZE];
+  private void zipDir(File dir, OutputStream output)
+  {
+    ZipOutputStream zos = new ZipOutputStream(output);
 
-		String[] dirList = dir.list();
+    byte[] buffer = new byte[BUFFER_SIZE];
 
-		int count = 0;
-		try {
-			for (int i = 0; i < dirList.length; i++) {
-				File f = new File(dir, dirList[i]);
-				FileInputStream fis = new FileInputStream(f);
-				zos.putNextEntry(new ZipEntry(f.getName()));
-				while ((count = fis.read(buffer)) != -1) {
-					zos.write(buffer, 0, count);
-				}
-			}
-			zos.finish();
-		} catch (FileNotFoundException e) {
-			// This should be impossible, since we're iterating over a directory
-			// listing
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-		}
-	}
+    String[] dirList = dir.list();
 
-	private synchronized File createTempDir(String prefix) {
-		File dir = null;
+    int count = 0;
+    try
+    {
+      for (int i = 0; i < dirList.length; i++)
+      {
+        File f = new File(dir, dirList[i]);
+        FileInputStream fis = new FileInputStream(f);
+        zos.putNextEntry(new ZipEntry(f.getName()));
+        while ( ( count = fis.read(buffer) ) != -1)
+        {
+          zos.write(buffer, 0, count);
+        }
+      }
+      zos.finish();
+    }
+    catch (FileNotFoundException e)
+    {
+      // This should be impossible, since we're iterating over a directory
+      // listing
+    }
+    catch (IOException e)
+    {
+      e.printStackTrace();
+    }
+    finally
+    {
+    }
+  }
 
-		try {
-			dir = File.createTempFile(prefix, "");
-			dir.delete(); // Delete the tempfile
-			dir.mkdir(); // Recreate it as a directory
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+  private synchronized File createTempDir(String prefix)
+  {
+    File dir = null;
 
-		return dir;
-	}
+    try
+    {
+      dir = File.createTempFile(prefix, "");
+      dir.delete(); // Delete the tempfile
+      dir.mkdir(); // Recreate it as a directory
+    }
+    catch (IOException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
 
-	private void pgsql2shp(File dir, String name, String viewName) {
-	    ArrayList<String> args = new ArrayList<String>();
-	    
-		args.add(DatabaseProperties.getDatabaseBinDirectory() + SHAPEFILE_EXTRACTION_CMD);
-		
-		// Output file name
-		args.add("-f");
-		args.add(name);
-		
-		// Column containing geodata
-		args.add("-g");
-		args.add(QueryConstants.GEOMETRY_NAME_COLUMN);
-		
-		// Host / port
-		args.add("-h");
-	    args.add(DatabaseProperties.getServerName());
-	    args.add("-p");
-	    args.add(Integer.toString(DatabaseProperties.getPort()));
+    return dir;
+  }
 
-	    // Username / password
-	    args.add("-u");
-	    args.add(DatabaseProperties.getUser());
-		args.add( "-P");
-	    args.add(DatabaseProperties.getPassword());
+  private void pgsql2shp(File dir, String name, String viewName)
+  {
+    ArrayList<String> args = new ArrayList<String>();
 
-		// Database name
-	    args.add(DatabaseProperties.getDatabaseName());
-	    
-	    // View/table name (or SQL)
-		args.add(viewName);
+    String bin = DatabaseProperties.getDatabaseBinDirectory();
+    if (!bin.endsWith(File.separator))
+    {
+      bin += File.separator;
+    }
 
-	    ProcessBuilder pb = new ProcessBuilder(args);
-	    pb.directory(dir);
-		String output = this.run(pb);
-	}
+    args.add(bin + SHAPEFILE_EXTRACTION_CMD);
 
-	private String run(ProcessBuilder pb) {
-		this.print(pb);
-		StringBuilder sb = new StringBuilder();
+    // Output file name
+    args.add("-f");
+    args.add(name);
 
-		String line = null;
-		int exitVal = 0;
-		try {
-			Process pr = pb.start();
-			BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-			while ((line = input.readLine()) != null) {
-				sb.append(line + "\n");
-			}
-			exitVal = pr.waitFor();
-		} catch (IOException e) {
-			throw new ProgrammingErrorException(e);
-		} catch (InterruptedException e) {
-			// Do nothing);
-		}
+    // Column containing geodata
+    args.add("-g");
+    args.add(QueryConstants.GEOMETRY_NAME_COLUMN);
 
-		if (exitVal != 0) {
-			return sb.toString();
-		} else {
-			return null;
-		}
-	}
+    // Host / port
+    args.add("-h");
+    args.add(DatabaseProperties.getServerName());
+    args.add("-p");
+    args.add(Integer.toString(DatabaseProperties.getPort()));
 
-	private void print(ProcessBuilder pb) {
-	    String message = new String();
-		for (String cmd: pb.command()) {
-		    message += cmd + "\n";
-		}
-	}
+    // Username / password
+    args.add("-u");
+    args.add(DatabaseProperties.getUser());
+    args.add("-P");
+    args.add(DatabaseProperties.getPassword());
+
+    // Database name
+    args.add(DatabaseProperties.getDatabaseName());
+
+    // View/table name (or SQL)
+    args.add(viewName);
+
+    ProcessBuilder pb = new ProcessBuilder(args);
+    pb.directory(dir);
+    String output = this.run(pb);
+  }
+
+  private String run(ProcessBuilder pb)
+  {
+    this.print(pb);
+    StringBuilder sb = new StringBuilder();
+
+    String line = null;
+    int exitVal = 0;
+    try
+    {
+      Process pr = pb.start();
+      BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+      while ( ( line = input.readLine() ) != null)
+      {
+        sb.append(line + "\n");
+      }
+      exitVal = pr.waitFor();
+    }
+    catch (IOException e)
+    {
+      throw new ProgrammingErrorException(e);
+    }
+    catch (InterruptedException e)
+    {
+      // Do nothing);
+    }
+
+    if (exitVal != 0)
+    {
+      return sb.toString();
+    }
+    else
+    {
+      return null;
+    }
+  }
+
+  private void print(ProcessBuilder pb)
+  {
+    String message = new String();
+    for (String cmd : pb.command())
+    {
+      message += cmd + "\n";
+    }
+  }
 }
