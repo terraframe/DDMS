c88c94fb7a2639ba520cc3d5fac40ef38cc1d969
egrunzke@terraframe.com <egrunzke@terraframe.com>
2011-07-08 21:27:45 +0000

    Updated for multi-install
    git-svn-id: https://subversion.assembla.com/svn/mdss@6225 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: ef06223c738075ebb02104f3c7f1d7d28c64c486 [formerly c1bd8e37899a84b02c6f7295ba9ee74afe54e8c5]
    Former-commit-id: c47b6ce492bdca5124e4bcc126f289f1b5d62d1a

--- trunk/src/server/dss/vector/solutions/util/PostInstallSetup.java
+++ trunk/src/server/dss/vector/solutions/util/PostInstallSetup.java
@@ -2,35 +2,96 @@ package dss.vector.solutions.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.AccessController;
+
+import sun.security.action.GetPropertyAction;
 
 import com.runwaysdk.util.FileIO;
 
+/**
+ * Called at the end of NSIS installation.  Edits the user-supplied app name into all appropriate files
+ * 
+ * @author Eric Grunzke
+ */
 public class PostInstallSetup
 {
   public static void main(String[] args) throws Exception
   {
-    String installationNumber = args[0];
-    editFile(installationNumber + ".mdss.ivcc.com", "domain", "C:/MDSS/tomcat6/webapps/DDMS/WEB-INF/classes/terraframe.properties");
+    String appName = args[0];
+    String installationNumber = args[1];
+    Boolean isMaster = Boolean.parseBoolean(args[2]);
     
-    Boolean isMaster = Boolean.parseBoolean(args[1]);
+    PostInstallSetup setup = new PostInstallSetup(appName, installationNumber, isMaster);
+    setup.go();
+  }
+  
+  private File appRoot;
+  private File classes;
+  private String appName;
+  private String installationNumber;
+  private Boolean isMaster;
+  
+  public PostInstallSetup(String appName, String installationNumber, Boolean isMaster)
+  {
+    this.appName = appName;
+    this.installationNumber = installationNumber;
+    this.isMaster = isMaster;
+    appRoot = new File("C:/MDSS/tomcat6/webapps/" + appName);
+    classes = new File(appRoot, "WEB-INF/classes");
+  }
+  
+  public void go() throws IOException
+  {
+    String domain = installationNumber + ".mdss.ivcc.com";
+    File manager = new File("C:/MDSS/manager/profiles/manager/common/terraframe.properties");
+    String lineSeparator = (String) AccessController.doPrivileged(new GetPropertyAction("line.separator"));
     
+    // Update property files
+    editWebappProperty("domain", domain, "terraframe.properties");
+    editWebappProperty("databaseName", appName, "database.properties");
     if (isMaster)
     {
-      editFile("true", "master", "C:/MDSS/tomcat6/webapps/DDMS/WEB-INF/classes/install.properties");
-      editFile("0.mdss.ivcc.com", "domain", "C:/MDSS/tomcat6/webapps/DDMS/WEB-INF/classes/terraframe.properties");
-      editFile("0.mdss.ivcc.com", "domain", "C:/MDSS/manager/profiles/manager/common/terraframe.properties");
+      editWebappProperty("master", "true", "install.properties");
+      editWebappProperty("domain", "0.mdss.ivcc.com", "terraframe.properties");
+      editProperty("domain", "0.mdss.ivcc.com", manager);
     }
     else
     {
-      editFile("false", "master", "C:/MDSS/tomcat6/webapps/DDMS/WEB-INF/classes/install.properties");
-      editFile(installationNumber + ".mdss.ivcc.com", "domain", "C:/MDSS/tomcat6/webapps/DDMS/WEB-INF/classes/terraframe.properties");
-      editFile(installationNumber + ".mdss.ivcc.com", "domain", "C:/MDSS/manager/profiles/manager/common/terraframe.properties");
+      editWebappProperty("master", "false", "install.properties");
+      editWebappProperty("domain", domain, "terraframe.properties");
+      editProperty("domain", domain, manager);
     }
+    
+    // Update css and jsps
+    String template = "/DDMS/";
+    String replacer = "/" + appName + "/";
+    readAndReplace(new File(appRoot, "login.jsp"), template, replacer);
+    readAndReplace(new File(appRoot, "css/style.css"), template, replacer);
+    readAndReplace(new File(appRoot, "test/viewComponentOldWay.jsp"), template, replacer);
+    
+    // Add this app to the list in applications.txt
+    File applications = new File("C:/MDSS/manager/manager-1.0.0/classes/applications.txt");
+    String appTxt = FileIO.readString(applications);
+    appTxt += lineSeparator + appName;
+    FileIO.write(applications, appTxt);
+    
+    // And give common.properties a unique rmi.port
+    int appCount = appTxt.split("\\s+").length;
+    editWebappProperty("rmi.port", Integer.toString(1099 - appCount), "common.properties");
+    
+    // Need to update tomcat RAM
+    File startup = new File("C:/MDSS/tomcat6/bin/startup.bat");
+    int totalMemory = 512 * appCount;
+    readAndReplace(startup, "-Xmx\\d*M", "-Xmx" + totalMemory + "M");
   }
   
-  private static void editFile(String newValue, String key, String path) throws IOException
+  public void editWebappProperty(String key, String newValue, String bundle) throws IOException
   {
-    File file = new File(path);
+    editProperty(key, newValue, new File(classes, bundle));
+  }
+  
+  private void editProperty(String key, String newValue, File file) throws IOException
+  {
     String data = new String();
     for (String line : FileIO.readLines(file))
     {
@@ -45,5 +106,12 @@ public class PostInstallSetup
       data += "\n";
     }
     FileIO.write(file, data);
+  }
+  
+  private void readAndReplace(File file, String regex, String replacement) throws IOException
+  {
+    String data = FileIO.readString(file);
+    String replaced = data.replaceAll(regex, replacement);
+    FileIO.write(file, replaced);
   }
 }
