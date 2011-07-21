c687cbd52db85d6f932808161bc8afe42c79bf12
egrunzke@terraframe.com <egrunzke@terraframe.com>
2011-07-21 22:33:56 +0000

    Added reflection to ContextListenerManager and removed Reloadable
    git-svn-id: https://subversion.assembla.com/svn/mdss@6277 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: e6766bd653c09b13dd5966ad4e2ede093fcc7792 [formerly 517baba6ef4dfc158054e8207f092ec0de557354]
    Former-commit-id: aac8164bac558248b5e3d11aff62092a5a0e84b4

--- trunk/src/server/dss/vector/solutions/ContextListenerManager.java
+++ trunk/src/server/dss/vector/solutions/ContextListenerManager.java
@@ -1,30 +1,50 @@
 package dss.vector.solutions;
 
+import java.lang.reflect.InvocationTargetException;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import com.runwaysdk.defaults.RMIContextListener;
-import com.runwaysdk.generation.loader.Reloadable;
-import com.runwaysdk.util.ClientInitializer;
-
-import dss.vector.solutions.general.EmailContextListener;
+import com.runwaysdk.business.generation.LoaderDecoratorException;
+import com.runwaysdk.generation.loader.LoaderDecorator;
 
 
 
-public class ContextListenerManager implements ServletContextListener, Reloadable
+public class ContextListenerManager implements ServletContextListener
 {
-  RMIContextListener  rMIContextListener;
-  EmailContextListener emailContextListener;
-  ClientInitializer clientInitializer;
-  CleanupContextListener cleanupContextListener;
+  ServletContextListener rMIContextListener;
+  ServletContextListener emailContextListener;
+  ServletContextListener clientInitializer;
+  ServletContextListener cleanupContextListener;
+  
+  private static final String RMI = "com.runwaysdk.defaults.RMIContextListener";
+  private static final String EMAIL = "dss.vector.solutions.general.EmailContextListener";
+  private static final String CLIENT = "com.runwaysdk.util.ClientInitializer";
+  private static final String CLEANUP = "dss.vector.solutions.CleanupContextListener";
   
   
   public ContextListenerManager()
   {
-    rMIContextListener = new RMIContextListener();
-    clientInitializer = new ClientInitializer();
-    cleanupContextListener = new CleanupContextListener();
-    emailContextListener = new EmailContextListener();
+//    rMIContextListener = new RMIContextListener();
+//    clientInitializer = new ClientInitializer();
+//    cleanupContextListener = new CleanupContextListener();
+//    emailContextListener = new EmailContextListener();
+    
+    try
+    {
+      rMIContextListener = (ServletContextListener)LoaderDecorator.load(RMI).getConstructor().newInstance();
+      clientInitializer = (ServletContextListener)LoaderDecorator.load(CLIENT).getConstructor().newInstance();
+      cleanupContextListener = (ServletContextListener)LoaderDecorator.load(CLEANUP).getConstructor().newInstance();
+      emailContextListener = (ServletContextListener)LoaderDecorator.load(EMAIL).getConstructor().newInstance();
+    }
+    catch (InvocationTargetException ite)
+    {
+      throw new LoaderDecoratorException("InvocationTargetException initializing Context Listeners", ite.getTargetException());
+    }
+    catch (Exception e)
+    {
+      throw new LoaderDecoratorException("Exception initializing Context Listeners", e);
+    }
   }
   
   
