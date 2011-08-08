5756a71889340e84c87c31e14cd85294ea887a08
jsmethie@terraframe.com <jsmethie@terraframe.com>
2011-08-08 17:18:00 +0000

    -Added error handling in the PostInstallSetup
    git-svn-id: https://subversion.assembla.com/svn/mdss@6301 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 4297863e4d66ae6e7b4879d39d68e62e5af22107 [formerly cad672f5743bca12ad61b6ee01139055f2d321e8]
    Former-commit-id: 5559c66e452ae73e22d728c0eea7f9935eb0da47

--- trunk/src/server/dss/vector/solutions/util/PostInstallSetup.java
+++ trunk/src/server/dss/vector/solutions/util/PostInstallSetup.java
@@ -9,44 +9,57 @@ import sun.security.action.GetPropertyAction;
 import com.runwaysdk.util.FileIO;
 
 /**
- * Called at the end of NSIS installation.  Edits the user-supplied app name into all appropriate files
+ * Called at the end of NSIS installation. Edits the user-supplied app name into
+ * all appropriate files
  * 
  * @author Eric Grunzke
  */
 public class PostInstallSetup
 {
-  public static void main(String[] args) throws Exception
-  {
-    String appName = args[0];
-    String installationNumber = args[1];
-    Boolean isMaster = Boolean.parseBoolean(args[2]);
-    
-    PostInstallSetup setup = new PostInstallSetup(appName, installationNumber, isMaster);
-    setup.go();
-  }
-  
-  private File appRoot;
-  private File classes;
-  private String appName;
-  private String dbName;
-  private String installationNumber;
-  private Boolean isMaster;
-  
+  /**
+   * Exit status for the success case. NSIS considers any non-zero exist status
+   * as an error, as such the SUCCESS exist status must be 0.
+   */
+  public static int SUCCESS = 0;
+
+  /**
+   * Exit status for the failure case.
+   */
+  public static int FAILURE = -1;
+
+  private File      appRoot;
+
+  private File      classes;
+
+  private String    appName;
+
+  private String    dbName;
+
+  private String    installationNumber;
+
+  private Boolean   isMaster;
+
+  /**
+   * Exit status of the setup
+   */
+  private int       exitStatus;
+
   public PostInstallSetup(String appName, String installationNumber, Boolean isMaster)
   {
     this.appName = appName;
     this.dbName = appName.toLowerCase();
     this.installationNumber = installationNumber;
     this.isMaster = isMaster;
-    appRoot = new File("C:/MDSS/tomcat6/webapps/" + appName);
-    classes = new File(appRoot, "WEB-INF/classes");
+    this.appRoot = new File("C:/MDSS/tomcat6/webapps/" + appName);
+    this.classes = new File(appRoot, "WEB-INF/classes");
+    this.exitStatus = PostInstallSetup.SUCCESS;
   }
-  
+
   public void go() throws IOException
   {
     String domain = installationNumber + ".mdss.ivcc.com";
     String lineSeparator = (String) AccessController.doPrivileged(new GetPropertyAction("line.separator"));
-    
+
     // Update property files
     editWebappProperty("deploy.appname", appName, "terraframe.properties");
     editWebappProperty("databaseName", dbName, "database.properties");
@@ -60,51 +73,81 @@ public class PostInstallSetup
       editWebappProperty("master", "false", "install.properties");
       editWebappProperty("domain", domain, "terraframe.properties");
     }
-    
+
     // Update css and jsps
     String template = "/DDMS/";
     String replacer = "/" + appName + "/";
     readAndReplace(new File(appRoot, "login.jsp"), template, replacer);
     readAndReplace(new File(appRoot, "css/style.css"), template, replacer);
     readAndReplace(new File(appRoot, "test/viewComponentOldWay.jsp"), template, replacer);
-    
+
     // Add this app to the list in applications.txt
     File applications = new File("C:/MDSS/manager/manager-1.0.0/classes/applications.txt");
     String appTxt = FileIO.readString(applications);
     appTxt += lineSeparator + appName;
     appTxt = appTxt.trim();
-    FileIO.write(applications, appTxt);
-    
-    // And give common.properties a unique rmi.port
+
     int appCount = appTxt.split("\\s+").length;
-    editWebappProperty("rmi.port", Integer.toString(1099 - appCount), "common.properties");
-    
-    // Update tomcat RAM
-    File startup = new File("C:/MDSS/tomcat6/bin/startup.bat");
-    int totalMemory = 512 * appCount;
-    readAndReplace(startup, "-Xmx\\d*M", "-Xmx" + totalMemory + "M");
-    
-    // Update Geoserver's catalog.xml
-    File catalogFile = new File("C:/MDSS/tomcat6/webapps/geoserver/data/catalog.xml");
-    String catalogData = FileIO.readString(catalogFile);
-    int index = catalogData.indexOf("<datastore ");
-    String pre = catalogData.substring(0, index);
-    String post = catalogData.substring(index);
-    catalogData = pre + createDatastoreXML() + post;
-    
-    index = catalogData.indexOf("<namespace ");
-    pre = catalogData.substring(0, index);
-    post = catalogData.substring(index);
-    catalogData = pre + createNamespaceXML() + post;
-    
-    FileIO.write(catalogFile, catalogData);
+
+    try
+    {
+      FileIO.write(applications, appTxt);
+    }
+    catch (Exception e)
+    {
+      this.exitStatus = PostInstallSetup.FAILURE;
+    }
+
+    try
+    {
+      // And give common.properties a unique rmi.port
+      editWebappProperty("rmi.port", Integer.toString(1099 - appCount), "common.properties");
+    }
+    catch (Exception e)
+    {
+      this.exitStatus = PostInstallSetup.FAILURE;
+    }
+
+    try
+    {
+      // Update tomcat RAM
+      File startup = new File("C:/MDSS/tomcat6/bin/startup.bat");
+      int totalMemory = 512 * appCount;
+      readAndReplace(startup, "-Xmx\\d*M", "-Xmx" + totalMemory + "M");
+    }
+    catch (Exception e)
+    {
+      this.exitStatus = PostInstallSetup.FAILURE;
+    }
+
+    try
+    {
+      // Update Geoserver's catalog.xml
+      File catalogFile = new File("C:/MDSS/tomcat6/webapps/geoserver/data/catalog.xml");
+      String catalogData = FileIO.readString(catalogFile);
+      int index = catalogData.indexOf("<datastore ");
+      String pre = catalogData.substring(0, index);
+      String post = catalogData.substring(index);
+      catalogData = pre + createDatastoreXML() + post;
+
+      index = catalogData.indexOf("<namespace ");
+      pre = catalogData.substring(0, index);
+      post = catalogData.substring(index);
+      catalogData = pre + createNamespaceXML() + post;
+
+      FileIO.write(catalogFile, catalogData);
+    }
+    catch (Exception e)
+    {
+      this.exitStatus = PostInstallSetup.FAILURE;
+    }
   }
-  
+
   public void editWebappProperty(String key, String newValue, String bundle) throws IOException
   {
     editProperty(key, newValue, new File(classes, bundle));
   }
-  
+
   private void editProperty(String key, String newValue, File file) throws IOException
   {
     String data = new String();
@@ -122,40 +165,48 @@ public class PostInstallSetup
     }
     FileIO.write(file, data);
   }
-  
+
   private void readAndReplace(File file, String regex, String replacement) throws IOException
   {
     String data = FileIO.readString(file);
     String replaced = data.replaceAll(regex, replacement);
     FileIO.write(file, replaced);
   }
-  
+
   private String createDatastoreXML()
   {
-    String datastore =
-    "<datastore id = \"MDSS_maps_" + appName + "\" enabled = \"true\" namespace = \"" + appName + "\" >\n" +
-    "      <connectionParams >\n" +
-    "        <parameter name = \"port\" value = \"5444\" />\n" +
-    "        <parameter name = \"passwd\" value = \"mdssdeploy\" />\n" +
-    "        <parameter name = \"dbtype\" value = \"postgis\" />\n" +
-    "        <parameter name = \"host\" value = \"localhost\" />\n" +
-    "        <parameter name = \"validate connections\" value = \"false\" />\n" +
-    "        <parameter name = \"max connections\" value = \"10\" />\n" +
-    "        <parameter name = \"database\" value = \"" + dbName + "\" />\n" +
-    "        <parameter name = \"wkb enabled\" value = \"true\" />\n" +
-    "        <parameter name = \"namespace\" value = \"http://" + appName + ".terraframe.com\" />\n" +
-    "        <parameter name = \"schema\" value = \"public\" />\n" +
-    "        <parameter name = \"estimated extent\" value = \"false\" />\n" +
-    "        <parameter name = \"loose bbox\" value = \"true\" />\n" +
-    "        <parameter name = \"user\" value = \"mdssdeploy\" />\n" +
-    "        <parameter name = \"min connections\" value = \"4\" />\n" +
-    "      </connectionParams>\n" +
-    "    </datastore>\n    ";
+    String datastore = "<datastore id = \"MDSS_maps_" + appName + "\" enabled = \"true\" namespace = \"" + appName + "\" >\n" + "      <connectionParams >\n" + "        <parameter name = \"port\" value = \"5444\" />\n" + "        <parameter name = \"passwd\" value = \"mdssdeploy\" />\n" + "        <parameter name = \"dbtype\" value = \"postgis\" />\n" + "        <parameter name = \"host\" value = \"localhost\" />\n" + "        <parameter name = \"validate connections\" value = \"false\" />\n"
+        + "        <parameter name = \"max connections\" value = \"10\" />\n" + "        <parameter name = \"database\" value = \"" + dbName + "\" />\n" + "        <parameter name = \"wkb enabled\" value = \"true\" />\n" + "        <parameter name = \"namespace\" value = \"http://" + appName + ".terraframe.com\" />\n" + "        <parameter name = \"schema\" value = \"public\" />\n" + "        <parameter name = \"estimated extent\" value = \"false\" />\n"
+        + "        <parameter name = \"loose bbox\" value = \"true\" />\n" + "        <parameter name = \"user\" value = \"mdssdeploy\" />\n" + "        <parameter name = \"min connections\" value = \"4\" />\n" + "      </connectionParams>\n" + "    </datastore>\n    ";
     return datastore;
   }
-  
+
   private String createNamespaceXML()
   {
     return "<namespace prefix = \"" + appName + "\" uri = \"http://" + appName + ".terraframe.com\" />\n    ";
+  }
+
+  public int getExitStatus()
+  {
+    return exitStatus;
+  }
+
+  public static void main(String[] args) throws Exception
+  {
+    try
+    {
+      String appName = args[0];
+      String installationNumber = args[1];
+      Boolean isMaster = Boolean.parseBoolean(args[2]);
+
+      PostInstallSetup setup = new PostInstallSetup(appName, installationNumber, isMaster);
+      setup.go();
+
+      System.exit(setup.getExitStatus());
+    }
+    catch (Exception e)
+    {
+      System.exit(PostInstallSetup.FAILURE);
+    }
   }
 }
