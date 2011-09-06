a62b2694e8a2c6c9a618680c77af275573a136d2
jnaifeh@terraframe.com <jnaifeh@terraframe.com>
2011-09-06 21:47:43 +0000

    Added skeleton for the form admin javascript.
    git-svn-id: https://subversion.assembla.com/svn/mdss@6360 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 2be7669cc59b956b4ced279d5656f601ce64a44b [formerly fd454b1f3e5745784cc64086a12662d8e537ea18]
    Former-commit-id: f1c031b543db09957fae8622a83c276e4f4fbd4b

---
+++ trunk/webapp/js/MdFormAdmin.js
@@ -0,0 +1,26 @@
+/*
+ * Manages control flow for the MdForm admin page.
+ */
+(function(){
+
+var UI = Mojo.Meta.alias("com.runwaysdk.ui.*");
+
+/**
+ * Primary class to handle control flow in the UI.
+ */
+Mojo.Meta.newClass('dss.vector.solutions.MdFormAdmin', 
+{
+  Extends : UI.Component,
+  Instance : {
+    initialize : function()
+    {
+      
+    },
+    render : function()
+    {
+    
+    }
+  }
+});
+
+})();
