92b51f26bfe7fad9d7f75834aa4daef3f7ac028a
cruisecontrol@terraframe.com <cruisecontrol@terraframe.com>
2011-09-10 02:41:45 +0000

    git-svn-id: https://subversion.assembla.com/svn/mdss@6392 e0e46c49-be69-4f5a-ad62-21024a331aea
    Former-commit-id: 1c6f98f3ae35a028a57726486721525c622fa318 [formerly 84b9440fa03a9cb4f2e7da8c916818646d2f1bd1]
    Former-commit-id: 9aa08deff5eb22297dc036600bd5f5217a29b351

--- trunk/webapp/js/MdFormAdmin.js
+++ trunk/webapp/js/MdFormAdmin.js
@@ -4,6 +4,8 @@
 (function(){
 
 var UI = Mojo.Meta.alias("com.runwaysdk.ui.*");
+UI.Manager.setFactory("YUI3");
+var FACTORY = RUNWAY_UI.Manager.getFactory();
 
 /**
  * Primary class to handle control flow in the UI.
@@ -11,13 +13,20 @@ var UI = Mojo.Meta.alias("com.runwaysdk.ui.*");
 Mojo.Meta.newClass('dss.vector.solutions.MdFormAdmin', 
 {
   Instance : {
-    initialize : function(adminPanelId)
+    initialize : function(adminPanelId, formList)
     {
+			this._parentDiv = adminPanelId;
       this._adminLayout = FACTORY.newLayout("horizontal");
+			this._formList = FACTORY.newList("Form List");
+			for (var i = 0; i < formList.length; i++)
+			{
+				this._formList.addItem(formList[i]);
+			}
+			this._adminLayout.appendChild(this._formList);
     },
     render : function()
     {
-    
+      this._adminLayout.render();
     },
     availableFields : function()
     {
