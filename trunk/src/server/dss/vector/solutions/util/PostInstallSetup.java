d4b13ee334a6b78234c223c858625204726b1437
jsmethie@terraframe.com <jsmethie@terraframe.com>
2011-10-03 16:53:48 +0000

    -Refactored the PostInstallSetup to have better command line parsing for more flexability and better testing.
    git-svn-id: https://subversion.assembla.com/svn/mdss@6501 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 7bfc1ed5cf77f16649e1a8b9d2dae48408c8faf9 [formerly 5d323138d6313720303b37b338994089102a4e36]
    Former-commit-id: 07be9c99bbec7fb7e52e01194a36ff28d9425072

--- trunk/src/server/dss/vector/solutions/util/PostInstallSetup.java
+++ trunk/src/server/dss/vector/solutions/util/PostInstallSetup.java
@@ -4,6 +4,19 @@ import java.io.File;
 import java.io.IOException;
 import java.security.AccessController;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+import org.xml.sax.SAXException;
+
 import sun.security.action.GetPropertyAction;
 
 import com.runwaysdk.util.FileIO;
@@ -20,45 +33,100 @@ public class PostInstallSetup
    * Exit status for the success case. NSIS considers any non-zero exit status
    * as an error, as such the SUCCESS exit status must be 0.
    */
-  public static int SUCCESS = 0;
+  public static int    SUCCESS         = 0;
 
   /**
    * Exit status for the failure case.
    */
-  public static int FAILURE = -1;
+  public static int    FAILURE         = -1;
 
-  private File      appRoot;
+  public static String DEFAULT_TOMCAT  = "C:/MDSS/tomcat6/";
 
-  private File      classes;
+  public static String DEFAULT_MANAGER = "C:/MDSS/manager/";
 
-  private String    appName;
+  private File         appRoot;
 
-  private String    dbName;
+  private File         classes;
 
-  private String    installationNumber;
+  private String       appName;
 
-  private Boolean   isMaster;
+  private String       dbName;
 
-  /**
-   * Exit status of the setup
-   */
-  private int       exitStatus;
+  private String       installationNumber;
 
-  public PostInstallSetup(String appName, String installationNumber, Boolean isMaster)
+  private Boolean      isMaster;
+
+  private String       managerDirectory;
+
+  private String       tomcatDirectory;
+
+  public PostInstallSetup(String appName, String installationNumber, Boolean isMaster, String tomcatDirectory, String managerDirectory)
   {
     this.appName = appName;
     this.dbName = appName.toLowerCase();
     this.installationNumber = installationNumber;
     this.isMaster = isMaster;
-    this.appRoot = new File("C:/MDSS/tomcat6/webapps/" + appName);
+    this.tomcatDirectory = tomcatDirectory;
+    this.managerDirectory = managerDirectory;
+
+    this.appRoot = new File(tomcatDirectory + "webapps/" + appName);
     this.classes = new File(appRoot, "WEB-INF/classes");
-    this.exitStatus = PostInstallSetup.SUCCESS;
   }
 
-  public void go() throws IOException
+  public void go() throws IOException, ParserConfigurationException, SAXException, TransformerFactoryConfigurationError, TransformerException
+  {
+    this.updateProperties();
+
+    // Update css and jsps
+    this.updateCSS();
+
+    this.updateJSPs();
+
+    // Add this app to the list in applications.txt
+    String lineSeparator = (String) AccessController.doPrivileged(new GetPropertyAction("line.separator"));
+
+    File applications = new File(this.managerDirectory + "manager-1.0.0/classes/applications.txt");
+    String appTxt = FileIO.readString(applications);
+    appTxt += lineSeparator + appName;
+    appTxt = appTxt.trim();
+
+    int appCount = appTxt.split("\\s+").length;
+
+    FileIO.write(applications, appTxt);
+    // And give common.properties a unique rmi.port
+    editWebappProperty("rmi.port", Integer.toString(1099 - appCount), "common.properties");
+
+    // Update tomcat RAM
+    File startup = new File(tomcatDirectory + "/bin/startup.bat");
+    int totalMemory = Math.min(2048, 512 * appCount);
+    readAndReplace(startup, "-Xmx\\d*M", "-Xmx" + totalMemory + "M");
+
+    // Update Geoserver's catalog.xml
+    CatalogBuilder builder = new CatalogBuilder(tomcatDirectory + "/webapps/geoserver/data/catalog.xml");
+    builder.addApplication(appName, dbName);
+    builder.write();
+  }
+
+  private void updateJSPs() throws IOException
+  {
+    String template = "/DDMS/";
+    String replacer = "/" + appName + "/";
+
+    readAndReplace(new File(appRoot, "login.jsp"), template, replacer);
+    readAndReplace(new File(appRoot, "test/viewComponentOldWay.jsp"), template, replacer);
+  }
+
+  private void updateCSS() throws IOException
+  {
+    String template = "/\\w+/imgs/";
+    String replacer = "/" + appName + "/imgs/";
+
+    readAndReplace(new File(appRoot, "css/style.css"), template, replacer);
+  }
+
+  private void updateProperties() throws IOException
   {
     String domain = installationNumber + ".mdss.ivcc.com";
-    String lineSeparator = (String) AccessController.doPrivileged(new GetPropertyAction("line.separator"));
 
     // Update property files
     editWebappProperty("deploy.appname", appName, "terraframe.properties");
@@ -72,64 +140,6 @@ public class PostInstallSetup
     {
       editWebappProperty("master", "false", "install.properties");
       editWebappProperty("domain", domain, "terraframe.properties");
-    }
-
-    // Update css and jsps
-    String template = "/DDMS/";
-    String replacer = "/" + appName + "/";
-    readAndReplace(new File(appRoot, "login.jsp"), template, replacer);
-    readAndReplace(new File(appRoot, "css/style.css"), template, replacer);
-    readAndReplace(new File(appRoot, "test/viewComponentOldWay.jsp"), template, replacer);
-
-    // Add this app to the list in applications.txt
-    File applications = new File("C:/MDSS/manager/manager-1.0.0/classes/applications.txt");
-    String appTxt = FileIO.readString(applications);
-    appTxt += lineSeparator + appName;
-    appTxt = appTxt.trim();
-
-    int appCount = appTxt.split("\\s+").length;
-
-    try
-    {
-      FileIO.write(applications, appTxt);
-    }
-    catch (Exception e)
-    {
-      this.exitStatus = PostInstallSetup.FAILURE;
-    }
-
-    try
-    {
-      // And give common.properties a unique rmi.port
-      editWebappProperty("rmi.port", Integer.toString(1099 - appCount), "common.properties");
-    }
-    catch (Exception e)
-    {
-      this.exitStatus = PostInstallSetup.FAILURE;
-    }
-
-    try
-    {
-      // Update tomcat RAM
-      File startup = new File("C:/MDSS/tomcat6/bin/startup.bat");
-      int totalMemory = Math.min(2048, 512 * appCount);
-      readAndReplace(startup, "-Xmx\\d*M", "-Xmx" + totalMemory + "M");
-    }
-    catch (Exception e)
-    {
-      this.exitStatus = PostInstallSetup.FAILURE;
-    }
-
-    try
-    {
-      // Update Geoserver's catalog.xml
-      CatalogBuilder builder = new CatalogBuilder("C:/MDSS/tomcat6/webapps/geoserver/data/catalog.xml");
-      builder.addApplication(appName, dbName);
-      builder.write();
-    }
-    catch (Exception e)
-    {
-      this.exitStatus = PostInstallSetup.FAILURE;
     }
   }
 
@@ -162,41 +172,62 @@ public class PostInstallSetup
     String replaced = data.replaceAll(regex, replacement);
     FileIO.write(file, replaced);
   }
-//
-//  private String createDatastoreXML()
-//  {
-//    String datastore = "<datastore id = \"MDSS_maps_" + appName + "\" enabled = \"true\" namespace = \"" + appName + "\" >\n" + "      <connectionParams >\n" + "        <parameter name = \"port\" value = \"5444\" />\n" + "        <parameter name = \"passwd\" value = \"mdssdeploy\" />\n" + "        <parameter name = \"dbtype\" value = \"postgis\" />\n" + "        <parameter name = \"host\" value = \"localhost\" />\n" + "        <parameter name = \"validate connections\" value = \"false\" />\n"
-//        + "        <parameter name = \"max connections\" value = \"10\" />\n" + "        <parameter name = \"database\" value = \"" + dbName + "\" />\n" + "        <parameter name = \"wkb enabled\" value = \"true\" />\n" + "        <parameter name = \"namespace\" value = \"http://" + appName + ".terraframe.com\" />\n" + "        <parameter name = \"schema\" value = \"public\" />\n" + "        <parameter name = \"estimated extent\" value = \"false\" />\n"
-//        + "        <parameter name = \"loose bbox\" value = \"true\" />\n" + "        <parameter name = \"user\" value = \"mdssdeploy\" />\n" + "        <parameter name = \"min connections\" value = \"4\" />\n" + "      </connectionParams>\n" + "    </datastore>\n    ";
-//    return datastore;
-//  }
-//
-//  private String createNamespaceXML()
-//  {
-//    return "<namespace prefix = \"" + appName + "\" uri = \"http://" + appName + ".terraframe.com\" />\n    ";
-//  }
 
-  public int getExitStatus()
+  public static void main(String[] args)
   {
-    return exitStatus;
-  }
+    Option appNameOption = new Option("a", true, "Name of the app (required)");
+    appNameOption.setRequired(true);
 
-  public static void main(String[] args) throws Exception
-  {
+    Option installationNumberOption = new Option("n", true, "Installation number (required)");
+    installationNumberOption.setRequired(true);
+
+    Option isMasterOption = new Option("i", true, "Flag indicating if the install type is a master or dependent (required)");
+    isMasterOption.setRequired(true);
+
+    Options options = new Options();
+    options.addOption(appNameOption);
+    options.addOption(installationNumberOption);
+    options.addOption(isMasterOption);
+    options.addOption(new Option("c", "css-only", false, "Setup will only update the css files"));
+    options.addOption(new Option("t", true, "Tomcat directory"));
+    options.addOption(new Option("m", true, "Manager directory"));
+
     try
     {
-      String appName = args[0];
-      String installationNumber = args[1];
-      Boolean isMaster = Boolean.parseBoolean(args[2]);
+      CommandLineParser parser = new PosixParser();
+      CommandLine cmd = parser.parse(options, args);
 
-      PostInstallSetup setup = new PostInstallSetup(appName, installationNumber, isMaster);
-      setup.go();
+      String appName = cmd.getOptionValue("a").trim();
+      String installationNumber = cmd.getOptionValue("n").trim();
+      Boolean isMaster = Boolean.parseBoolean(cmd.getOptionValue("i").trim());
+      String tomcatDirectory = cmd.getOptionValue("t", DEFAULT_TOMCAT);
+      String managerDirectory = cmd.getOptionValue("m", DEFAULT_MANAGER);
 
-      System.exit(setup.getExitStatus());
+      PostInstallSetup setup = new PostInstallSetup(appName, installationNumber, isMaster, tomcatDirectory, managerDirectory);
+
+      if (cmd.hasOption("c"))
+      {
+        setup.updateCSS();
+      }
+      else
+      {
+        setup.go();
+      }
+    }
+    catch (ParseException exp)
+    {
+      HelpFormatter formatter = new HelpFormatter();
+      formatter.printHelp("setup", options);
+
+      throw new RuntimeException(exp);
+    }
+    catch (RuntimeException e)
+    {
+      throw e;
     }
     catch (Exception e)
     {
-      System.exit(PostInstallSetup.FAILURE);
+      throw new RuntimeException(e);
     }
   }
 }
