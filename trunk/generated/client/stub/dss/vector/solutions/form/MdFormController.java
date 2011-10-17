dd4fa4bc5fdd2dca5f418a0ebc771b277b38f909
wrose <wrose>
2011-10-17 20:00:57 +0000

    git-svn-id: https://subversion.assembla.com/svn/mdss@6529 e0e46c49-be69-4f5a-ad62-21024a331aea
    Former-commit-id: aa200d02be1ba1d4ee3084d713ac5b337c2087ec [formerly 621b3fbe13a4fb695851b0704edc3eb99cae9fe7]
    Former-commit-id: b07bc819238a7459c9d47a7a37904248ab986409

---
+++ trunk/generated/client/stub/dss/vector/solutions/form/MdFormController.java
@@ -0,0 +1,12 @@
+package dss.vector.solutions.form;
+
+public class MdFormController extends MdFormControllerBase implements com.runwaysdk.generation.loader.Reloadable
+{
+  private static final long serialVersionUID = 1615923422;
+  
+  public MdFormController(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp, java.lang.Boolean isAsynchronous)
+  {
+    super(req, resp, isAsynchronous);
+  }
+  
+}
