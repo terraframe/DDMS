6ad266950d0a1e7b9e0e746b49acd70e511665a9
jsmethie@terraframe.com <jsmethie@terraframe.com>
2012-10-22 16:41:56 +0000

    -Base checkin for Will's system alert changes
    git-svn-id: https://subversion.assembla.com/svn/mdss@6996 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 043635dd5785d192aa4207f493d5fdd0801b1727 [formerly b15fc8c50aba327c77c065461f408d0a9691510c]
    Former-commit-id: be481e3fbd8edbbe377f5921febb6bfb99ceb361

---
+++ trunk/generated/server/stub/dss/vector/solutions/general/SystemAlertEmailSubjectText.java
@@ -0,0 +1,17 @@
+package dss.vector.solutions.general;
+
+public class SystemAlertEmailSubjectText extends SystemAlertEmailSubjectTextBase implements com.runwaysdk.generation.loader.Reloadable
+{
+  private static final long serialVersionUID = -1352905808;
+  
+  public SystemAlertEmailSubjectText()
+  {
+    super();
+  }
+  
+  public SystemAlertEmailSubjectText(com.runwaysdk.business.MutableWithStructs entity, String structName)
+  {
+    super(entity, structName);
+  }
+  
+}
