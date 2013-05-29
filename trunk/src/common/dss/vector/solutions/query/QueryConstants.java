dc1bb8df36be791edef1aa2c8ad36591b7947e23
jnaifeh@terraframe.com <jnaifeh@terraframe.com>
2013-05-29 18:10:03 +0000

    Added Overall Mortality rate calculation
    git-svn-id: https://subversion.assembla.com/svn/mdss@7165 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 09826492f6debda7b4a32c131e45bb4bbf3b7293 [formerly 3f1b4efca77bf9b43b50b630865f8c0d0bfdffe8]
    Former-commit-id: 7f0c2c1aa96d002be96d50c0c03d42180fa17ba7

--- trunk/src/common/dss/vector/solutions/query/QueryConstants.java
+++ trunk/src/common/dss/vector/solutions/query/QueryConstants.java
@@ -64,6 +64,8 @@ public class QueryConstants implements Reloadable
   public static final String  SUM_AREA_TARGETS                        = "get_area_spray_target_by_id_and_tar";
 
   public static final String  ASSAY_TYPE                              = "assay_type";
+  
+  public static final String  OVERALL_MORTALITY                       = "overall_mortality";
 
   /*
    * BEGIN InterventionControl query builder constants.
