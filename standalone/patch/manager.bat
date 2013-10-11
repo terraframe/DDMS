d4f21101873a0b7047da218c2666d34eff257653
cruisecontrol@terraframe.com <cruisecontrol@terraframe.com>
2013-10-11 16:06:06 +0000

    Update of manager.bat to run the manager with Admin privileges.  This is required because starting and stopping a service requires privileges.
    git-svn-id: https://subversion.assembla.com/svn/mdss@7296 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 25c3ed97e46e5b0c7b2e058e2d26a9e62533c858 [formerly 72877ecd9b9ec2660b14b8f9713c5c0937430851]
    Former-commit-id: ea97173141c1b2c416087399f9b6698a77ac5b7d

--- standalone/patch/manager.bat
+++ standalone/patch/manager.bat
@@ -1,1 +1,37 @@
-start C:\MDSS\Java\jdk1.6.0_16\bin\javaw.exe -Djavax.rmi.ssl.client.enabledProtocols=TLSv1 -Djavax.rmi.ssl.client.enabledCipherSuites=SSL_RSA_WITH_RC4_128_MD5 -Djavax.net.ssl.trustStorePassword=1206b6579Acb3 -Djavax.net.ssl.trustStore=C:\MDSS\manager\keystore\ddms.ts -Djavax.net.ssl.keyStorePassword=4b657920666fZ -Djavax.net.ssl.keyStore=C:\MDSS\manager\keystore\ddms.ks -Djavax.rmi.ssl.enabled=true -Xmx1024M -cp C:\MDSS\manager\bin;C:\MDSS\manager\lib\*;C:\MDSS\manager\profiles;C:\MDSS\tomcat6\webapps\DDMS\WEB-INF\classes dss/vector/solutions/admin/MDSSModule
+:::::::::::::::::::::::::::::::::::::::::
+:: Automatically check & get admin rights
+:::::::::::::::::::::::::::::::::::::::::
+@echo off
+CLS 
+ECHO.
+ECHO =============================
+ECHO Running Admin shell
+ECHO =============================
+
+:checkPrivileges 
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges ) 
+
+:getPrivileges 
+if '%1'=='ELEV' (shift & goto gotPrivileges)  
+ECHO. 
+ECHO **************************************
+ECHO Invoking UAC for Privilege Escalation 
+ECHO **************************************
+
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+setlocal EnableDelayedExpansion
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs" 
+ECHO UAC.ShellExecute "!batchPath!", "ELEV", "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs" 
+"%temp%\OEgetPrivileges.vbs" 
+exit /B 
+
+:gotPrivileges 
+::::::::::::::::::::::::::::
+:START
+::::::::::::::::::::::::::::
+setlocal & pushd .
+
+REM Run shell as admin (example) - put here code as you like
+start C:\MDSS\Java\jdk1.6.0_16\bin\javaw.exe -Djavax.rmi.ssl.client.enabledProtocols=TLSv1 -Djavax.rmi.ssl.client.enabledCipherSuites=SSL_RSA_WITH_RC4_128_MD5 -Djavax.net.ssl.trustStorePassword=1206b6579Acb3 -Djavax.net.ssl.trustStore=C:\MDSS\manager\keystore\ddms.ts -Djavax.net.ssl.keyStorePassword=4b657920666fZ -Djavax.net.ssl.keyStore=C:\MDSS\manager\keystore\ddms.ks -Djavax.rmi.ssl.enabled=true -cp "C:\MDSS\manager\manager-1.0.0\classes\;C:\MDSS\manager\manager-1.0.0\lib\*" dss.vector.solutions.manager.ServerManagerWindow
