bb32700582ef2f0cf19b7ed8df789ae47d09098a
jnaifeh@terraframe.com <jnaifeh@terraframe.com>
2014-02-10 17:53:49 +0000

    Fixed #2323
    git-svn-id: https://subversion.assembla.com/svn/mdss@7384 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: bf016af679b84210948b4e44ef023a4400fddb89 [formerly 865246094efecd073c3b7ae616080ddcd6d9931a]
    Former-commit-id: d2bcce8713650799e1c448bbc8aea2a78937c55e

--- trunk/src/server/dss/vector/solutions/querybuilder/irs/OperatorJoin.java
+++ trunk/src/server/dss/vector/solutions/querybuilder/irs/OperatorJoin.java
@@ -21,7 +21,8 @@ public class OperatorJoin extends TargetJoin implements Reloadable
     {
       String sql = "";
 
-      sql += a + " FULL OUTER JOIN " + p + " \n";
+      sql += p + " LEFT JOIN " + a + " \n";
+//      sql += a + " FULL OUTER JOIN " + p + " \n";
       
       // NOTE: old code for reference
 //      sql += "ON extract(YEAR FROM "+TargetJoin.ACTUAL_ALIAS+"."+Alias.SPRAY_DATE.getAlias()+") " +
@@ -38,6 +39,16 @@ public class OperatorJoin extends TargetJoin implements Reloadable
       
       sql += "AND " + TargetJoin.PLANNED_ALIAS + "." + Alias.DISEASE + " = " + TargetJoin.ACTUAL_ALIAS
           + "." + Alias.DISEASE + " \n";
+      
+      // #2323 Change: restrict the planned rows by showing only those that have activity within the time and geo criteria, but don't do a strict row-by-row
+      // join on target week. Reference the ticket examples for something better
+      sql += "INNER JOIN \n";
+      sql += "(SELECT "+Alias.UNIQUE_PLANNED_ID+" FROM "+IRSQB.View.PLANNED_OPERATOR+" p2 INNER JOIN "+IRSQB.View.ALL_ACTUALS+" a2 ON \n";
+      sql += "p2."+Alias.SPRAY_SEASON+" = a2."+Alias.SPRAY_SEASON+"  \n";
+      sql += "AND p2."+Alias.SPRAY_OPERATOR_DEFAULT_LOCALE+" = a2."+Alias.SPRAY_OPERATOR_DEFAULT_LOCALE+"  \n";
+      sql += "AND p2."+Alias.DISEASE+" = a2."+Alias.DISEASE+") restriction \n";
+      sql += "ON p."+Alias.UNIQUE_PLANNED_ID+" = restriction."+Alias.UNIQUE_PLANNED_ID+" \n";
+      
       return sql;
     }
     else if(hasPlanned)
