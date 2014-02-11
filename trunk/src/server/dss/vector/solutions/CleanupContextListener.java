d6001fd3fbff80f50eb0dd1af353a37f6018f305
jsmethie@terraframe.com <jsmethie@terraframe.com>
2014-02-11 21:38:12 +0000

    -Report scheduler integration
    git-svn-id: https://subversion.assembla.com/svn/mdss@7395 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: e132b81706589fde0a6530f45e983cfaa71f20a3 [formerly f3eba6db11eea18598ce914b6b0745cc9b6464e1]
    Former-commit-id: 64d17d313a25e9b3be6b263136af876ab4ab5eb9

--- trunk/src/server/dss/vector/solutions/CleanupContextListener.java
+++ trunk/src/server/dss/vector/solutions/CleanupContextListener.java
@@ -3,19 +3,24 @@ package dss.vector.solutions;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import com.runwaysdk.system.scheduler.SchedulerManager;
+
 public class CleanupContextListener implements ServletContextListener
 {
 
   @Override
   public void contextDestroyed(ServletContextEvent arg0)
   {
+    SchedulerManager.shutdown();
+
     ServerContext.instance().contextDestroyed();
   }
 
   @Override
   public void contextInitialized(ServletContextEvent arg0)
   {
-    // Do nothing
     ServerContext.instance();
+
+    SchedulerManager.start();
   }
 }
