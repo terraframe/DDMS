a8c76f8f5a6e3184048c2d1510960d315ee46a92
cruisecontrol@terraframe.com <cruisecontrol@terraframe.com>
2014-04-22 23:09:31 +0000

    -Fixed bug where the java home wasn't being set correctly on new 32-bit installs
    git-svn-id: https://subversion.assembla.com/svn/mdss@7462 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: ebc7de09dc6431f81cde024c7501698f315a6059 [formerly 8aa11e98aa78467af3ea9ce22529eefc31142824]
    Former-commit-id: b3ed032486be017d822ac6ba44d4cc04ff914e5b

--- standalone/patch/manager.bat
+++ standalone/patch/manager.bat
@@ -34,4 +34,4 @@ exit /B
 setlocal & pushd .
 
 REM Run shell as admin (example) - put here code as you like
-start C:\MDSS\Java\jdk1.6.0_16\bin\javaw.exe -Djavax.rmi.ssl.client.enabledProtocols=TLSv1 -Djavax.rmi.ssl.client.enabledCipherSuites=SSL_RSA_WITH_RC4_128_MD5 -Djavax.net.ssl.trustStorePassword=1206b6579Acb3 -Djavax.net.ssl.trustStore=C:\MDSS\manager\keystore\ddms.ts -Djavax.net.ssl.keyStorePassword=4b657920666fZ -Djavax.net.ssl.keyStore=C:\MDSS\manager\keystore\ddms.ks -Djavax.rmi.ssl.enabled=true -cp "C:\MDSS\manager\manager-1.0.0\classes\;C:\MDSS\manager\manager-1.0.0\lib\*" dss.vector.solutions.manager.ServerManagerWindow
+start JAVA_HOME_VALUE\bin\javaw.exe -Djavax.rmi.ssl.client.enabledProtocols=TLSv1 -Djavax.rmi.ssl.client.enabledCipherSuites=SSL_RSA_WITH_RC4_128_MD5 -Djavax.net.ssl.trustStorePassword=1206b6579Acb3 -Djavax.net.ssl.trustStore=C:\MDSS\manager\keystore\ddms.ts -Djavax.net.ssl.keyStorePassword=4b657920666fZ -Djavax.net.ssl.keyStore=C:\MDSS\manager\keystore\ddms.ks -Djavax.rmi.ssl.enabled=true -cp "C:\MDSS\manager\manager-1.0.0\classes\;C:\MDSS\manager\manager-1.0.0\lib\*" dss.vector.solutions.manager.ServerManagerWindow
