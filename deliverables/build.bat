e476284a8699c668e1642b711b51c02a2ae74370
cruisecontrol@terraframe.com <cruisecontrol@terraframe.com>
2015-01-12 21:32:37 +0000

    Smethie changed some more stuff for our new build environment.
    git-svn-id: https://subversion.assembla.com/svn/mdss@7756 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 327b35abe7f3a649c0a2f6a36faef07eebeb0b25 [formerly 7903e14df9f86829ca180383bb83e0e3e23dfcb5]
    Former-commit-id: 9a72f0e99a7189b260428e95368f10c9e630afc9

--- deliverables/build.bat
+++ deliverables/build.bat
@@ -1,11 +1,11 @@
 @echo off
 rem ant -buildfile cc-build.xml 
 
-SET ANT_HOME=C:\Program Files (x86)\Java\apache-ant-1.8.0
+SET ANT_HOME=C:\Program Files\Java\apache-ant-1.9.4
 SET ANT_OPTS=-Xmx1024m -XX:MaxPermSize=128M
-SET JAVA_HOME=C:\Program Files (x86)\Java\jdk1.6.0_19
-SET JDK_HOME=C:\Program Files (x86)\Java\jdk1.6.0_19
-SET JRE_HOME=C:\Program Files (x86)\Java\jre6
+SET JAVA_HOME=C:\Program Files\Java\jdk1.6.0_45
+SET JDK_HOME=C:\Program Files\Java\jdk1.6.0_45
+SET JRE_HOME=C:\Program Files\Java\jre6
 
 
 rem "%ANT_HOME%\bin\ant" -buildfile cc-build.xml -Dpostgres.bin="C:/Program Files/PostgreSQL/8.4/bin"  build_installer_exe
