4f483f9056ffa0ed96e3e86bbfa0ece71f04f2e6
rrowlands <rrowlands>
2015-12-21 22:13:37 +0000

    Removed EhCache 2.x dependencies.
    git-svn-id: https://subversion.assembla.com/svn/mdss@8060 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: ac887e214864ea70b55e809394f3d67e122fa91f [formerly 7e3064727af1cad152293a9e5a4a36d4dd2c5084]
    Former-commit-id: 6e05f6a9d9d6d5cafb8af92a454eb519e5a7a384

--- synch-manager/src/main/java/dss/vector/solutions/admin/controller/ErrorConfiguration.java
+++ synch-manager/src/main/java/dss/vector/solutions/admin/controller/ErrorConfiguration.java
@@ -1,11 +1,6 @@
 package dss.vector.solutions.admin.controller;
 
-import java.io.InvalidClassException;
-
-import net.sf.ehcache.CacheException;
-
 import com.runwaysdk.manager.controller.ConfigurationAdapter;
-import com.runwaysdk.manager.general.Localizer;
 
 public abstract class ErrorConfiguration extends ConfigurationAdapter
 {
@@ -13,13 +8,13 @@ public abstract class ErrorConfiguration extends ConfigurationAdapter
   @Override
   public void handleError(Throwable throwable)
   {
-    if (throwable instanceof CacheException)
-    {
-      if (throwable.getCause() instanceof InvalidClassException)
-      {
-        throwable = new RuntimeException(Localizer.getMessage("CACHE_MISMATCH"), throwable);
-      }
-    }
+//    if (throwable instanceof CacheException)
+//    {
+//      if (throwable.getCause() instanceof InvalidClassException)
+//      {
+//        throwable = new RuntimeException(Localizer.getMessage("CACHE_MISMATCH"), throwable);
+//      }
+//    }
 
     this.getWindow().error(throwable);
   }
