e08295190cd5b1002597b5d4673db8e3d2f4bfe0
rrowlands <rrowlands>
2016-01-13 00:43:30 +0000

    Memory only global cache = true.
    git-svn-id: https://subversion.assembla.com/svn/mdss@8125 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 1247dcf0ff03ce880c65dff2f95c192887da9e83 [formerly fcb1db0e72214599306749819e9d96f35b8be8db]
    Former-commit-id: 51cdd71863c3e538025f1b569c14a66face2c282

--- trunk/src/server/dss/vector/solutions/query/GeoserverFacade.java
+++ trunk/src/server/dss/vector/solutions/query/GeoserverFacade.java
@@ -3,7 +3,9 @@ package dss.vector.solutions.query;
 import it.geosolutions.geoserver.rest.GeoServerRESTPublisher;
 import it.geosolutions.geoserver.rest.GeoServerRESTReader;
 import it.geosolutions.geoserver.rest.decoder.RESTLayerList;
+import it.geosolutions.geoserver.rest.encoder.GSLayerEncoder;
 import it.geosolutions.geoserver.rest.encoder.datastore.GSPostGISDatastoreEncoder;
+import it.geosolutions.geoserver.rest.encoder.feature.GSFeatureTypeEncoder;
 import it.geosolutions.geoserver.rest.manager.GeoServerRESTStoreManager;
 
 import java.net.MalformedURLException;
@@ -27,7 +29,10 @@ import com.runwaysdk.business.Business;
 import com.runwaysdk.configuration.RunwayConfigurationException;
 import com.runwaysdk.constants.CommonProperties;
 import com.runwaysdk.constants.DatabaseProperties;
+import com.runwaysdk.dataaccess.ValueObject;
 import com.runwaysdk.generation.loader.Reloadable;
+import com.runwaysdk.query.OIterator;
+import com.runwaysdk.query.QueryFactory;
 import com.runwaysdk.query.ValueQuery;
 import com.runwaysdk.system.gis.ConfigurationException;
 import com.terraframe.utf8.UTF8ResourceBundle;
@@ -37,6 +42,20 @@ import dss.vector.solutions.geo.GeoServerReloadException;
 public class GeoserverFacade implements Reloadable
 {
   private Logger logger = LoggerFactory.getLogger(GeoserverFacade.class);
+  
+  public final int    SRS_CODE   = 4326;
+
+  public final String SRS        = "EPSG:" + SRS_CODE;
+
+  public static final String      GEOM_COLUMN = QueryConstants.GEOMETRY_NAME_COLUMN;
+  
+  public int          MINX_INDEX = 0;
+
+  public int          MINY_INDEX = 1;
+
+  public int          MAXX_INDEX = 2;
+
+  public int          MAXY_INDEX = 3;
   
   /**
    * Default geoserver username
@@ -119,7 +138,6 @@ public class GeoserverFacade implements Reloadable
     return restReader;
   }
 
-  @SuppressWarnings("deprecation")
   public void reload(Map<Layer, ValueQuery> reloads, MapConfiguration configuration)
   {
     try
@@ -161,7 +179,8 @@ public class GeoserverFacade implements Reloadable
 
         String viewName = configuration.getViewName(reload);
         String defaultStyle = reload.getRenderAs().get(0) == AllRenderTypes.POINT ? "point" : "polygon";
-        publisher.publishDBLayer(CommonProperties.getDeployAppName(), QueryConstants.getNamespacedDataStore(), viewName, "EPSG:4326", defaultStyle);
+//        publisher.publishDBLayer(CommonProperties.getDeployAppName(), QueryConstants.getNamespacedDataStore(), viewName, "EPSG:4326", defaultStyle);
+        publishLayer(viewName, defaultStyle);
       }
     }
     catch (Exception e)
@@ -170,6 +189,122 @@ public class GeoserverFacade implements Reloadable
       GeoServerReloadException ex = new GeoServerReloadException(error, e);
       throw ex;
     }
+  }
+  
+  /**
+   * Adds a database view and publishes the layer if necessary.
+   * 
+   * @param layer
+   */
+  public boolean publishLayer(String layer, String styleName)
+  {
+    double[] bbox = getBBOX(layer);
+
+    GSFeatureTypeEncoder fte = new GSFeatureTypeEncoder();
+    fte.setEnabled(true);
+    fte.setName(layer);
+    fte.setSRS(SRS);
+    fte.setTitle(layer);
+    fte.addKeyword(layer);
+
+    if (bbox != null)
+    {
+      double minX = bbox[MINX_INDEX];
+      double minY = bbox[MINY_INDEX];
+      double maxX = bbox[MAXX_INDEX];
+      double maxY = bbox[MAXY_INDEX];
+
+      fte.setNativeBoundingBox(minX, minY, maxX, maxY, SRS);
+      fte.setLatLonBoundingBox(minX, minY, maxX, maxY, SRS);
+    }
+
+    GSLayerEncoder le = new GSLayerEncoder();
+    le.setDefaultStyle(styleName);
+    le.setEnabled(true);
+
+    if (getPublisher().publishDBLayer(CommonProperties.getDeployAppName(), QueryConstants.getNamespacedDataStore(), fte, le))
+    {
+      logger.debug("Created the layer [" + layer + "] in geoserver.");
+      return true;
+    }
+    else
+    {
+      logger.error("Failed to create the layer [" + layer + "] in geoserver.");
+      return false;
+    }
+  }
+  
+  /**
+   * Calculates the bounding box of all the layers.
+   * 
+   * @param views
+   * @return double[] {minx, miny, maxx, maxy}
+   */
+  public double[] getBBOX(String... views)
+  {
+    // collect all the views and extend the bounding box
+    ValueQuery union = new ValueQuery(new QueryFactory());
+    if (views.length == 1)
+    {
+      String view = views[0];
+      union.SELECT(union.aSQLClob(GeoserverFacade.GEOM_COLUMN, GeoserverFacade.GEOM_COLUMN, GeoserverFacade.GEOM_COLUMN));
+      union.FROM(view, view);
+    }
+    else if (views.length > 1)
+    {
+      ValueQuery[] unionVQs = new ValueQuery[views.length];
+
+      for (int i = 0; i < unionVQs.length; i++)
+      {
+        String view = views[i];
+        ValueQuery vq = new ValueQuery(union.getQueryFactory());
+        vq.SELECT(vq.aSQLClob(GeoserverFacade.GEOM_COLUMN, GeoserverFacade.GEOM_COLUMN, GeoserverFacade.GEOM_COLUMN));
+        vq.FROM(view, view);
+
+        unionVQs[i] = vq;
+      }
+
+      union.UNION_ALL(unionVQs);
+    }
+    else
+    {
+//      throw new MapLayerException("The map has no layers");
+      return null;
+    }
+
+    ValueQuery collected = new ValueQuery(union.getQueryFactory());
+    collected.SELECT(collected.aSQLAggregateClob("collected", "st_collect(" + GeoserverFacade.GEOM_COLUMN + ")", "collected"));
+    collected.FROM("(" + union.getSQL() + ")", "unioned");
+
+    ValueQuery outer = new ValueQuery(union.getQueryFactory());
+    outer.SELECT(union.aSQLAggregateDouble("minx", "st_xmin(collected)"), union.aSQLAggregateDouble("miny", "st_ymin(collected)"), union.aSQLAggregateDouble("maxx", "st_xmax(collected)"), union.aSQLAggregateDouble("maxy", "st_ymax(collected)"));
+
+    outer.FROM("(" + collected.getSQL() + ")", "collected");
+
+    OIterator<? extends ValueObject> iter = outer.getIterator();
+
+    try
+    {
+      ValueObject o = iter.next();
+
+      double[] bbox = new double[4];
+      bbox[MINX_INDEX] = Double.parseDouble(o.getValue("minx"));
+      bbox[MINY_INDEX] = Double.parseDouble(o.getValue("miny"));
+      bbox[MAXX_INDEX] = Double.parseDouble(o.getValue("maxx"));
+      bbox[MAXY_INDEX] = Double.parseDouble(o.getValue("maxy"));
+
+      return bbox;
+    }
+    catch (Exception e)
+    {
+      return null;
+      // throw new NoLayerDataException();
+    }
+    finally
+    {
+      iter.close();
+    }
+
   }
 
   public void removeLayers(String... layerNames)
