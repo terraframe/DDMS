6f23da57f79124b4a035e202ae9976135680c96a
rrowlands <rrowlands>
2016-03-01 18:42:43 +0000

    ticket 3380
    git-svn-id: https://subversion.assembla.com/svn/mdss@8160 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 75a1cc9efaf014f2f03237a214137fc4f4d985f6 [formerly aad281b1720a23138c1aecb8c5ebb1c3e5ba8d4d]
    Former-commit-id: 8eba8db737b2670f192cd058936daa06e3056322

--- standalone/patch/manager.bat
+++ standalone/patch/manager.bat
@@ -33,5 +33,4 @@ exit /B
 ::::::::::::::::::::::::::::
 setlocal & pushd .
 
-REM Run shell as admin (example) - put here code as you like
-start JAVA_HOME_VALUE\bin\javaw.exe -Djavax.rmi.ssl.client.enabledProtocols=TLSv1 -Djavax.rmi.ssl.client.enabledCipherSuites=SSL_RSA_WITH_RC4_128_MD5 -Djavax.net.ssl.trustStorePassword=1206b6579Acb3 -Djavax.net.ssl.trustStore=C:\MDSS\manager\keystore\ddms.ts -Djavax.net.ssl.keyStorePassword=4b657920666fZ -Djavax.net.ssl.keyStore=C:\MDSS\manager\keystore\ddms.ks -Djavax.rmi.ssl.enabled=true -cp "C:\MDSS\manager\manager-1.0.0\classes\;C:\MDSS\manager\manager-1.0.0\lib\*" dss.vector.solutions.manager.ServerManagerWindow
+start JAVA_HOME_VALUE\bin\javaw.exe -Djavax.net.ssl.trustStorePassword=1206b6579Acb3 -Djavax.net.ssl.trustStore=C:\MDSS\manager\keystore\ddms.ts -Djavax.net.ssl.keyStorePassword=4b657920666fZ -Djavax.net.ssl.keyStore=C:\MDSS\manager\keystore\ddms.ks -Djavax.rmi.ssl.enabled=true -cp "C:\MDSS\manager\manager-1.0.0\classes\;C:\MDSS\manager\manager-1.0.0\lib\*" dss.vector.solutions.manager.ServerManagerWindow
