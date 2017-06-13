a822220fa16c4cef395ac7873659da881ba39c59
jsmethie@terraframe.com <jsmethie@terraframe.com>
2017-06-13 01:31:54 +0000

    #3527 - A mechanism will be provided for caching Open Street Map (OSM) Base Tiles offline
    git-svn-id: https://subversion.assembla.com/svn/mdss@8461 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 8dd80ebd320f50f8e0c88965b439024650448b6b [formerly aeb199286f7a4addf5d5ea7103ea1a7a76c3d7e9]
    Former-commit-id: b8f31e3ea7cba9e89ca6650711b95de408fc4605

--- branch/1.6/webapp/js/kaleidoscope/MapFactory.js
+++ branch/1.6/webapp/js/kaleidoscope/MapFactory.js
@@ -877,7 +877,7 @@
 	              else if(base.LAYER_SOURCE_TYPE.toLowerCase() === "osm-local"){
 	                baseObj.setSource( 
 	                  new ol.source.TileWMS(({
-	                	    url: 'http://localhost:8080/geoserver/gwc/service/wms',
+	                	    url: 'http://localhost:8080/geoserver-2.9.1/gwc/service/wms',
 	                	    params: {'LAYERS': 'OSM:osm_basic', 'TILED': true, 'VERSION': '1.1.1',
 	                	      'FORMAT': 'image/png', 'WIDTH': 256, 'HEIGHT': 256, 'SRS': 'EPSG:3857'}
 	                	    }))
