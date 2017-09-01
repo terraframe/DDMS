11e4018b2d8aca4815831f9b2697eacf7a5edae7
jsmethie@terraframe.com <jsmethie@terraframe.com>
2017-09-01 14:21:02 +0000

    Org Unit searching
    git-svn-id: https://subversion.assembla.com/svn/mdss@8665 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 7bfede6216cde0780c079005fbaec17ea5a0c395 [formerly fac399d9d001d960bc01a9e5cdb3a686842662ac]
    Former-commit-id: 8f534352a8dcfb3c464dd6e180a3ad11413fb301

--- branch/1.6/generated/server/stub/dss/vector/solutions/etl/dhis2/OrgUnit.java
+++ branch/1.6/generated/server/stub/dss/vector/solutions/etl/dhis2/OrgUnit.java
@@ -12,12 +12,6 @@ import com.runwaysdk.query.OIterator;
 import com.runwaysdk.query.QueryFactory;
 import com.runwaysdk.query.Selectable;
 import com.runwaysdk.query.ValueQuery;
-import com.runwaysdk.session.Request;
-import com.runwaysdk.system.metadata.MdBusiness;
-
-import dss.vector.solutions.geo.generated.District;
-import dss.vector.solutions.geo.generated.Earth;
-import dss.vector.solutions.geo.generated.GeoEntity;
 
 public class OrgUnit extends OrgUnitBase implements com.runwaysdk.generation.loader.Reloadable
 {
@@ -86,57 +80,57 @@ public class OrgUnit extends OrgUnitBase implements com.runwaysdk.generation.loa
     }
   }
 
-  public static void main(String[] args)
-  {
-    test();
-  }
-
-  @Request
-  private static void test()
-  {
-    GeoLevelMap map = new GeoLevelMap();
-    map.setUniversal(MdBusiness.getMdBusiness(Earth.CLASS));
-    map.apply();
-
-    map = new GeoLevelMap();
-    map.setUniversal(MdBusiness.getMdBusiness(GeoEntity.CLASS));
-    map.apply();
-
-    // MdBusiness universal = MdBusiness.getMdBusiness(District.CLASS);
-    //
-    // GeoLevelMap map = new GeoLevelMap();
-    // map.setUniversal(universal);
-    // map.apply();
-    //
-    // OrgUnitLevel level = new OrgUnitLevel();
-    // level.setDhis2Id("District");
-    // level.setLevel(0);
-    // level.setName("District");
-    // level.apply();
-    //
-    // OrgUnit unit = new OrgUnit();
-    // unit.setValid(true);
-    // unit.setCode("DHISCODE");
-    // unit.setDhis2Id("Test Location");
-    // unit.setOrgUnitLevel(level);
-    // unit.setName("Test Location");
-    // unit.apply();
-    //
-    // OrgUnit unit2 = new OrgUnit();
-    // unit2.setValid(true);
-    // unit2.setCode("DHISCODE2");
-    // unit2.setDhis2Id("Test District");
-    // unit2.setOrgUnitLevel(level);
-    // unit2.setName("Test Name");
-    // unit2.apply();
-
-    // OrgUnitLevel level = OrgUnitLevel.getByKey("District");
-    //
-    // OrgUnit unit3 = new OrgUnit();
-    // unit3.setValid(true);
-    // unit3.setDhis2Id("Test District 3");
-    // unit3.setOrgUnitLevel(level);
-    // unit3.setName("Test Name");
-    // unit3.apply();
-  }
+//  public static void main(String[] args)
+//  {
+//    test();
+//  }
+//
+//  @Request
+//  private static void test()
+//  {
+//    GeoLevelMap map = new GeoLevelMap();
+//    map.setUniversal(MdBusiness.getMdBusiness(Earth.CLASS));
+//    map.apply();
+//
+//    map = new GeoLevelMap();
+//    map.setUniversal(MdBusiness.getMdBusiness(GeoEntity.CLASS));
+//    map.apply();
+//
+//    // MdBusiness universal = MdBusiness.getMdBusiness(District.CLASS);
+//    //
+//    // GeoLevelMap map = new GeoLevelMap();
+//    // map.setUniversal(universal);
+//    // map.apply();
+//    //
+//    // OrgUnitLevel level = new OrgUnitLevel();
+//    // level.setDhis2Id("District");
+//    // level.setLevel(0);
+//    // level.setName("District");
+//    // level.apply();
+//    //
+//    // OrgUnit unit = new OrgUnit();
+//    // unit.setValid(true);
+//    // unit.setCode("DHISCODE");
+//    // unit.setDhis2Id("Test Location");
+//    // unit.setOrgUnitLevel(level);
+//    // unit.setName("Test Location");
+//    // unit.apply();
+//    //
+//    // OrgUnit unit2 = new OrgUnit();
+//    // unit2.setValid(true);
+//    // unit2.setCode("DHISCODE2");
+//    // unit2.setDhis2Id("Test District");
+//    // unit2.setOrgUnitLevel(level);
+//    // unit2.setName("Test Name");
+//    // unit2.apply();
+//
+//    // OrgUnitLevel level = OrgUnitLevel.getByKey("District");
+//    //
+//    // OrgUnit unit3 = new OrgUnit();
+//    // unit3.setValid(true);
+//    // unit3.setDhis2Id("Test District 3");
+//    // unit3.setOrgUnitLevel(level);
+//    // unit3.setName("Test Name");
+//    // unit3.apply();
+//  }
 }
