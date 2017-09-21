3965f47799021caf195b23ba2958740c1420ac39
jsmethie@terraframe.com <jsmethie@terraframe.com>
2017-09-21 15:13:54 +0000

    #3765 - ETL import does not cause update of expression fields
    git-svn-id: https://subversion.assembla.com/svn/mdss@8696 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: 10b6712aa192a8c92dd0aa4a96f218272d26f2a8 [formerly 799a8e71ff097c4a3ba6a9069fa758f2f4670a5e]
    Former-commit-id: 7b01e55127a02efff811cc0b2d166e053d45931f

--- trunk/src/server/dss/vector/solutions/kaleidoscope/data/etl/Converter.java
+++ trunk/src/server/dss/vector/solutions/kaleidoscope/data/etl/Converter.java
@@ -1,16 +1,38 @@
 package dss.vector.solutions.kaleidoscope.data.etl;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
+import com.runwaysdk.business.Business;
+import com.runwaysdk.business.BusinessFacade;
+import com.runwaysdk.business.ExpressionException;
+import com.runwaysdk.business.InvalidExpressionSyntaxException;
 import com.runwaysdk.business.Transient;
+import com.runwaysdk.constants.MdAttributeBooleanUtil;
+import com.runwaysdk.constants.MdAttributeDateTimeUtil;
+import com.runwaysdk.constants.MdAttributeDateUtil;
+import com.runwaysdk.constants.MdAttributeTimeUtil;
 import com.runwaysdk.dataaccess.BusinessDAO;
+import com.runwaysdk.dataaccess.MdAttributeBooleanDAOIF;
+import com.runwaysdk.dataaccess.MdAttributeCharDAOIF;
 import com.runwaysdk.dataaccess.MdAttributeConcreteDAOIF;
+import com.runwaysdk.dataaccess.MdAttributeDateDAOIF;
+import com.runwaysdk.dataaccess.MdAttributeDateTimeDAOIF;
+import com.runwaysdk.dataaccess.MdAttributeNumberDAOIF;
+import com.runwaysdk.dataaccess.MdAttributePrimitiveDAOIF;
+import com.runwaysdk.dataaccess.MdAttributeTimeDAOIF;
 import com.runwaysdk.dataaccess.metadata.MdAttributeConcreteDAO;
 import com.runwaysdk.generation.loader.Reloadable;
 
 import dss.vector.solutions.LocalProperty;
 import dss.vector.solutions.general.Disease;
 import dss.vector.solutions.generator.MdFormUtil;
+import ognl.ExpressionSyntaxException;
+import ognl.Ognl;
+import ognl.OgnlClassResolver;
+import ognl.OgnlContext;
+import ognl.OgnlException;
 
 public class Converter implements ConverterIF, Reloadable
 {
@@ -41,8 +63,10 @@ public class Converter implements ConverterIF, Reloadable
 
         MdAttributeConcreteDAOIF mdAttribute = (MdAttributeConcreteDAOIF) business.getMdAttributeDAO(attributeName);
 
-        // get value can intentionally fail if attempting to get the value of a location that is on the
-        // location exclusion list. Note the custom effor after this TRY statement.
+        // get value can intentionally fail if attempting to get the value of a
+        // location that is on the
+        // location exclusion list. Note the custom effor after this TRY
+        // statement.
         FieldValue fValue = field.getValue(mdAttribute, source);
         Object value = fValue.getValue();
 
@@ -69,12 +93,116 @@ public class Converter implements ConverterIF, Reloadable
         TargetDefinitionIF definition = this.context.getDefinition(source.getType());
         PersistenceStrategyIF strategy = definition.getStrategy();
 
+        this.calculate(business);
+
         strategy.handle(business);
       }
     }
     catch (ExclusionException e)
     {
-      // Do nothing. It's likely that a source value was not found because of location exclusions
+      // Do nothing. It's likely that a source value was not found because of
+      // location exclusions
+    }
+  }
+
+  private void calculate(BusinessDAO businessDAO)
+  {
+    // Time to process the expression attributes.
+    List<? extends MdAttributeConcreteDAOIF> mdAttrList = businessDAO.getMdAttributeDAOs();
+
+    for (MdAttributeConcreteDAOIF mdAttributeConcreteDAOIF : mdAttrList)
+    {
+      if (mdAttributeConcreteDAOIF instanceof MdAttributePrimitiveDAOIF)
+      {
+        MdAttributePrimitiveDAOIF mdAttributePrimitiveDAOIF = (MdAttributePrimitiveDAOIF) mdAttributeConcreteDAOIF;
+
+        if (mdAttributePrimitiveDAOIF.isExpression())
+        {
+          String attributeName = mdAttributePrimitiveDAOIF.definesAttribute();
+          // Clear the existing value
+          businessDAO.setValue(attributeName, "");
+
+          String expressionString = mdAttributePrimitiveDAOIF.getExpression();
+
+          try
+          {
+            Object expression;
+            try
+            {
+              expression = Ognl.parseExpression(expressionString);
+            }
+            catch (ExpressionSyntaxException e)
+            {
+              String devMessage = "The attribute [" + mdAttributePrimitiveDAOIF.definesAttribute() + "] has an invalid expression syntax:\n" + e.getLocalizedMessage();
+              throw new InvalidExpressionSyntaxException(devMessage, mdAttributePrimitiveDAOIF, e);
+            }
+
+            OgnlContext ognlContext = new OgnlContext();
+            ognlContext.put("age", businessDAO.getObjectValue("age"));
+
+            Object expressionValue;
+
+            try
+            {
+              Business business = BusinessFacade.get(businessDAO);
+
+              // I am offended that I even have to do this. OGNL stores
+              // reflection method definitions which cause
+              // problems when classes are reloaded.
+              OgnlClassResolver.clearOgnlRuntimeMethodCache();
+              expressionValue = Ognl.getValue(expression, ognlContext, business);
+            }
+            catch (RuntimeException e)
+            {
+              String devMessage = "The expression on attribute [" + mdAttributePrimitiveDAOIF.definesAttribute() + "] has an error:\n" + e.getLocalizedMessage();
+              throw new ExpressionException(devMessage, mdAttributePrimitiveDAOIF, e);
+            }
+
+            Object setterValue;
+
+            if (mdAttributePrimitiveDAOIF instanceof MdAttributeNumberDAOIF && ( expressionValue instanceof Integer || expressionValue instanceof Long || expressionValue instanceof Float || expressionValue instanceof Double || expressionValue instanceof BigDecimal ))
+            {
+              setterValue = expressionValue.toString();
+            }
+            else if (mdAttributePrimitiveDAOIF instanceof MdAttributeDateTimeDAOIF && expressionValue instanceof Date)
+            {
+              setterValue = MdAttributeDateTimeUtil.getTypeUnsafeValue((Date) expressionValue);
+            }
+            else if (mdAttributePrimitiveDAOIF instanceof MdAttributeDateDAOIF && expressionValue instanceof Date)
+            {
+              setterValue = MdAttributeDateUtil.getTypeUnsafeValue((Date) expressionValue);
+            }
+            else if (mdAttributePrimitiveDAOIF instanceof MdAttributeTimeDAOIF && expressionValue instanceof Date)
+            {
+              setterValue = MdAttributeTimeUtil.getTypeUnsafeValue((Date) expressionValue);
+            }
+            else if (mdAttributePrimitiveDAOIF instanceof MdAttributeBooleanDAOIF && expressionValue instanceof Integer)
+            {
+              setterValue = MdAttributeBooleanUtil.format((Integer) expressionValue);
+            }
+            else if (mdAttributePrimitiveDAOIF instanceof MdAttributeCharDAOIF && expressionValue instanceof String)
+            {
+              setterValue = expressionValue;
+            }
+            else if (expressionValue == null)
+            {
+              setterValue = "";
+            }
+            else
+            {
+              setterValue = expressionValue.toString();
+            }
+
+            // Clear the existing value
+            businessDAO.setValue(attributeName, setterValue);
+          }
+          catch (OgnlException e)
+          {
+            String devMessage = "The expression on attribute [" + mdAttributePrimitiveDAOIF.definesAttribute() + "] has an error:\n" + e.getLocalizedMessage();
+            throw new ExpressionException(devMessage, mdAttributePrimitiveDAOIF, e);
+          }
+        }
+      }
     }
   }
 
