36f3dbef46be0bd7d855099ea1882b160b27134c
rrowlands <rrowlands>
2018-04-12 20:55:06 +0000

    #3890 - ODK Form Gen : Exporting GeoEntities
    git-svn-id: https://subversion.assembla.com/svn/mdss@8853 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: e262461b15420e6671070dc8b66fcc7f40b84f75 [formerly d8e5c8158b125f2d2af7128e7c9e972e9f6d1230]
    Former-commit-id: 1c128ebbd1f4090a90a9d850a1f17a08acc48eda

--- trunk/src/server/dss/vector/solutions/odk/ODKFormExporter.java
+++ trunk/src/server/dss/vector/solutions/odk/ODKFormExporter.java
@@ -22,9 +22,13 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Stack;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -35,23 +39,25 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import org.apache.commons.collections.map.LinkedMap;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -69,8 +75,13 @@ import com.runwaysdk.dataaccess.io.excel.ExcelUtil;
 import com.runwaysdk.dataaccess.metadata.MdClassDAO;
 import com.runwaysdk.session.Request;
 
+import dss.vector.solutions.geo.generated.Earth;
+import dss.vector.solutions.geo.generated.GeoEntity;
+
 public class ODKFormExporter
 {
+  private static final String EXPORT_DIR = "/home/rick/Documents/eclipse/workspace/MDSS/dev/";
+  
   /**
    * The DOM <code>document</code> that is populated with data from the core.
    */
@@ -85,6 +96,8 @@ public class ODKFormExporter
   
   private String formName;
 
+  private Logger logger = LoggerFactory.getLogger(ODKFormExporter.class);
+  
   public ODKFormExporter()
   {
     this.odkAttrs = new ArrayList<ODKAttribute>();
@@ -168,14 +181,187 @@ public class ODKFormExporter
     root.setAttribute("xmlns:xsd", "http://www.w3.org/2001/XMLSchema");
     document.appendChild(root);
     
-    doHead();
+    int maxDepth = generateGeoEntityCSV();
     
-    doBody();
+    doHead(maxDepth);
+    
+    doBody(maxDepth);
     
     submit();
   }
   
-  private void doHead()
+  private int generateGeoEntityCSV()
+  {
+    // High level : Select lists must be defined for each level in the tree. The GeoEntities must then be exported to the CSV file at the appropriate level.
+    
+    // 1. Do a depth-first search of the Universal tree to determine the number of levels. TODO : Do we want Universal or GeoEntity?
+    // 2. Create select lists for each level and for each attribute
+    // 3. Export the GeoEntities to a CSV file
+    // 4. Geo filter criteria
+    // 5. Fix bug in form generator
+    
+    int maxDepth = geoLoop(null);
+    
+    GeoLoopHandler csvPrinter = new GeoLoopHandler(maxDepth);
+    
+    try
+    {
+      geoLoop(csvPrinter);
+    }
+    finally
+    {
+      csvPrinter.close();
+    }
+    
+    return maxDepth;
+  }
+  
+  private class GeoLoopHandler
+  {
+    private int maxDepth;
+    
+    private CSVPrinter csvp;
+    
+    private FileWriter writer;
+    
+    public GeoLoopHandler(int maxDepth)
+    {
+      this.maxDepth = maxDepth;
+      
+      FileWriter writer = null;
+      csvp = null;
+      
+      try
+      {
+        writer = new FileWriter(EXPORT_DIR + "itemsets.csv");
+        csvp = new CSVPrinter(writer, CSVFormat.DEFAULT.withRecordSeparator(System.getProperty("line.separator")));
+        
+        ArrayList<String> header = new ArrayList<String>();
+        header.add("label::English");
+        header.add("name");
+        header.add("list_name");
+        
+        for (int i = 0; i < maxDepth-1; ++i)
+        {
+          header.add("geolist_" + i);
+        }
+        
+        csvp.printRecord(header);
+      }
+      catch (IOException e)
+      {
+        throw new RuntimeException(e);
+      }
+    }
+    
+    public void processGeo(GeoEntity geo, LinkedMap parents)
+    {
+      ArrayList<String> geoCSV = new ArrayList<String>();
+      
+      geoCSV.add(geo.getEntityLabel().getValue());
+      geoCSV.add(geo.getGeoId());
+      
+      int curDepth = (parents.size() - 1);
+      geoCSV.add("geolist_" + curDepth);
+      
+      for (int listIndex = 0; listIndex < maxDepth-1; ++listIndex)
+      {
+        int parentIndex = listIndex + 1;
+        if (parentIndex < parents.size())
+        {
+          geoCSV.add(((GeoEntity)parents.getValue(parentIndex)).getGeoId());
+        }
+        else
+          
+        {
+          geoCSV.add("");
+        }
+      }
+      
+      try
+      {
+        csvp.printRecord(geoCSV);
+      }
+      catch (IOException e)
+      {
+        throw new RuntimeException(e);
+      }
+    }
+    
+    public void close()
+    {
+      try
+      {
+        if (writer != null)
+        {
+          writer.close();
+        }
+        if (csvp != null)
+        {
+          csvp.close();
+        }
+      }
+      catch (IOException e)
+      {
+        logger.error("Error occurred while closing resources.", e);
+      }
+    }
+  }
+  
+  private int geoLoop(GeoLoopHandler handler)
+  {
+    GeoEntity parent = Earth.getEarthInstance();
+    Stack<GeoEntity> allChildrenStack = new Stack<GeoEntity>(); // This stack contains GeoEntitys that have not been processed yet
+    LinkedMap parentMap = new LinkedMap(); // Maps the first child geo entity to the parent. This is necessary to know the parents and also when we've gone up in the hierarchy
+    
+    int maxDepth = 0;
+    
+    do
+    {
+      List<GeoEntity> children = parent.getImmediateChildren();
+      
+      for (int i = 0; i < children.size(); ++i)
+      {
+        GeoEntity child = children.get(i);
+        
+        if (i == 0)
+        {
+          parentMap.put(child, parent);
+          
+          if (parentMap.size() > maxDepth)
+          {
+            maxDepth = parentMap.size();
+          }
+        }
+        
+        if (handler != null)
+        {
+          handler.processGeo(child, parentMap);
+        }
+        
+        allChildrenStack.push(child);
+      }
+      
+      if (!allChildrenStack.empty())
+      {
+        parent = allChildrenStack.pop();
+        
+        if (parent.getId().equals(((GeoEntity)parentMap.lastKey()).getId()))
+        {
+          parentMap.remove(parentMap.size()-1);
+        }
+      }
+      else
+      {
+        parent = null;
+      }
+      
+    } while (allChildrenStack.size() > 0 || parent != null);
+    
+    return maxDepth;
+  }
+  
+  private void doHead(int maxDepth)
   {
     Element head = document.createElement("h:head");
     
@@ -183,12 +369,12 @@ public class ODKFormExporter
     title.setTextContent(this.formName);
     head.appendChild(title);
     
-    doModel(head);
+    doModel(head, maxDepth);
     
     root.appendChild(head);
   }
   
-  private void doModel(Element head)
+  private void doModel(Element head, int maxDepth)
   {
     Element model = document.createElement("model");
     head.appendChild(model);
@@ -202,6 +388,18 @@ public class ODKFormExporter
     {
       attr.writeTranslation(translation, document, this.formName);
     }
+    for (int i = 0; i < maxDepth-1; ++i)
+    {
+      Element text = document.createElement("text");
+      
+      text.setAttribute("id", "/" + this.formName + "/geolist_" + i + ":label");
+      
+      Element value = document.createElement("value");
+      value.setTextContent("GeoEntity <Attribute_Name> Level " + (i+1)); // TODO : This needs to repeat for every geo attribute
+      text.appendChild(value);
+      
+      translation.appendChild(text);
+    }
     itext.appendChild(translation);
     
     Element instance = document.createElement("instance");
@@ -212,15 +410,42 @@ public class ODKFormExporter
     {
       attr.writeInstance(instRoot, document, this.formName);
     }
+    for (int i = 0; i < maxDepth-1; ++i)
+    {
+      Element geolist = document.createElement("geolist_" + i);
+      instRoot.appendChild(geolist);
+    }
     model.appendChild(instance);
     
     for (ODKAttribute attr : this.odkAttrs)
     {
       attr.writeBind(model, document, this.formName);
     }
+    
+    // Geo lists //
+    
+    Element bind0 = document.createElement("bind");
+    bind0.setAttribute("nodeset", "/" + this.formName + "/geolist_0");
+    bind0.setAttribute("type", "select1");
+    model.appendChild(bind0);
+    
+    for (int i = 1; i < maxDepth-1; ++i)
+    {
+      Element bind = document.createElement("bind");
+      bind.setAttribute("nodeset", "/" + this.formName + "/geolist_" + i);
+      bind.setAttribute("type", "string");
+      model.appendChild(bind);
+    }
+    
+    Element bindGeoId = document.createElement("bind");
+    bindGeoId.setAttribute("calculate", "concat('uuid:', uuid())");
+    bindGeoId.setAttribute("nodeset", "/" + this.formName + "/meta/instanceID");
+    bindGeoId.setAttribute("readonly", "true()");
+    bindGeoId.setAttribute("type", "string");
+    model.appendChild(bindGeoId);
   }
   
-  private void doBody()
+  private void doBody(int maxDepth)
   {
     Element body = document.createElement("h:body");
     root.appendChild(body);
@@ -228,6 +453,50 @@ public class ODKFormExporter
     for (ODKAttribute attr : this.odkAttrs)
     {
       attr.writeBody(body, document, this.formName);
+    }
+    
+    // Geo lists //
+    
+    Element select1 = document.createElement("select1");
+    select1.setAttribute("ref", "/" + this.formName + "/geolist_0");
+    body.appendChild(select1);
+    
+    Element geolist0Label = document.createElement("label");
+    geolist0Label.setAttribute("ref", "jr:itext('/" + this.formName + "/geolist_0:label')");
+    select1.appendChild(geolist0Label);
+    
+    GeoEntity earth = Earth.getEarthInstance();
+    List<GeoEntity> countries = earth.getImmediateChildren();
+    for (GeoEntity country : countries)
+    {
+      Element item = document.createElement("item");
+      select1.appendChild(item);
+      
+      Element label = document.createElement("label");
+      label.setTextContent(country.getEntityLabel().getValue());
+      item.appendChild(label);
+      
+      Element value = document.createElement("value");
+      value.setTextContent(country.getGeoId());
+      item.appendChild(value);
+    }
+    
+    for (int i = 1; i < maxDepth-1; ++i)
+    {
+      ArrayList<String> queries = new ArrayList<String>();
+      for (int listIndex = 0; listIndex < i; ++listIndex)
+      {
+        queries.add("geolist_" + listIndex + "= /" + this.formName + "/geolist_" + listIndex);
+      }
+      
+      Element input = document.createElement("input");
+      input.setAttribute("query", "instance('geolist_" + i + "')/root/item[" + StringUtils.join(queries, " and ") + "]");
+      input.setAttribute("ref", "/" + this.formName + "/geolist_" + i);
+      body.appendChild(input);
+      
+      Element label = document.createElement("label");
+      input.appendChild(label);
+      label.setAttribute("ref", "jr:itext('/" + this.formName + "/geolist_" + i + ":label')");
     }
   }
   
@@ -248,23 +517,11 @@ public class ODKFormExporter
       baos.close();
       
       // 2. Write the document to a file (for reference)
-      FileOutputStream fos = new FileOutputStream(new File("/home/rick/Documents/eclipse/workspace/MDSS/dev/mosquitos-test.xml")); // TODO : This shouldn't be hardcoded
+      FileOutputStream fos = new FileOutputStream(new File(EXPORT_DIR + "mosquitos-test.xml")); // TODO : This shouldn't be hardcoded
       fos.write(form.getBytes());
       fos.close();
       
       // 3. Push the document to ODK
-//      HTTPConnector http = new HTTPConnector();
-//      http.setCredentials("ddms", "aggregate"); // TODO : This shouldn't be hardcoded
-//      http.setServerUrl("http://172.19.0.1:8080/ODKAggregate"); // TODO : This shouldn't be hardcoded
-//      HTTPResponse response = http.postAsMultipart("formUpload", new File("/home/rick/Documents/eclipse/workspace/MDSS/dev/mosquitos-test.xml"));
-//      
-//      int statusCode = response.getStatusCode();
-//      if (statusCode != HttpStatus.SC_OK && statusCode != HttpStatus.SC_CREATED)
-//      {
-//        System.out.println(response.getResponse());
-//        throw new RuntimeException("Invalid status code [" + statusCode + "].");
-//      }
-      
       CredentialsProvider credsProvider = new BasicCredentialsProvider();
       credsProvider.setCredentials(
               new AuthScope("172.19.0.1", 80),
@@ -275,12 +532,19 @@ public class ODKFormExporter
       MultipartEntityBuilder builder = MultipartEntityBuilder.create();
 
       // This attaches the file to the POST:
-      File f = new File("/home/rick/Documents/eclipse/workspace/MDSS/dev/mosquitos-test.xml"); // TODO : don't hardcode
+      File fFormDef = new File(EXPORT_DIR + "mosquitos-test.xml"); // TODO : don't hardcode
       builder.addBinaryBody(
           "form_def_file",
-          new FileInputStream(f),
+          new FileInputStream(fFormDef),
           ContentType.APPLICATION_XML,
-          f.getName()
+          fFormDef.getName()
+      );
+      File itemsets = new File(EXPORT_DIR + "itemsets.csv"); // TODO : don't hardcode
+      builder.addBinaryBody(
+          "mediaFiles",
+          new FileInputStream(itemsets),
+          ContentType.APPLICATION_XML,
+          itemsets.getName()
       );
       
       HttpEntity multipart = builder.build();
