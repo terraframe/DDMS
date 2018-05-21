8d3e2bd7bd8a7801f9211ce2ffe916a9d8eb3258
jsmethie@terraframe.com <jsmethie@terraframe.com>
2018-05-21 15:55:07 +0000

    #3884 - Reusable spreadsheet data exporter #3886 - ODK Form Generation – Part 2
    git-svn-id: https://subversion.assembla.com/svn/mdss@8959 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: e4178267e057b2157491ae2e8c20d17cbe6a7831 [formerly 5c911e345bb46c434ee37a4ff68938f7c248f5c2]
    Former-commit-id: 783117f8cc0e11221b9576f042c9ea52c0954791

--- trunk/src/common/dss/vector/solutions/MDSSInfo.java
+++ trunk/src/common/dss/vector/solutions/MDSSInfo.java
@@ -1,18 +1,18 @@
 /*******************************************************************************
  * Copyright (C) 2018 IVCC
  * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package dss.vector.solutions;
 
@@ -41,4 +41,7 @@ public interface MDSSInfo
   public static final String FORM_SURVEY_QB                  = ROOT_PACKAGE + ".querybuilder.FormSurveyQB";
 
   public static final String ROOT_FORM_SURVEY_CLASS          = GENERATED_FORM_BUSINESS_PACKAGE + ".FormSurvey";
+
+  public static final String LISTENER_METHOD                 = "setupExportListener";
+
 }
