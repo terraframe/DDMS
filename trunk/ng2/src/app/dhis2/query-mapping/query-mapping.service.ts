2adaf4e105c368e621e20610d078c9300401145c
jmapping <jmapping>
2018-07-02 21:08:21 +0000

    Fixing catch block statements
    git-svn-id: https://subversion.assembla.com/svn/mdss@9089 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: f2be40ae7a1930ad24d2e881ebea7fd55790aaf8 [formerly 0a7f8ef8a45a84ba546d485f8550027fcfa9a6dd]
    Former-commit-id: 989f398da11a96f4fa6eb987ecc4f373ca540ed3

--- trunk/ng2/src/app/dhis2/query-mapping/query-mapping.service.ts
+++ trunk/ng2/src/app/dhis2/query-mapping/query-mapping.service.ts
@@ -33,9 +33,10 @@ export class QueryMappingService extends BasicService {
 
         return response.json() as QueryMapping[];
       })
-      .catch(
+      .catch(e => {
     		  this.analyticsService.pushAalyticsTrackingTagEvent("FAILURE", "/dhis2/mappings", "post", {});
-    		  this.handleError.bind(this));      
+    		  this.handleError.bind(this)
+      });      
   }    
   
   newInstance(): Promise<{id:string, label:string}[]> {
@@ -52,9 +53,10 @@ export class QueryMappingService extends BasicService {
 
       return response.json() as {id:string, label:string}[];
     })
-    .catch(
+    .catch(e => {
     		this.analyticsService.pushAalyticsTrackingTagEvent("FAILURE", "/dhis2/newInstance", "post", {});
-    		this.handleError.bind(this));      
+    		this.handleError.bind(this)
+    });      
   }    
   
   apply(mapping:QueryMapping): Promise<Response> {
@@ -67,9 +69,10 @@ export class QueryMappingService extends BasicService {
     return this.ehttp
     .post(acp + '/dhis2/apply', JSON.stringify({mapping:mapping}), {headers: headers})
     .toPromise()
-    .catch(
+    .catch(e => {
     		this.analyticsService.pushAalyticsTrackingTagEvent("FAILURE", "/dhis2/apply", "post", {mapping:mapping});
-    		this.handleError.bind(this));      
+    		this.handleError.bind(this)
+    });      
   }    
   
   remove(id:string): Promise<Response> {
@@ -82,9 +85,10 @@ export class QueryMappingService extends BasicService {
     return this.ehttp
     .post(acp + '/dhis2/remove', JSON.stringify({id:id}), {headers: headers})
     .toPromise()
-    .catch(
+    .catch(e => {
     		this.analyticsService.pushAalyticsTrackingTagEvent("FAILURE", "/dhis2/remove", "post", {id:id});
-    		this.handleError.bind(this));      
+    		this.handleError.bind(this)
+    });      
   }    
   
   
