1988385b9915461375ee0b4b299cb609b586aae6
jsmethie@terraframe.com <jsmethie@terraframe.com>
2018-07-31 21:27:02 +0000

    #3952 - ODK: imported spreadsheet is not stored in the archive directory when import is partially successful
    git-svn-id: https://subversion.assembla.com/svn/mdss@9107 e0e46c49-be69-4f5a-ad62-21024a331aea
    
    Former-commit-id: f8b79d356e8f81398f472f816e2f7ed1aacf8904 [formerly 2a2b45b52ed4313c977c9e300b5239496f8b1bad]
    Former-commit-id: 5d1ec38a339f6081bc7ff948acaced46eee68dfe

--- trunk/generated/server/stub/dss/vector/solutions/odk/MobileDataUploadJob.java
+++ trunk/generated/server/stub/dss/vector/solutions/odk/MobileDataUploadJob.java
@@ -132,8 +132,8 @@ public class MobileDataUploadJob extends MobileDataUploadJobBase implements com.
 
     try
     {
-       ODK2Excel importer = new ODK2Excel(form, null);
-//      ODK2Excel importer = new ODK2Excel(form, this.getQueryCursor());
+//       ODK2Excel importer = new ODK2Excel(form, null);
+      ODK2Excel importer = new ODK2Excel(form, this.getQueryCursor());
       Collection<String> allUUIDS = importer.getUUIDs();
 
       if (allUUIDS.size() > 0)
