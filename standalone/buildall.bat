1e060da3fdf298cf7bf73a6a63ea13ba3c8c72f6
rrowlands <rrowlands@terraframe.com>
2019-11-07 23:33:18 +0000

    Removed large files

--- standalone/buildall.bat
+++ standalone/buildall.bat
@@ -13,9 +13,9 @@ copy ..\DDMS\lib\common\runwaysdk-gis-common.jar C:\git\DDMS\standalone\backup-m
 copy ..\DDMS\lib\server\runwaysdk-server.jar C:\git\DDMS\standalone\backup-manager-1.0.0\lib\runwaysdk-server.jar
 copy ..\DDMS\lib\server\runwaysdk-gis-server.jar C:\git\DDMS\standalone\backup-manager-1.0.0\lib\runwaysdk-gis-server.jar
 
+REM Tomcat Remote Listener
 call mvn -U -f ..\dss.vector.org.json\pom.xml clean install
 if %errorlevel% neq 0 exit /b %errorlevel%
-
 call mvn -U -f ..\tomcat-remote-listener\pom.xml clean install
 if %errorlevel% neq 0 exit /b %errorlevel%
 call mvn -U -f ..\manager\pom.xml dependency:copy-dependencies -DoutputDirectory=C:\git\DDMS\standalone\manager-1.0.0\lib
@@ -23,6 +23,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 xcopy /y ..\tomcat-remote-listener\target\tomcat-remote-listener.jar C:\git\DDMS\standalone\manager-1.0.0\lib
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+REM Manager
 call mvn -U -f ..\manager\pom.xml clean install
 if %errorlevel% neq 0 exit /b %errorlevel%
 call mvn -U -f ..\manager\pom.xml dependency:copy-dependencies -DoutputDirectory=C:\git\DDMS\standalone\manager-1.0.0\lib
@@ -30,9 +31,11 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 xcopy /y ..\manager\target\manager-1.0.0.jar C:\git\DDMS\standalone\manager-1.0.0\lib
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+REM DDMS Initializer
 cmd /c "%ANT_HOME%\bin\ant" -Dbasedir=..\ddms-initializer -buildfile ..\ddms-initializer\scripts\deploy.xml -Dmdss.admin=C:\git\DDMS\standalone -Dmdss.root=C:\git\DDMS\DDMS -Dswt.jar=C:\Users\Administrator\.m2\repository\org\eclipse\swt\org\eclipse\swt\win32\win32\x86_64\4\3\swt\org.eclipse.swt.win32.win32.x86_64\4.3\org.eclipse.swt.win32.win32.x86_64-4.3.jar deploy
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+REM Backup Manager
 call mvn -U -f ..\backup-manager\pom.xml clean install
 if %errorlevel% neq 0 exit /b %errorlevel%
 call mvn -U -f ..\manager\pom.xml dependency:copy-dependencies -DoutputDirectory=C:\git\DDMS\standalone\backup-manager-1.0.0\lib
@@ -40,8 +43,18 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 xcopy /y ..\backup-manager\target\backup-manager-1.0.0.jar C:\git\DDMS\standalone\backup-manager-1.0.0\lib
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+REM MDSSGISI
 cmd /c "%ANT_HOME%\bin\ant" -Dbasedir=..\MDSSGISI -buildfile ..\MDSSGISI\scripts\deploy.xml -Dmdss.admin=C:\git\DDMS\standalone -Dmdss.root=C:\git\DDMS\DDMS -Dswt.jar=C:\Users\Administrator\.m2\repository\org\eclipse\swt\org\eclipse\swt\win32\win32\x86_64\4\3\swt\org.eclipse.swt.win32.win32.x86_64\4.3\org.eclipse.swt.win32.win32.x86_64-4.3.jar deploy
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+REM Sync Manager
 cmd /c "%ANT_HOME%\bin\ant" -Dbasedir=..\synch-manager -buildfile ..\synch-manager\scripts\deploy.xml -Dmdss.admin=C:\git\DDMS\standalone -Dmdss.root=C:\git\DDMS\DDMS -Dswt.jar=C:\Users\Administrator\.m2\repository\org\eclipse\swt\org\eclipse\swt\win32\win32\x86_64\4\3\swt\org.eclipse.swt.win32.win32.x86_64\4.3\org.eclipse.swt.win32.win32.x86_64-4.3.jar deploy
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+REM Runway Patcher
+cmd /c "%ANT_HOME%\bin\ant" -buildfile ..\runway-patcher\scripts\build.xml -Dbasedir=C:\git\DDMS\runway-patcher
+if %errorlevel% neq 0 exit /b %errorlevel%
+call mvn -U -f ..\runway-patcher\pom.xml clean install
+if %errorlevel% neq 0 exit /b %errorlevel%
+xcopy /y ..\runway-patcher\target\runway-patcher-1.0.0.jar C:\git\DDMS\ddms-runway-patcher\lib
 if %errorlevel% neq 0 exit /b %errorlevel%
