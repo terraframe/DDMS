ca9288181e71ba249f84f964db48fcbc947a2634
rrowlands <rrowlands@terraframe.com>
2020-07-20 15:26:41 -0600

    Add files via upload

---
+++ documents/wiki/creating-new-installation/backup-and-mirror.bat
@@ -0,0 +1,23 @@
+setlocal
+
+REM This script can be used to automatically take periodic backups and place them in a transfer directory for mirroring. TODO : Not tested with multiple apps
+
+REM ##### Variables #########
+SET DDMS_DIR=C:\Users\Administrator\DDMS
+SET BACKUP_DIR=%DDMS_DIR%\backups
+SET BACKUP_LOG=%DDMS_DIR%\logs\backup.log
+SET TRANSFER_DIR=%DDMS_DIR%\transfer
+REM ##### Variables #########
+
+powershell -Command "powershell -ExecutionPolicy ByPass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File C:\MDSS\manager\manager.ps1 -backupAll %BACKUP_DIR% > %BACKUP_LOG%"
+
+
+set srcDir=%BACKUP_DIR%
+set destdir=%TRANSFER_DIR%
+set lastmod=
+pushd %srcDir%
+for /f "tokens=*" %%a in ('dir *.zip /b /o-d /a-d 2^>NUL') do set lastmod=%%a & goto :xx
+:xx
+if "%lastmod%"=="" echo Could not locate files.&goto :eof
+:::
+copy "%lastmod%" "%TRANSFER_DIR%"
