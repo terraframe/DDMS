64a1b9c8673057d6a3afdfa88f302f07254279f5
rrowlands <rrowlands@terraframe.com>
2020-07-27 19:13:45 -0600

    Update backup-and-mirror.bat

--- documents/wiki/creating-new-installation/backup-and-mirror.bat
+++ documents/wiki/creating-new-installation/backup-and-mirror.bat
@@ -3,13 +3,13 @@ setlocal
 REM This script can be used to automatically take periodic backups and place them in a transfer directory for mirroring. TODO : Not tested with multiple apps
 
 REM ##### Variables #########
-SET DDMS_DIR=C:\Users\Administrator\DDMS
+SET DDMS_DIR=C:\path\to\google-drive\DDMS
 SET BACKUP_DIR=%DDMS_DIR%\backups
 SET BACKUP_LOG=%DDMS_DIR%\logs\backup.log
 SET TRANSFER_DIR=%DDMS_DIR%\transfer
 REM ##### Variables #########
 
-powershell -Command "powershell -ExecutionPolicy ByPass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File C:\MDSS\manager\manager.ps1 -backupAll %BACKUP_DIR% > %BACKUP_LOG%"
+powershell -Command "powershell -ExecutionPolicy ByPass -File C:\MDSS\manager\manager.ps1 -backupAll \"%BACKUP_DIR%\" > \"%BACKUP_LOG%\""
 
 
 set srcDir=%BACKUP_DIR%
