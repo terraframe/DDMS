a0c57634a05a1a694df00a10cc4e42496fe49de7
rrowlands <rrowlands@terraframe.com>
2020-09-10 10:16:12 -0600

    Update ssl-renew.bat

--- build/ssl-renew.bat
+++ build/ssl-renew.bat
@@ -19,6 +19,7 @@ set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 rmdir /s /q C:\ssl-working\workdir
 rmdir /s /q C:\ssl-working\well-known
 rmdir /s /q C:\MDSS\tomcat\webapps\ROOT\.well-known
+if not exist "C:\ssl-working\certdir" mkdir C:\ssl-working\certdir
 
 powershell -Command "powershell -ExecutionPolicy ByPass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File C:\MDSS\manager\manager.ps1 -stopTomcat"
 IF %ERRORLEVEL% NEQ 0 ( CALL :HandleError  )
@@ -28,10 +29,27 @@ start /wait msgbox.vbs
 del /F /Q msgbox.vbs
 
 set BACKUP_DATE=%YYYY%-%MM%-%DD%
-copy certdir\* %BACKUP_DIR%\certdir-%BACKUP_DATE%\*
+if not exist "%BACKUP_DIR%\certdir-%BACKUP_DATE%" mkdir "%BACKUP_DIR%\certdir-%BACKUP_DATE%"
+IF %ERRORLEVEL% NEQ 0 ( CALL :HandleError )
+
+REM If the certdir is empty the copy below will fail.... so we have to check if the certdir is empty or not.
+set _TMP=
+for /f "delims=" %%a in ('dir /b C:\ssl-working\certdir') do set _TMP=%%a
+
+IF {%_TMP%}=={} (
+  echo "Certdir was empty. Not backing it up."
+) ELSE (
+  echo "Certdir was not empty. Backing it up."
+  copy certdir\* "%BACKUP_DIR%\certdir-%BACKUP_DATE%\*"
+  IF %ERRORLEVEL% NEQ 0 ( CALL :HandleError )
+)
 
 mkdir C:\ssl-working\workdir
+IF %ERRORLEVEL% NEQ 0 ( CALL :HandleError )
+
 mkdir C:\ssl-working\well-known
+IF %ERRORLEVEL% NEQ 0 ( CALL :HandleError )
+
 mkdir C:\ssl-working\well-known\acme-challenge
 IF %ERRORLEVEL% NEQ 0 ( CALL :HandleError )
 
@@ -39,15 +57,24 @@ C:\ssl-working\jdk\bin\java.exe -jar acme_client.jar --command order-certificate
 IF %ERRORLEVEL% NEQ 0 ( CALL :HandleError )
 
 mkdir C:\MDSS\tomcat\webapps\ROOT\.well-known
+IF %ERRORLEVEL% NEQ 0 ( CALL :HandleError )
+
 mkdir C:\MDSS\tomcat\webapps\ROOT\.well-known\acme-challenge
+IF %ERRORLEVEL% NEQ 0 ( CALL :HandleError )
+
 copy C:\ssl-working\well-known\acme-challenge\* C:\MDSS\tomcat\webapps\ROOT\.well-known\acme-challenge\*
 IF %ERRORLEVEL% NEQ 0 ( CALL :HandleError )
 
 powershell -Command "powershell -ExecutionPolicy ByPass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File C:\MDSS\manager\manager.ps1 -startTomcat"
 IF %ERRORLEVEL% NEQ 0 ( CALL :HandleError )
 
-del /F /Q certdir
+RMDIR /S /Q certdir
+IF %ERRORLEVEL% NEQ 0 ( CALL :HandleError )
+
 mkdir certdir
+IF %ERRORLEVEL% NEQ 0 ( CALL :HandleError )
+
+C:\ssl-working\jdk\bin\java.exe -jar acme_client.jar --command verify-domains -a account.key -w ./workdir -c ./%DOMAIN%.csr
 IF %ERRORLEVEL% NEQ 0 ( CALL :HandleError )
 
 C:\ssl-working\jdk\bin\java.exe -jar acme_client.jar --command generate-certificate -a ./account.key -w workdir --csr %DOMAIN%.csr --cert-dir certdir
@@ -55,6 +82,9 @@ IF %ERRORLEVEL% NEQ 0 ( CALL :HandleError )
 
 C:\ssl-working\jdk\bin\keytool.exe -importcert -alias %DOMAIN% -keystore cacerts.jks -storepass changeit -file .\certdir\fullchain.pem
 IF %ERRORLEVEL% NEQ 0 ( CALL :HandleError )
+
+powershell -Command "powershell -ExecutionPolicy ByPass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File C:\MDSS\manager\manager.ps1 -stopTomcat"
+IF %ERRORLEVEL% NEQ 0 ( CALL :HandleError  )
 
 echo x=msgbox("Open the manager settings and re-enable HTTPS. For the keystore location set it to C:/ssl-working/cacerts.jks. For the keystore alias type in %DOMAIN%. For the keystore pass type in %KEYSTORE_PASSWORD%", 0, "DDMS SSL Certificate Renewal") > msgbox.vbs
 start /wait msgbox.vbs
